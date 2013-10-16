74bffb43613a8cfa38135cf229f004b1b11ae142
Ole Weidner <ole.weidner@rutgers.edu>
2013-10-17 00:27:04 +0200

    First benchmark series -- check.
    MongoDB running on a 1-core VM on the same machine:
    
    (documents resemble a SAGA-Pilot work-unit)
    
    Results for mongodb://mongohost:27017/:
    ======================================================================
    Average time to create a collection: 0.001537 sec.
    Average time to add documents in bulks of 1: 0.000535 sec.
    Average time to add documents in bulks of 2: 0.000353 sec.
    Average time to add documents in bulks of 4: 0.000501 sec.
    Average time to add documents in bulks of 8: 0.000754 sec.
    Average time to add documents in bulks of 16: 0.000905 sec.
    Average time to add documents in bulks of 32: 0.001462 sec.
    Average time to add documents in bulks of 64: 0.002336 sec.
    Average time to add documents in bulks of 128: 0.004139 sec.
    Average time to add documents in bulks of 256: 0.008066 sec.
    Average time to add documents in bulks of 512: 0.017106 sec.
    Average time to add documents in bulks of 1024: 0.034532 sec.
    Average time to add documents in bulks of 2048: 0.077557 sec.
    Average time to add documents in bulks of 4096: 0.147238 sec.
    Average time to add documents in bulks of 8192: 2.678289 sec.
    Average time to add documents in bulks of 16384: 3.671604 sec.

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -7,20 +7,47 @@
 import uuid
 import time
 import numpy
+from copy import deepcopy
 from pymongo import MongoClient
 
 DBURL = 'mongodb://mongohost:27017/'
-REPS  = 10
+
+wu = {
+        "_UnitManagerID": "manager.objectID()",
+        "_PilotID:": "pilot.objectID()",
+
+        "Name": "BSON.STRING",
+        "Description": {
+            "Name": "BSON.STRING",
+            "Executable": "BSON.STRING",
+            "Arguments": ["BSON.STRING", "BSON.STRING", "..."],
+            "Environment": "BSON.STRING",
+            "StartTime": "BSON.STRING",
+            "RunTime": "BSON.STRING",
+            "WorkingDirectory": "BSON.STRING",
+            "Input": "BSON.STRING",
+            "Output": "BSON.STRING",
+            "Error": "BSON.STRING",
+            "Slots": "BSON.STRING"
+        },
+        "Info": {
+            "State": "BSON.STRING",
+            "SubmitTime": "BSON.Date",
+            "StartTime": "BSON.Date",
+            "EndTime": "BSON.Date" 
+        }
+    }
 
 client = MongoClient(DBURL)
 
-# Benchmark how fast we can create new collections. 
+################################################################################
+# Benchmark to see how fast we can create new collections. 
 # 
 # An important note about collections (and databases) in MongoDB is that they
 # are created lazily - none of the above commands have actually performed any
 # operations on the MongoDB server. Collections and databases are created when
 # the first document is inserted into them.
-
+#
 create_coll_timings = list()
 db = client.perftest
 for c in range(0, 1024):
@@ -31,9 +58,37 @@ for c in range(0, 1024):
     collection.drop()
     create_coll_timings.append(td)
 
+################################################################################
+# Benchmark how fast we can create new documents.
+#
+# Documents can be work units submitted to a pilot manager for example.
+#
+create_doc_timings = dict()
+db = client.perftest
+collection = db.test.workunits
+
+
+for i in [1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384]:
+    create_doc_timings[i] = list()
+    for d in range(0, 10):
+        # create a list that contains 'i' entries
+        insert = list()
+        for x in range(0, i):
+            insert.append(deepcopy(wu))
+
+
+        t1 = time.time()
+        collection.insert(insert)
+        td = time.time() -t1
+        create_doc_timings[i].append(td)
+    collection.drop()
+
+
 print "\nResults for %s: " % DBURL
 print "======================================================================"
 print " Average time to create a collection: %f sec." % numpy.mean(create_coll_timings)
+for key, val in sorted(create_doc_timings.items()):
+    print "Average time to add documents in bulks of %i: %f sec." % (key, numpy.mean(val))
 print ""
 
 
