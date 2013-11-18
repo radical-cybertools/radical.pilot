84693625960da8f374ea018db52d90859d1ef2ce
Ole Weidner <ole.weidner@rutgers.edu>
2013-11-18 13:41:26 -0500

    added missing file

--- src/sinon/utils/__init__.py
+++ src/sinon/utils/__init__.py
@@ -11,25 +11,3 @@ def as_list(obj):
     else:
         obj_list = obj
     return obj_list
-
-def create_saga_job_description(pilot_uid, pilot_desc, resource_cfg):
-    """Creates a SAGA job descritpion from a PilotDescription
-    and from a resource configuration. 
-    """
-
-    jd = Description()
-    jd.working_directory = "/N/u/oweidner/saga-pilot/"
-    jd.executable = "/N/u/oweidner/saga-pilot/bootsrap-and-run-agent-%s" % pilot_uid
-    jd.output = "saga-pilot-agent-%s.out" % pilot_uid
-    jd.error  = "saga-pilot-agent-%s.err" % pilot_uid
-    jd.arguments  = ["-r", "host", "-u", "123"]
-
-    # First we initialize the job description with whatever 
-    # we find in the resource configuration.
-
-
-    # Now we add attributes from the pilot description. 
-    # Resource configuration attributes get overwritten if 
-    # defined in the pilot description as well.
-
-    return jd
