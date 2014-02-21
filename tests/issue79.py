08749e7752f083e037d1c6690e9927f1eb7f3b21
Ole Weidner <ole.weidner@rutgers.edu>
2014-02-20 23:21:26 -0500

    attempt to verify #79

---
+++ tests/issue79.py
@@ -0,0 +1,46 @@
+import sagapilot as sp
+
+COORD       = "mongodb://ec2-184-72-89-141.compute-1.amazonaws.com:27017/"
+FG_CONF     = "https://raw.github.com/saga-project/saga-pilot/master/configs/futuregrid.json"
+XSEDE_CONF  = "https://raw.github.com/saga-project/saga-pilot/master/configs/xsede.json"
+PILOT_DESCR = {'project':     None, 
+               'resource':    'india.futuregrid.org', 
+               'queue':       'interactive', 
+               'sandbox':     '/N/u/merzky//troy_agents/', 
+               'cleanup':     None, 
+               'cores':       4, 
+               'runtime':     600}
+UNIT_DESCR  = {'environment': None,
+               'input_data':  None,
+               'executable':  '/N/u/marksant/bin/mdrun',
+               'name':        None,
+               'output_data': None,
+               'cores':       None,
+               'working_directory_priv': '/N/u/merzky//troy_tutorial/troy_tutorial_01_2/',
+               'arguments':   ['topol.tpr']}
+
+
+session = sp.Session           (database_url = COORD)
+sp_um    = sp.UnitManager      (session      = session, 
+                                scheduler    = 'direct_submission')
+sp_pm    = sp.PilotManager     (session      = session,
+                                resource_configurations = [FG_CONF, XSEDE_CONF])
+sp_pilot_descr = sp.ComputePilotDescription ()
+sp_pilot_descr.resource = PILOT_DESCR['resource']
+sp_pilot_descr.cores    = PILOT_DESCR['cores']
+sp_pilot_descr.runtime  = PILOT_DESCR['runtime']
+sp_pilot_descr.queue    = PILOT_DESCR['queue']
+sp_pilot_descr.sandbox  = PILOT_DESCR['sandbox']
+
+sp_pilot = sp_pm.submit_pilots (sp_pilot_descr)
+
+sp_um.add_pilots (sp_pilot)
+sp_cu_descr = sp.ComputeUnitDescription ()
+for key in UNIT_DESCR :
+    sp_cu_descr[key] = UNIT_DESCR[key]
+
+
+sp_cu    = sp_um.submit_units  (sp_cu_descr)
+
+print "STATE: %s" % sp_cu.state
+
