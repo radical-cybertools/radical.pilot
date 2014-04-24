686f3919ea55759bbff502d355b0d416eafb2e3e
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2014-04-25 01:29:27 +0200

    Some comments and notes.

--- src/radical/pilot/staging_directives.py
+++ src/radical/pilot/staging_directives.py
@@ -5,7 +5,7 @@
 COPY     = 'Copy'     # local cp
 LINK     = 'Link'     # local ln -s
 MOVE     = 'Move'     # local mv
-TRANSFER = 'Transfer' # saga remote transfer
+TRANSFER = 'Transfer' # saga remote transfer TODO: This might just be a special case of copy
 
 #
 # Flags
@@ -23,4 +23,4 @@ class StagingDirectives(saga.Attributes):
         self.target   = None # radical.pilot.Url() or string
         self.action   = None # COPY, LINK, MOVE, TRANSFER
         self.flags    = None # SKIP_FAILED, CREATE_PARENTS
-        self.priority = 0    # Control ordering of actions
+        self.priority = 0    # Control ordering of actions, mainly interesting for output probably
