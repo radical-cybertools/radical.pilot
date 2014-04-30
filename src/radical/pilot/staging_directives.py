c8facec7f98e1210c2f1052f51de91f27591ce74
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2014-04-30 14:02:41 +0200

    Allow "serialisation".

--- src/radical/pilot/staging_directives.py
+++ src/radical/pilot/staging_directives.py
@@ -14,7 +14,7 @@ SKIP_FAILED    = 'SkipFailed'     # Don't stage out files if tasks failed
 CREATE_PARENTS = 'CreateParents'  # Create parent directories while writing to staging area
 
 
-class StagingDirectives(saga.Attributes):
+class StagingDirectives(dict):
 
     def __init__(self):
 
@@ -24,3 +24,25 @@ class StagingDirectives(saga.Attributes):
         self.action   = None # COPY, LINK, MOVE, TRANSFER
         self.flags    = None # SKIP_FAILED, CREATE_PARENTS
         self.priority = 0    # Control ordering of actions, mainly interesting for output probably
+
+    #------------------------------------------------------------------------------
+    #
+    def as_dict(self):
+        """Returns a Python dictionary representation of the object.
+        """
+        obj_dict = {
+            'source'   : self.source,
+            'target'   : self.target,
+            'action'   : self.action,
+            'flags'    : self.flags,
+            'priority' : self.priority,
+        }
+        return obj_dict
+
+
+    #------------------------------------------------------------------------------
+    #
+    def __str__(self):
+        """Returns a string representation of the object.
+        """
+        return str(self.as_dict())
