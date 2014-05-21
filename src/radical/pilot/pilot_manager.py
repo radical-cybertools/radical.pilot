e50cdc36c5fa5cc2e80884fa8bdb584b0704c78b
Ole Weidner <ole.weidner@rutgers.edu>
2014-05-20 21:52:13 -0400

    fix

--- src/radical/pilot/pilot_manager.py
+++ src/radical/pilot/pilot_manager.py
@@ -119,7 +119,7 @@ class PilotManager(Object):
         self._resource_cfgs["localhost"] = {
             "URL"              : "fork://localhost",
             "filesystem"       : "file://localhost",
-            "pre_bootstrap"    : ["brew search test", "date"],
+            "pre_bootstrap"    : ["hostname", "date"],
             "task_launch_mode" : "LOCAL",
             "bootstrapper"     : "default_bootstrapper.sh"
         }
