30331d0d787382d7a2405270a5c2775fd2599a71
Andre Merzky <andre@merzky.net>
2014-06-17 14:37:02 +0200

    use poor mans data chaining...
    you need to create a $HOME/tmp/ dir on the target machine.  Not sure if that should be used for the tutorial, or if that will rather confuse...

--- examples/tutorial/chained_ensembles.py
+++ examples/tutorial/chained_ensembles.py
@@ -37,10 +37,10 @@ def pilot_state_cb(pilot, state):
     """pilot_state_change_cb() is a callback function. It gets called very
     time a ComputePilot changes its state.
     """
-    print "[Callback]: ComputePilot '{0}' state changed to {1}.".format(
-        pilot.uid, state)
 
     if state == radical.pilot.states.FAILED:
+        print "[Callback]: Pilot '%s' state changed to %s." % (pilot.uid, state)
+        print "            Log: \n%s" % pilot.log
         sys.exit(1)
 
 #------------------------------------------------------------------------------
@@ -49,10 +49,9 @@ def unit_state_change_cb(unit, state):
     """unit_state_change_cb() is a callback function. It gets called very
     time a ComputeUnit changes its state.
     """
-    print "[Callback]: ComputeUnit '{0}' state changed to {1}.".format(
-        unit.uid, state)
     if state == radical.pilot.states.FAILED:
-        print "            Log: %s" % unit.log[-1]
+        print "[Callback]: CU '%s' state changed to '%s'." % (unit.uid, state)
+        print "            Log: \n%s" % unit.log
 
 # ---------------- END REQUIRED PILOT SETUP -----------------
 #
@@ -71,6 +70,7 @@ def main():
         session.add_credential(cred)
 
         # Add a Pilot Manager. Pilot managers manage one or more ComputePilots.
+        print "create pilot manager"
         pmgr = radical.pilot.PilotManager(session=session)
 
         # Register our callback with the PilotManager. This callback will get
@@ -87,11 +87,13 @@ def main():
         pdesc.cores    = PILOT_SIZE
         pdesc.cleanup  = True
 
-        # Launch the pilot.
+        # submit the pilot.
+        print "submit pilot"
         pilot = pmgr.submit_pilots(pdesc)
 
         # Combine the ComputePilot, the ComputeUnits and a scheduler via
         # a UnitManager object.
+        print "create unit manager"
         umgr = radical.pilot.UnitManager(
             session=session,
             scheduler=radical.pilot.SCHED_DIRECT_SUBMISSION)
@@ -102,20 +104,19 @@ def main():
         umgr.register_callback(unit_state_change_cb)
 
         # Add the previsouly created ComputePilot to the UnitManager.
+        print "add    pilot"
         umgr.add_pilots(pilot)
 
-        # submit 'A' cus to pilot job
+        # submit A cus to pilot job
         cudesc_set_A = list ()
         for i in range(NUMBER_JOBS):
 
             # -------- BEGIN USER DEFINED CU 1 DESCRIPTION --------- #
             cudesc = radical.pilot.ComputeUnitDescription()
-            cudesc.environment = {"CU_SET": "A", "CU_NO": i}
+            cudesc.environment = {"CU_SET": "A", "CU_NO": "%02d" % i}
             cudesc.executable  = "/bin/echo"
-            cudesc.arguments   = ['I am an $CU_SET CU with id $CU_NO', ]
+            cudesc.arguments   = ['"$CU_SET CU with id $CU_NO"', '>', '$HOME/tmp/A-$CU_NO.txt', ]
             cudesc.cores       = 1
-        ##  cudesc.stdout      = 'A-stdout.txt'
-        ##  cudesc.stderr      = 'A-stderr.txt'
             # -------- END USER DEFINED CU 1 DESCRIPTION --------- #
 
             cudesc_set_A.append(cudesc)
@@ -123,35 +124,39 @@ def main():
         # Submit the previously created ComputeUnit descriptions to the
         # PilotManager. This will trigger the selected scheduler to start
         # assigning ComputeUnits to the ComputePilots.
+        print "submit units A"
         cu_set_A = umgr.submit_units (cudesc_set_A)
 
         # Chaining cus i.e submit a compute unit, when compute unit from A is successfully executed.
-        # A 'B' CU reads the content of the output file of an 'A' CU and writes it into its own
+        # A B CU reads the content of the output file of an A CU and writes it into its own
         # output file.
         cu_set_B = list()
         while len(cu_set_A) > 0 :
             for idx,cu_a in enumerate (cu_set_A) :
                 cu_a.wait ()
-                print "One 'A' CU %s finished. Launching a 'B' CU." % (cu_a.uid)
+                print "---------------"
+                print "A CU '%s' finished. Submitting B CU." % (cu_a.uid)
 
                 # -------- BEGIN USER DEFINED CU 2 DESCRIPTION --------- #
                 cudesc = radical.pilot.ComputeUnitDescription()
+                cudesc.environment = {'CU_SET': 'B', 'CU_NO': "%02d" % idx}
                 cudesc.executable  = '/bin/echo'
-                cudesc.arguments   = ['I am a $CU_SET CU with id $CU_NO', ]
-                cudesc.environment = {'CU_SET': 'B', 'CU_NO': idx}
+                cudesc.arguments   = ['$CU_SET CU with id $CU_NO (`cat $HOME/tmp/A-%02d.txt`)' % idx, ]
                 cudesc.cores       = 1
-            ##  cudesc.stdout      = 'B-stdout.txt'
-            ##  cudesc.stderr      = 'B-stderr.txt'
                 # -------- END USER DEFINED CU 2 DESCRIPTION --------- #
 
                 # Submit CU to Pilot Job
                 cu_b = umgr.submit_units (cudesc)
-                print "* Submitted 'B' cu '%s' with id '%s'" % (i, cu_b.uid)
+                print "---------------"
+                print "B CU '%s' '%s' submitted" % (i, cu_b.uid)
                 cu_set_B.append(cu_b)
                 cu_set_A.remove(cu_a)
 
         for cu_b in cu_set_B :
             cu_b.wait ()
+            print "---------------"
+            print "B CU '%s' finished." % (cu_b.uid)
+            print cu_b.stdout
 
         return(0)
 
