1b4629b49a011b67badf589ce053fe34405f5b5e
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2014-06-18 10:37:12 -0400

    Working coupled task example.
    Not sure what this pattern is supposed to communicate, because now its more of
    a barrier sync than coupling really.

--- examples/tutorial/coupled_tasks.py
+++ examples/tutorial/coupled_tasks.py
@@ -3,9 +3,8 @@ import os
 import sys
 import radical.pilot
 
-""" DESCRIPTION: Tutorial 3: Coupled Ensembles
-Note: User must edit PILOT SETUP and TASK DESCRIPTION 1-3 sections
-This example will not run if these values are not set.
+""" DESCRIPTION: Tutorial 3: Coupled Tasks
+For every task A1 and B1 a C1 is started.
 """
 
 # ---------------- BEGIN REQUIRED PILOT SETUP -----------------
@@ -17,19 +16,6 @@ if DBURL is None:
     print "ERROR: RADICAL_PILOT_DBURL (MongoDB server URL) is not defined."
     sys.exit(1)
 
-# resource information
-# Note: Set fields to "None" if not applicable
-HOSTNAME     = "india.futuregrid.org" # remote resource
-USERNAME     = "merzky"               # username on the remote resource
-QUEUE        =  None # add queue you want to use
-PROJECT      =  None # add project / allocation / account to charge
-WALLTIME     =    10 # add pilot wallsime in minutes
-PILOT_SIZE   =     1 # number of cores required for the Pilot-Job
-NUMBER_JOBS  =    10 # the total number of cus to run
-
-# Continue to USER DEFINED CU DESCRIPTION to add 
-# the required information about the individual cus.
-
 #------------------------------------------------------------------------------
 #
 def pilot_state_cb(pilot, state):
@@ -38,9 +24,11 @@ def pilot_state_cb(pilot, state):
     """
 
     if state == radical.pilot.states.FAILED:
-        print "[Callback]: Pilot '%s' state changed to %s." % (pilot.uid, state)
-        print "            Log: \n%s" % pilot.log
+        print "Compute Pilot '%s' failed, exiting ..." % pilot.uid
         sys.exit(1)
+
+    elif state == radical.pilot.states.ACTIVE:
+        print "Compute Pilot '%s' became active!" % (pilot.uid)
 
 
 #------------------------------------------------------------------------------
@@ -50,8 +38,7 @@ def unit_state_change_cb(unit, state):
     time a ComputeUnit changes its state.
     """
     if state == radical.pilot.states.FAILED:
-        print "[Callback]: CU '%s' state changed to '%s'." % (unit.uid, state)
-        print "            Log: \n%s" % unit.log
+        print "Compute Unit '%s' failed ..." % unit.uid
         sys.exit(1)
 
 
@@ -60,17 +47,17 @@ def unit_state_change_cb(unit, state):
 def main():
     try:
         # Create a new session. A session is the 'root' object for all other
-        # RADICAL-Pilot objects. It encapsualtes the MongoDB connection(s) as
-        # well as security crendetials.
+        # RADICAL-Pilot objects. It encapsulates the MongoDB connection(s) as
+        # well as security credentials.
         session = radical.pilot.Session(database_url=DBURL)
 
         # Add an ssh identity to the session.
         cred = radical.pilot.SSHCredential()
-        cred.user_id = USERNAME
+        #cred.user_id = 'osdcXX'
         session.add_credential(cred)
 
         # Add a Pilot Manager. Pilot managers manage one or more ComputePilots.
-        print "create pilot manager"
+        print "Initializing Pilot Manager ..."
         pmgr = radical.pilot.PilotManager(session=session)
 
         # Register our callback with the PilotManager. This callback will get
@@ -80,21 +67,18 @@ def main():
 
         # this describes the parameters and requirements for our pilot job
         pdesc = radical.pilot.ComputePilotDescription ()
-        pdesc.resource = HOSTNAME
-        pdesc.runtime  = WALLTIME
-        pdesc.queue    = QUEUE
-        pdesc.project  = PROJECT
-        pdesc.cores    = PILOT_SIZE
+        pdesc.resource = "fs2.das4.science.uva.nl" # NOTE: This is a "label", not a hostname
+        pdesc.runtime  = 5
+        pdesc.cores    = 1
         pdesc.cleanup  = True
 
         # submit the pilot.
-        print "submit pilot"
+        print "Submitting Compute Pilot to Pilot Manager ..."
         pilot = pmgr.submit_pilots(pdesc)
-
 
         # Combine the ComputePilot, the ComputeUnits and a scheduler via
         # a UnitManager object.
-        print "create unit manager"
+        print "Initializing Unit Manager ..."
         umgr = radical.pilot.UnitManager(
             session=session,
             scheduler=radical.pilot.SCHED_DIRECT_SUBMISSION)
@@ -105,93 +89,87 @@ def main():
         umgr.register_callback(unit_state_change_cb)
 
         # Add the previsouly created ComputePilot to the UnitManager.
-        print "add    pilot"
+        print "Registering Compute Pilot with Unit Manager ..."
         umgr.add_pilots(pilot)
 
+        NUMBER_JOBS  = 2 # the total number of CUs to chain
 
         # submit A cus to pilot job
-        cudesc_set_A = list ()
-        for i in range(NUMBER_JOBS):
+        cudesc_list_A = []
+        for idx in range(NUMBER_JOBS):
 
             # -------- BEGIN USER DEFINED CU 1 DESCRIPTION --------- #
             cudesc = radical.pilot.ComputeUnitDescription()
-            cudesc.environment = {"CU_SET": "A", "CU_NO": "%02d" % i}
+            cudesc.environment = {"CU_LIST": "A", "CU_NO": "%02d" % idx}
             cudesc.executable  = "/bin/echo"
-            cudesc.arguments   = ['"$CU_SET CU with id $CU_NO"', '>', '$HOME/tmp/A-$CU_NO.txt', ]
+            cudesc.arguments   = ['"$CU_LIST CU with id $CU_NO"']
             cudesc.cores       = 1
             # -------- END USER DEFINED CU 1 DESCRIPTION --------- #
 
-            cudesc_set_A.append(cudesc)
+            cudesc_list_A.append(cudesc)
 
         # Submit the previously created ComputeUnit descriptions to the
         # PilotManager. This will trigger the selected scheduler to start
         # assigning ComputeUnits to the ComputePilots.
-        print "submit units A"
-        cu_set_A = umgr.submit_units (cudesc_set_A)
-
-
+        print "Submit Compute Units 'A' to Unit Manager ..."
+        cu_set_A = umgr.submit_units(cudesc_list_A)
 
         # submit B cus to pilot job
-        cudesc_set_B = list ()
-        for i in range(NUMBER_JOBS):
+        cudesc_list_B = []
+        for idx in range(NUMBER_JOBS):
 
             # -------- BEGIN USER DEFINED CU 1 DESCRIPTION --------- #
             cudesc = radical.pilot.ComputeUnitDescription()
-            cudesc.environment = {"CU_SET": "B", "CU_NO": "%02d" % i}
+            cudesc.environment = {"CU_LIST": "B", "CU_NO": "%02d" % idx}
             cudesc.executable  = "/bin/echo"
-            cudesc.arguments   = ['"$CU_SET CU with id $CU_NO"', '>', '$HOME/tmp/B-$CU_NO.txt', ]
+            cudesc.arguments   = ['"$CU_LIST CU with id $CU_NO"']
             cudesc.cores       = 1
             # -------- END USER DEFINED CU 1 DESCRIPTION --------- #
 
-            cudesc_set_B.append(cudesc)
+            cudesc_list_B.append(cudesc)
 
         # Submit the previously created ComputeUnit descriptions to the
         # PilotManager. This will trigger the selected scheduler to start
         # assigning ComputeUnits to the ComputePilots.
-        print "submit units B"
-        cu_set_B = umgr.submit_units (cudesc_set_B)
+        print "Submit Compute Units 'B' to Unit Manager ..."
+        cu_set_B = umgr.submit_units(cudesc_list_B)
 
 
         # ---------------------------------------------------------------------
         print "Waiting for 'A' and 'B' CUs to complete..."
-        umgr.wait_units ()
-
+        umgr.wait_units()
 
         print "Executing 'C' tasks now..."
         # ---------------------------------------------------------------------
 
         # submit 'C' tasks to pilot job. each 'C' task takes the output of
         # an 'A' and a 'B' task and puts them together.
-        cudesc_set_C = list ()
-        for i in range(NUMBER_JOBS):
+        cudesc_list_C = []
+        for idx in range(NUMBER_JOBS):
 
             # -------- BEGIN USER DEFINED CU 1 DESCRIPTION --------- #
             cudesc = radical.pilot.ComputeUnitDescription()
-            cudesc.environment = {"CU_SET": "C", "CU_NO": "%02d" % i}
+            cudesc.environment = {"CU_SET": "C", "CU_NO": "%02d" % idx}
             cudesc.executable  = "/bin/echo"
-            cudesc.arguments   = ['"$CU_SET CU with id $CU_NO"', 
-                                  " (`cat $HOME/tmp/A-%02d.txt`)" % i, 
-                                  " (`cat $HOME/tmp/B-%02d.txt`)" % i]
+            cudesc.arguments   = ['"$CU_SET CU with id $CU_NO"']
             cudesc.cores       = 1
             # -------- END USER DEFINED CU 1 DESCRIPTION --------- #
 
-            cudesc_set_C.append(cudesc)
+            cudesc_list_C.append(cudesc)
 
         # Submit the previously created ComputeUnit descriptions to the
         # PilotManager. This will trigger the selected scheduler to start
         # assigning ComputeUnits to the ComputePilots.
-        print "submit units C"
-        cu_set_C = umgr.submit_units (cudesc_set_C)
-
-
+        print "Submit Compute Units 'C' to Unit Manager ..."
+        cu_set_C = umgr.submit_units(cudesc_list_C)
 
         # ---------------------------------------------------------------------
         print "Waiting for 'C' CUs to complete..."
-        for cu in cu_set_C :
-            cu.wait ()
-            print "---------------"
-            print "CU '%s' finished." % (cu.uid)
-            print cu.stdout
+        umgr.wait_units()
+        print "All CUs completed successfully!"
+
+        session.close()
+        print "Closed session, exiting now ..."
 
     except Exception as e:
             print "AN ERROR OCCURRED: %s" % ((str(e)))
@@ -206,4 +184,3 @@ if __name__ == "__main__":
 
 #
 #------------------------------------------------------------------------------
-
