d6ea482431f5e47f96c4647f78a35fa61ead2468
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2014-06-18 10:25:18 -0400

    Beefed up chained task example.
    This has become quite convoluted and non-optimal at the same time.
    
    One should really wrap this in a library.
    
    And call it MTMS or so ..
    
    Oh wait ...

--- examples/tutorial/chained_tasks.py
+++ examples/tutorial/chained_tasks.py
@@ -4,9 +4,8 @@ import sys
 import radical.pilot
 import traceback
 
-""" DESCRIPTION: Tutorial 2: Chaining CUs
-Note: User must edit PILOT SETUP and CU DESCRIPTION 1-2 sections
-This example will not run if these values are not set.
+""" DESCRIPTION: Tutorial 2: Chaining Tasks.
+For every task A_n a task B_n is started consecutively.
 """
 
 # ---------------- BEGIN REQUIRED PILOT SETUP -----------------
@@ -18,19 +17,6 @@ if DBURL is None:
     print "ERROR: RADICAL_PILOT_DBURL (MongoDB server URL) is not defined."
     sys.exit(1)
 
-# resource information
-# Note: Set fields to "None" if not applicable
-HOSTNAME     = "india.futuregrid.org" # remote resource
-USERNAME     = "merzky"               # username on the remote resource
-QUEUE        =  None # add queue you want to use
-PROJECT      =  None # add project / allocation / account to charge
-WALLTIME     =    10 # add pilot wallsime in minutes
-PILOT_SIZE   =     1 # number of cores required for the Pilot-Job
-NUMBER_JOBS  =    10 # the total number of cus to run
-
-# Continue to USER DEFINED CU DESCRIPTION to add 
-# the required information about the individual cus.
-
 #------------------------------------------------------------------------------
 #
 def pilot_state_cb(pilot, state):
@@ -39,9 +25,11 @@ def pilot_state_cb(pilot, state):
     """
 
     if state == radical.pilot.states.FAILED:
-        print "[Callback]: Pilot '%s' state changed to %s." % (pilot.uid, state)
-        print "            Log: \n%s" % pilot.log
+        print "Compute Pilot '%s' failed, exiting ..." % pilot.uid
         sys.exit(1)
+
+    elif state == radical.pilot.states.ACTIVE:
+        print "Compute Pilot '%s' became active!" % (pilot.uid)
 
 
 #------------------------------------------------------------------------------
@@ -50,9 +38,9 @@ def unit_state_change_cb(unit, state):
     """unit_state_change_cb() is a callback function. It gets called very
     time a ComputeUnit changes its state.
     """
+
     if state == radical.pilot.states.FAILED:
-        print "[Callback]: CU '%s' state changed to '%s'." % (unit.uid, state)
-        print "            Log: \n%s" % unit.log
+        print "Compute Unit '%s' failed ..." % unit.uid
         sys.exit(1)
 
 
@@ -62,17 +50,17 @@ def main():
 
     try:
         # Create a new session. A session is the 'root' object for all other
-        # RADICAL-Pilot objects. It encapsualtes the MongoDB connection(s) as
-        # well as security crendetials.
+        # RADICAL-Pilot objects. It encapsulates the MongoDB connection(s) as
+        # well as security credentials.
         session = radical.pilot.Session(database_url=DBURL)
 
         # Add an ssh identity to the session.
         cred = radical.pilot.SSHCredential()
-        cred.user_id = USERNAME
+        #cred.user_id = 'osdcXX'
         session.add_credential(cred)
 
         # Add a Pilot Manager. Pilot managers manage one or more ComputePilots.
-        print "create pilot manager"
+        print "Initializing Pilot Manager ..."
         pmgr = radical.pilot.PilotManager(session=session)
 
         # Register our callback with the PilotManager. This callback will get
@@ -82,20 +70,18 @@ def main():
 
         # this describes the parameters and requirements for our pilot job
         pdesc = radical.pilot.ComputePilotDescription ()
-        pdesc.resource = HOSTNAME
-        pdesc.runtime  = WALLTIME
-        pdesc.queue    = QUEUE
-        pdesc.project  = PROJECT
-        pdesc.cores    = PILOT_SIZE
+        pdesc.resource = 'fs2.das4.science.uva.nl'
+        pdesc.runtime  = 5 # minutes
+        pdesc.cores    = 1
         pdesc.cleanup  = True
 
         # submit the pilot.
-        print "submit pilot"
+        print "Submitting Compute Pilot to Pilot Manager ..."
         pilot = pmgr.submit_pilots(pdesc)
 
         # Combine the ComputePilot, the ComputeUnits and a scheduler via
         # a UnitManager object.
-        print "create unit manager"
+        print "Initializing Unit Manager ..."
         umgr = radical.pilot.UnitManager(
             session=session,
             scheduler=radical.pilot.SCHED_DIRECT_SUBMISSION)
@@ -105,61 +91,67 @@ def main():
         # change their state.
         umgr.register_callback(unit_state_change_cb)
 
-        # Add the previsouly created ComputePilot to the UnitManager.
-        print "add    pilot"
+        # Add the previously created ComputePilot to the UnitManager.
+        print "Registering Compute Pilot with Unit Manager ..."
         umgr.add_pilots(pilot)
 
+        NUMBER_JOBS  = 10 # the total number of cus to run
+
         # submit A cus to pilot job
-        cudesc_set_A = list ()
+        cudesc_list_A = []
         for i in range(NUMBER_JOBS):
 
-            # -------- BEGIN USER DEFINED CU 1 DESCRIPTION --------- #
+            # -------- BEGIN USER DEFINED COMPUTE UNIT A_n DESCRIPTION --------- #
             cudesc = radical.pilot.ComputeUnitDescription()
-            cudesc.environment = {"CU_SET": "A", "CU_NO": "%02d" % i}
+            cudesc.environment = {"CU_LIST": "A", "CU_NO": "%02d" % i}
             cudesc.executable  = "/bin/echo"
-            cudesc.arguments   = ['"$CU_SET CU with id $CU_NO"', '>', '$HOME/tmp/A-$CU_NO.txt', ]
+            cudesc.arguments   = ['"$CU_LIST CU with id $CU_NO"']
             cudesc.cores       = 1
-            # -------- END USER DEFINED CU 1 DESCRIPTION --------- #
+            # -------- END USER DEFINED COMPUTE UNIT A_n DESCRIPTION --------- #
 
-            cudesc_set_A.append(cudesc)
+            cudesc_list_A.append(cudesc)
 
         # Submit the previously created ComputeUnit descriptions to the
         # PilotManager. This will trigger the selected scheduler to start
         # assigning ComputeUnits to the ComputePilots.
-        print "submit units A"
-        cu_set_A = umgr.submit_units (cudesc_set_A)
+        print "Submit 'A' Compute Units to Unit Manager ..."
+        cu_list_A = umgr.submit_units (cudesc_list_A)
 
         # Chaining cus i.e submit a compute unit, when compute unit from A is successfully executed.
         # A B CU reads the content of the output file of an A CU and writes it into its own
         # output file.
-        cu_set_B = list()
-        while len(cu_set_A) > 0 :
-            for idx,cu_a in enumerate (cu_set_A) :
-                cu_a.wait ()
-                print "---------------"
-                print "A CU '%s' finished. Submitting B CU." % (cu_a.uid)
+        cu_list_B = []
+        cu_list_A_copy = cu_list_A[:]
+        while cu_list_A:
+            for cu_a in cu_list_A:
+                idx = cu_list_A_copy.index(cu_a)
 
-                # -------- BEGIN USER DEFINED CU 2 DESCRIPTION --------- #
+                cu_a.wait ()
+                print "'A' Compute Unit '%s' finished. Submitting 'B' CU ..." % idx
+
+                # -------- BEGIN USER DEFINED COMPUTE UNIT B_n DESCRIPTION --------- #
                 cudesc = radical.pilot.ComputeUnitDescription()
-                cudesc.environment = {'CU_SET': 'B', 'CU_NO': "%02d" % idx}
+                cudesc.environment = {'CU_LIST': 'B', 'CU_NO': "%02d" % idx}
                 cudesc.executable  = '/bin/echo'
-                cudesc.arguments   = ['$CU_SET CU with id $CU_NO (`cat $HOME/tmp/A-%02d.txt`)' % idx, ]
+                cudesc.arguments   = ['"$CU_LIST CU with id %s"' % idx]
                 cudesc.cores       = 1
-                # -------- END USER DEFINED CU 2 DESCRIPTION --------- #
+                # -------- END USER DEFINED COMPUTE UNIT B_n DESCRIPTION --------- #
 
                 # Submit CU to Pilot Job
                 cu_b = umgr.submit_units (cudesc)
-                print "---------------"
-                print "B CU '%s' '%s' submitted" % (i, cu_b.uid)
-                cu_set_B.append(cu_b)
-                cu_set_A.remove(cu_a)
+                cu_list_B.append(cu_b)
+                cu_list_A.remove(cu_a)
 
-        for cu_b in cu_set_B :
+        print "Waiting for 'B' Compute Units to complete ..."
+        for cu_b in cu_list_B :
             cu_b.wait ()
-            print "---------------"
-            print "B CU '%s' finished." % (cu_b.uid)
+            print "'B' Compute Unit '%s' finished with output:" % (cu_b.uid)
             print cu_b.stdout
 
+        print "All Compute Units completed successfully!"
+
+        session.close(delete=False)
+        print "Closed session, exiting now ..."
 
     except Exception as e:
             print "AN ERROR OCCURRED: %s" % ((str(e)))
