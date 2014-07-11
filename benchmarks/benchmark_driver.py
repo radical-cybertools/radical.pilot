2060553462ec2d3d6b2740dd0a46e6feaf5e93f8
Andre Merzky <andre@merzky.net>
2014-07-11 23:01:28 +0000

    add some scripts...

---
+++ benchmarks/benchmark_driver.py
@@ -0,0 +1,176 @@
+import os
+import sys
+import time
+import radical.pilot
+
+# READ: The RADICAL-Pilot documentation: 
+#   http://radicalpilot.readthedocs.org/en/latest
+#
+# Try running this example with RADICAL_PILOT_VERBOSE=debug set if 
+# you want to see what happens behind the scences!
+#
+
+
+# DBURL defines the MongoDB server URL and has the format mongodb://host:port.
+# For the installation of a MongoDB server, refer to http://docs.mongodb.org.
+DBURL = os.getenv("RADICAL_PILOT_DBURL")
+if DBURL is None:
+    print "ERROR: RADICAL_PILOT_DBURL (MongoDB server URL) is not defined."
+    sys.exit(1)
+
+
+#------------------------------------------------------------------------------
+#
+def pilot_state_cb(pilot, state):
+    """pilot_state_change_cb() is a callback function. It gets called very
+    time a ComputePilot changes its state.
+    """
+    print "[Callback]: ComputePilot '{0}' state changed to {1}.".format(
+        pilot.uid, state)
+
+    if state == radical.pilot.FAILED:
+        print "exit"
+        sys.exit(0)
+
+#------------------------------------------------------------------------------
+#
+def unit_state_change_cb(unit, state):
+    """unit_state_change_cb() is a callback function. It gets called very
+    time a ComputeUnit changes its state.
+    """
+    print "[Callback]: ComputeUnit '{0}' state changed to {1}.".format(
+        unit.uid, state)
+    if state == radical.pilot.FAILED:
+        print "            Log: %s" % unit.log
+
+#------------------------------------------------------------------------------
+#
+try:
+    rp_user     = str(os.getenv ("RP_USER",     "merzky"))
+    rp_cores    = int(os.getenv ("RP_CORES",    4))
+    rp_cu_cores = int(os.getenv ("RP_CU_CORES", 1))
+    rp_units    = int(os.getenv ("RP_UNITS",    10))
+    rp_runtime  = int(os.getenv ("RP_RUNTIME",  10))
+    rp_host     = str(os.getenv ("RP_HOST",     "localhost"))
+    rp_queue    = str(os.getenv ("RP_QUEUE",    ""))
+    rp_project  = str(os.getenv ("RP_PROJECT",  ""))
+
+    # Create a new session. A session is the 'root' object for all other
+    # RADICAL-Pilot objects. It encapsualtes the MongoDB connection(s) as
+    # well as security crendetials.
+    session = radical.pilot.Session(database_url=DBURL)
+    print "session: %s" % session.uid
+
+    # make jenkins happy
+    c         = radical.pilot.Context ('ssh')
+    c.user_id = rp_user
+    session.add_context (c)
+
+    # Add a Pilot Manager. Pilot managers manage one or more ComputePilots.
+    pmgr = radical.pilot.PilotManager(session=session)
+
+    # Register our callback with the PilotManager. This callback will get
+    # called every time any of the pilots managed by the PilotManager
+    # change their state.
+    pmgr.register_callback(pilot_state_cb)
+
+    # Define 1-core local pilots that run for 10 minutes and clean up
+    # after themself.
+    pdescriptions = list()
+    for i in range (0, 1) :
+        pdesc = radical.pilot.ComputePilotDescription()
+        pdesc.resource = rp_host
+        pdesc.runtime  = rp_runtime
+        pdesc.cores    = rp_cores
+        pdesc.cleanup  = False
+        if rp_queue   : pdesc.queue    = rp_queue
+        if rp_project : pdesc.project  = rp_project
+
+        pdescriptions.append(pdesc)
+
+        import pprint
+        pprint.pprint (pdesc)
+
+
+    # Launch the pilots.
+    pilots = pmgr.submit_pilots (pdescriptions)
+    print "pilots: %s" % pilots
+
+    pmgr.wait_pilots (state=radical.pilot.ACTIVE)
+
+
+
+    # Create a workload of 8 ComputeUnits (tasks). Each compute unit
+    # uses /bin/cat to concatenate two input files, file1.dat and
+    # file2.dat. The output is written to STDOUT. cu.environment is
+    # used to demonstrate how to set environment variables withih a
+    # ComputeUnit - it's not strictly necessary for this example. As
+    # a shell script, the ComputeUnits would look something like this:
+    #
+    #    export INPUT1=file1.dat
+    #    export INPUT2=file2.dat
+    #    /bin/cat $INPUT1 $INPUT2
+    #
+    cu_descriptions = []
+
+    for unit_count in range(0, rp_units):
+        cu = radical.pilot.ComputeUnitDescription()
+        cu.executable  = "/bin/sleep"
+        cu.arguments   = [str(int(rp_cores/32)+10)]
+        cu.cores       = 32
+        cu.mpi         = True
+
+        import pprint
+        pprint.pprint (cu)
+
+        cu_descriptions.append(cu)
+
+    # Combine the ComputePilot, the ComputeUnits and a scheduler via
+    # a UnitManager object.
+    umgr = radical.pilot.UnitManager(
+        session=session,
+        scheduler=radical.pilot.SCHED_ROUND_ROBIN)
+
+    # Register our callback with the UnitManager. This callback will get
+    # called every time any of the units managed by the UnitManager
+    # change their state.
+    umgr.register_callback(unit_state_change_cb)
+
+    # Add the previsouly created ComputePilots to the UnitManager.
+    umgr.add_pilots(pilots)
+
+    # Submit the previously created ComputeUnit descriptions to the
+    # PilotManager. This will trigger the selected scheduler to start
+    # assigning ComputeUnits to the ComputePilots.
+    units = umgr.submit_units(cu_descriptions)
+    print "units: %s" % umgr.list_units ()
+
+    # Wait for all compute units to reach a terminal state (DONE or FAILED).
+    umgr.wait_units()
+
+    if not isinstance (units, list) :
+        units=[units]
+    
+    for unit in units:
+        print "* Task %s (executed @ %s) state %s, exit code: %s, started: %s, finished: %s" \
+            % (unit.uid, unit.execution_locations, unit.state, unit.exit_code, unit.start_time, unit.stop_time)
+
+    # Close automatically cancels the pilot(s).
+    pmgr.cancel_pilots ()
+    time.sleep (3)
+  
+    sid = session.uid
+    # session.close(delete=False)
+
+    print "session id: %s" % sid
+
+    # run the stats plotter
+    os.system ("bin/radicalpilot-stats -m plot -s %s" % sid) 
+    os.system ("cp -v %s.png report/rp.benchmark.png" % sid) 
+
+
+except radical.pilot.PilotException, ex:
+    # Catch all exceptions and exit with and error.
+    print "Error during execution: %s" % ex
+    sys.exit(1)
+
---
+++ benchmarks/run_benchmark_series.sh
@@ -0,0 +1,154 @@
+__author__    = "RADICAL Team"
+__copyright__ = "Copyright 2013, RADICAL Research, Rutgers University"
+__license__   = "MIT"
+
+
+""" Setup script. Used by easy_install and pip. """
+
+import os
+import sys
+import subprocess as sp
+
+from setuptools import setup, find_packages, Command
+
+name     = 'radical.pilot'
+mod_root = 'src/radical/pilot/'
+
+#-----------------------------------------------------------------------------
+#
+# versioning mechanism:
+#
+#   - version:          1.2.3            - is used for installation
+#   - version_detail:  v1.2.3-9-g0684b06 - is used for debugging
+#   - version is read from VERSION file in src root, which is on installation
+#     copied into the module dir.
+#   - version_detail is derived from the git tag, and only available when
+#     installed from git -- this is stored in VERSION.git, in the same
+#     locations, on install.
+#   - both files, VERSION and VERSION.git are used to provide the runtime
+#     version information.
+#
+def get_version (mod_root):
+    """
+    mod_root
+        a VERSION and VERSION.git file containing the version strings is created
+        in mod_root, during installation.  Those files are used at runtime to
+        get the version information.
+
+    """
+
+    try:
+
+        version        = None
+        version_detail = None
+
+        # get version from './VERSION'
+        src_root = os.path.dirname (__file__)
+        if  not src_root :
+            src_root = '.'
+
+        with open (src_root + "/VERSION", "r") as f :
+            version = f.readline ().strip()
+
+
+        # attempt to get version detail information from git
+        p   = sp.Popen ('cd %s ; '\
+                        'tag=`git describe --tags --always` ; '\
+                        'branch=`git branch | grep -e "^*" | cut -f 2 -d " "` ; '\
+                        'echo $tag@$branch'  % src_root,
+                        stdout=sp.PIPE, stderr=sp.STDOUT, shell=True)
+        version_detail = p.communicate()[0].strip()
+
+        if  p.returncode   !=  0  or \
+            version_detail == '@' or \
+            'fatal'        in version_detail :
+            version_detail =  "v%s" % version
+
+        print 'version: %s (%s)'  % (version, version_detail)
+
+
+        # make sure the version files exist for the runtime version inspection
+        path = "%s/%s" % (src_root, mod_root)
+        print 'creating %s/VERSION' % path
+
+        with open (path + "/VERSION",     "w") as f : f.write (version        + "\n")
+        with open (path + "/VERSION.git", "w") as f : f.write (version_detail + "\n")
+
+        return version, version_detail
+
+    except Exception as e :
+        raise RuntimeError ("Could not extract/set version: %s" % e)
+
+
+#-----------------------------------------------------------------------------
+# get version info -- this will create VERSION and srcroot/VERSION
+version, version_detail = get_version (mod_root)
+
+
+#-----------------------------------------------------------------------------
+# check python version. we need > 2.6, <3.x
+if  sys.hexversion < 0x02060000 or sys.hexversion >= 0x03000000:
+    raise RuntimeError("%s requires Python 2.x (2.6 or higher)" % name)
+
+
+#-----------------------------------------------------------------------------
+#
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+#-----------------------------------------------------------------------------
+setup_args = {
+    'name'             : name,
+    'version'          : version,
+    'description'      : "The RADICAL pilot job framework",
+    'long_description' : (read('README.md') + '\n\n' + read('CHANGES.md')),
+    'author'           : 'RADICAL Group at Rutgers University',
+    'author_email'     : "radical@rutgers.edu",
+    'maintainer'       : "Ole Weidner",
+    'maintainer_email' : "ole.weidner@rutgers.edu",
+    'url'              : "https://github.com/radical-cybertools/radical.pilot",
+    'license'          : "MIT",
+    'keywords'         : "radical pilot job saga",
+    'classifiers'      : [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+        'Topic :: System :: Distributed Computing',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: Unix'
+    ],
+
+    'namespace_packages': ['radical'],
+    'packages'    : find_packages('src'),
+    'package_dir' : {'': 'src'},
+    'scripts'          : ['bin/radicalpilot-version',
+                          'bin/radicalpilot-profiler',
+                          'bin/radicalpilot-stats',
+                          'bin/radicalpilot-stats.plot'
+                         ],
+    'package_data'     : {'': ['*.sh', '*.json', 'VERSION', 'VERSION.git']},
+
+    'install_requires' : ['setuptools',
+                          'saga-python',
+                          'radical.utils',
+                          'pymongo>=2.5',
+                          'python-hostlist'],
+    'tests_require'    : ['setuptools', 'nose'],
+    'test_suite'       : 'radical.pilot.tests',
+    'zip_safe'         : False,
+}
+
+#-----------------------------------------------------------------------------
+
+setup (**setup_args)
+
+#-----------------------------------------------------------------------------
