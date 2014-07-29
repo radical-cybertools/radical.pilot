52b13fe96d75e33e391b12d5dc678e705f718aee
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2014-07-30 00:29:28 +0200

    Staging directives are a dict now.

--- src/radical/pilot/staging_directives.py
+++ src/radical/pilot/staging_directives.py
@@ -1,3 +1,10 @@
+# staging_directive = {
+#     'source':   None, # radical.pilot.Url() or string
+#     'target':   None, # radical.pilot.Url() or string
+#     'action':   None, # COPY, LINK, MOVE, TRANSFER
+#     'flags':    None, # SKIP_FAILED, CREATE_PARENTS
+#     'priority': 0    # Control ordering of actions, mainly interesting for output probably
+# }
 
 #
 # Action operators
@@ -12,63 +19,3 @@ TRANSFER = 'Transfer' # saga remote transfer TODO: This might just be a special 
 #
 SKIP_FAILED    = 'SkipFailed'     # Don't stage out files if tasks failed
 CREATE_PARENTS = 'CreateParents'  # Create parent directories while writing to staging area
-
-
-# TODO: Consider migrating to dict?
-class StagingDirectives(dict):
-
-    def __init__(self):
-
-        # TODO: Convert to attrib iface
-        self.source   = None # radical.pilot.Url() or string
-        self.target   = None # radical.pilot.Url() or string
-        self.action   = None # COPY, LINK, MOVE, TRANSFER
-        self.flags    = None # SKIP_FAILED, CREATE_PARENTS
-        self.priority = 0    # Control ordering of actions, mainly interesting for output probably
-
-    #------------------------------------------------------------------------------
-    #
-    def as_dict(self):
-        """Returns a Python dictionary representation of the object.
-        """
-        obj_dict = {
-            'source'   : self.source,
-            'target'   : self.target,
-            'action'   : self.action,
-            'flags'    : self.flags,
-            'priority' : self.priority,
-        }
-        return obj_dict
-
-
-    #------------------------------------------------------------------------------
-    #
-    def __str__(self):
-        """Returns a string representation of the object.
-        """
-        return str(self.as_dict())
-
-
-    #------------------------------------------------------------------------------
-    #
-    def __repr__ (self):
-        """Returns a string representation of the object.
-        """
-        return str(self)
-
-
-    #------------------------------------------------------------------------------
-    #
-    def __deepcopy__ (self, memo):
-
-        other = StagingDirectives ()
-
-        other.source   = self.source   
-        other.target   = self.target   
-        other.action   = self.action   
-        other.flags    = self.flags    
-        other.priority = self.priority 
-
-        return other
-
-
