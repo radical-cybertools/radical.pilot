7dec876b56b6609c92cea882bf85138d8e3fa785
Andre Merzky <andre@merzky.net>
2014-08-12 00:21:23 +0200

    more fixes on tests, remove some tests which don"t test anything and are covered by examples already

--- tests/session_ctx.py
+++ tests/session_ctx.py
@@ -1,34 +1,35 @@
 
 import radical.pilot as rp
 
-session = rp.Session()
-print "Session 1: %s " % session.uid
+s1 = rp.Session()
+print "Session 1: %s (%d)" % (s1.uid, len(s1.list_contexts()))
 
 # Add an ssh identity to the session.
 c1 = rp.Context('ssh')
 c1.user_id = "tg802352"
 
-print c1
+print 'context 1: %s' % c1
 
-session.add_context(c1)
+s1.add_context(c1)
 
 # Add an ssh identity to the session.
 c2 = rp.Context('ssh')
 c2.user_id = "abcedesds"
 
-print c2
+print 'context 2: %s' % c2
 
-session.add_context(c2)
+s1.add_context(c2)
 
-for c in session.list_contexts():
+for c in s1.list_contexts():
     print c
 
 
-session2 = rp.Session(session_uid=session.uid)
-print "Session 2: %s " % session2.uid
+s2 = rp.Session(session_uid=s1.uid)
+print "Session 2: %s (%d)" % (s2.uid, len(s2.list_contexts()))
 
-for c in session2.list_contexts():
-    # contexts are *not* serialized as part of the session information, but need
-    # to be added again!
+
+for c in s2.list_contexts():
     print c
+
+assert (len(s1.list_contexts()) == len(s2.list_contexts()))
 
