859087bfaaeb212477f45014f7606af1a8cf1f92
Andre Merzky <andre@merzky.net>
2014-08-21 22:27:02 +0200

    re-include version details

--- src/radical/pilot/__init__.py
+++ src/radical/pilot/__init__.py
@@ -40,5 +40,5 @@ from staging_directives import COPY, LINK, MOVE, TRANSFER, SKIP_FAILED, CREATE_P
 
 # ------------------------------------------------------------------------------
 #
-from utils.version             import version
+from utils.version             import version, version_detail
 from utils.logger              import logger
