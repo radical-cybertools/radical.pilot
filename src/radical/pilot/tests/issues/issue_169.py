909812f40b3bf0cbfa43b816f4f2d2f1a7c9475f
Andre Merzky <andre@merzky.net>
2014-09-01 18:47:11 +0200

    make tests more verbose

--- src/radical/pilot/tests/issues/issue_169.py
+++ src/radical/pilot/tests/issues/issue_169.py
@@ -98,9 +98,9 @@ class TestIssue169(unittest.TestCase):
         pmgr.wait_pilots(timeout=5*60)
         
         for pilot in pilots:
-            assert pilot.state == radical.pilot.DONE
-            assert pilot.stop_time is not None
-            assert pilot.start_time is not None
+            assert pilot.state == radical.pilot.DONE, "state: %s", pilot.state
+            assert pilot.stop_time  is not None,      "time : %s", pilot.stop_time
+            assert pilot.start_time is not None,      "time : %s", pilot.start_time
 
         session.close()
 
