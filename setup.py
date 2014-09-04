de84d849b57af24808df181d8b94b616d19b0190
Andre Merzky <andre@merzky.net>
2014-09-05 00:09:46 +0200

    fix install for changed tool name

--- setup.py
+++ setup.py
@@ -131,7 +131,7 @@ setup_args = {
     'packages'    : find_packages('src'),
     'package_dir' : {'': 'src'},
     'scripts'          : ['bin/radicalpilot-version',
-                          'bin/radicalpilot-clean',
+                          'bin/radicalpilot-close-session',
                           'bin/radicalpilot-cleanup',
                           'bin/radicalpilot-stats',
                           'bin/radicalpilot-stats.plot'
