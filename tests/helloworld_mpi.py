5c1a404353a0cdcd49610a21d7d19b79898ac7e3
Andre Merzky <andre@merzky.net>
2014-09-11 11:42:11 +0200

    make mpi example a little more verbose

--- tests/helloworld_mpi.py
+++ tests/helloworld_mpi.py
@@ -3,16 +3,29 @@
 # This is an example MPI4Py program that is used
 # by different examples and tests.
 
-from   mpi4py import MPI
+import sys
 import time
+import traceback
+from   mpi4py import MPI
 
-SLEEP = 10
-name  = MPI.Get_processor_name()
-comm  = MPI.COMM_WORLD
+try :
+    print "start"
+    SLEEP = 10
+    name  = MPI.Get_processor_name()
+    comm  = MPI.COMM_WORLD
 
-print "mpi rank %d/%d/%s"  % (comm.rank, comm.size, name)
+    print "mpi rank %d/%d/%s"  % (comm.rank, comm.size, name)
 
-time.sleep(SLEEP)
+    time.sleep(SLEEP)
 
-comm.Barrier()   # wait for everybody to synchronize here
+    comm.Barrier()   # wait for everybody to synchronize here
+
+except Exception as e :
+    traceback.print_exc ()
+    print "error : %s" % s
+    sys.exit (1)
+
+finally :
+    print "done"
+    sys.exit (0)
 
