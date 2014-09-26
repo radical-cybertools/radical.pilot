40f11914a721e3dc5a8186a17727a43af0bf82e1
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2014-09-26 23:44:41 +0200

    Resolve merge conflict.

--- examples/getting_started_remote.py
+++ examples/getting_started_remote.py
@@ -36,96 +36,6 @@ def unit_state_cb (unit, state) :
 #
 if __name__ == "__main__":
 
-<<<<<<< HEAD
-    try:
-        # Create a new session. A session is the 'root' object for all other
-        # RADICAL-Pilot objects. It encapsualtes the MongoDB connection(s) as
-        # well as security crendetials.
-        session = radical.pilot.Session(database_url=DBURL)
-
-        # Add an ssh identity to the session.
-        c = radical.pilot.Context('ssh')
-        c.user_id = "merzky"
-        session.add_context(c)
-
-        # Add a Pilot Manager. Pilot managers manage one or more ComputePilots.
-        pmgr = radical.pilot.PilotManager(session=session)
-
-        # Register our callback with the PilotManager. This callback will get
-        # called every time any of the pilots managed by the PilotManager
-        # change their state.
-        pmgr.register_callback(pilot_state_cb)
-
-        # Define a 32-core on stamped that runs for 15 mintutes and 
-        # uses $HOME/radical.pilot.sandbox as sandbox directoy. 
-        pdesc = radical.pilot.ComputePilotDescription()
-        pdesc.resource  = "localhost"
-        pdesc.runtime   = 15 # minutes
-        pdesc.cores     = 4
-        pdesc.cleanup   = True
-
-        # Launch the pilot.
-        pilot = pmgr.submit_pilots(pdesc)
-
-        # Create a workload of 8 ComputeUnits (tasks). Each compute unit
-        # uses /bin/cat to concatenate two input files, file1.dat and
-        # file2.dat. The output is written to STDOUT. cu.environment is
-        # used to demonstrate how to set environment variables withih a
-        # ComputeUnit - it's not strictly necessary for this example. As
-        # a shell script, the ComputeUnits would look something like this:
-        #
-        #    export INPUT1=file1.dat
-        #    export INPUT2=file2.dat
-        #    /bin/cat $INPUT1 $INPUT2
-        #
-        compute_units = []
-
-        for unit_count in range(0, 16):
-            cu = radical.pilot.ComputeUnitDescription()
-            cu.environment = {"INPUT1": "file1.dat", "INPUT2": "file2.dat"}
-            cu.executable  = "/bin/cat"
-            cu.arguments   = ["$INPUT1", "$INPUT2"]
-            cu.cores       = 1
-            cu.input_data  = ["./file1.dat", "./file2.dat"]
-
-            compute_units.append(cu)
-
-        # Combine the ComputePilot, the ComputeUnits and a scheduler via
-        # a UnitManager object.
-        umgr = radical.pilot.UnitManager(
-            session=session,
-            scheduler=radical.pilot.SCHED_DIRECT_SUBMISSION)
-
-        # Register our callback with the UnitManager. This callback will get
-        # called every time any of the units managed by the UnitManager
-        # change their state.
-        umgr.register_callback(unit_state_change_cb)
-
-        # Add the previsouly created ComputePilot to the UnitManager.
-        umgr.add_pilots(pilot)
-
-        # Submit the previously created ComputeUnit descriptions to the
-        # PilotManager. This will trigger the selected scheduler to start
-        # assigning ComputeUnits to the ComputePilots.
-        units = umgr.submit_units(compute_units)
-
-        # Wait for all compute units to reach a terminal state (DONE or FAILED).
-        umgr.wait_units()
-
-        for unit in units:
-            print "* Task %s (executed @ %s) state: %s, exit code: %s, started: %s, finished: %s, output: %s" \
-                % (unit.uid, unit.execution_locations, unit.state, unit.exit_code, unit.start_time, unit.stop_time,
-                   unit.stdout)
-
-        # Close automatically cancels the pilot(s).
-        session.close(delete=False)
-        sys.exit(0)
-
-    except radical.pilot.PilotException, ex:
-        # Catch all exceptions and exit with and error.
-        print "Error during execution: %s" % ex
-        sys.exit(1)
-=======
     # prepare some input files for the compute units
     os.system ('hostname > file1.dat')
     os.system ('date     > file2.dat')
@@ -223,5 +133,4 @@ if __name__ == "__main__":
     os.system ('rm file2.dat')
 
 # ------------------------------------------------------------------------------
->>>>>>> devel
 
