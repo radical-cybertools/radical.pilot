b06bc33dd59a76c051707b18c09f23668074c003
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2014-12-12 14:00:42 +0100

    Backport of a9cc8dad64ba01ab0a37f65227b2e63a4b343a80 which addresses #455.

--- src/radical/pilot/agent/radical-pilot-agent-multicore.py
+++ src/radical/pilot/agent/radical-pilot-agent-multicore.py
@@ -676,7 +676,7 @@ class ExecutionEnvironment(object):
         slurm_nodes = hostlist.expand_hostlist(slurm_nodelist)
         self.log.info("Found SLURM_NODELIST %s. Expanded to: %s" % (slurm_nodelist, slurm_nodes))
 
-        # $SLURM_NPROCS = Total number of processes in the current job
+        # $SLURM_NPROCS = Total number of cores allocated for the current job
         slurm_nprocs_str = os.environ.get('SLURM_NPROCS')
         if slurm_nprocs_str is None:
             msg = "$SLURM_NPROCS not set!"
@@ -685,7 +685,7 @@ class ExecutionEnvironment(object):
         else:
             slurm_nprocs = int(slurm_nprocs_str)
 
-        # $SLURM_NNODES = Total number of nodes in the job's resource allocation
+        # $SLURM_NNODES = Total number of (partial) nodes in the job's resource allocation
         slurm_nnodes_str = os.environ.get('SLURM_NNODES')
         if slurm_nnodes_str is None:
             msg = "$SLURM_NNODES not set!"
@@ -694,26 +694,29 @@ class ExecutionEnvironment(object):
         else:
             slurm_nnodes = int(slurm_nnodes_str)
 
-        # $SLURM_CPUS_ON_NODE = Count of processors available to the job on this node.
+        # $SLURM_CPUS_ON_NODE = Number of cores per node (physically)
         slurm_cpus_on_node_str = os.environ.get('SLURM_CPUS_ON_NODE')
         if slurm_cpus_on_node_str is None:
-            msg = "$SLURM_NNODES not set!"
+            msg = "$SLURM_CPUS_ON_NODE not set!"
             self.log.error(msg)
             raise Exception(msg)
         else:
             slurm_cpus_on_node = int(slurm_cpus_on_node_str)
 
-        # Verify that $SLURM_NPROCS == $SLURM_NNODES * $SLURM_CPUS_ON_NODE
-        if slurm_nnodes * slurm_cpus_on_node != slurm_nprocs:
-            self.log.error("$SLURM_NPROCS(%d) != $SLURM_NNODES(%d) * $SLURM_CPUS_ON_NODE(%d)" % \
-                           (slurm_nnodes, slurm_cpus_on_node, slurm_nprocs))
+        # Verify that $SLURM_NPROCS <= $SLURM_NNODES * $SLURM_CPUS_ON_NODE
+        if not slurm_nprocs <= slurm_nnodes * slurm_cpus_on_node:
+            self.log.warning("$SLURM_NPROCS(%d) <= $SLURM_NNODES(%d) * $SLURM_CPUS_ON_NODE(%d)" % \
+                             (slurm_nprocs, slurm_nnodes, slurm_cpus_on_node))
 
         # Verify that $SLURM_NNODES == len($SLURM_NODELIST)
         if slurm_nnodes != len(slurm_nodes):
             self.log.error("$SLURM_NNODES(%d) != len($SLURM_NODELIST)(%d)" % \
                            (slurm_nnodes, len(slurm_nodes)))
 
-        self.cores_per_node = slurm_cpus_on_node
+        # Report the physical number of cores or the total number of cores
+        # in case of a single partial node allocation.
+        self.cores_per_node = min(slurm_cpus_on_node, slurm_nprocs)
+
         self.node_list = slurm_nodes
 
     #-------------------------------------------------------------------------
