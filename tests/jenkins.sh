a408717921bf9ac9c95f2419a9b69b452e746445
Andre Merzky <andre@merzky.net>
2015-04-08 15:33:22 +0200

    iteration on testing

--- tests/jenkins.sh
+++ tests/jenkins.sh
@@ -1,83 +1,71 @@
+#!/bin/sh
+
+# JENKINS_VERBOSE 
+#     TRUE : print logs and stdio while running the tests
+#     else : print logs and stdio only on failure, after running the test
+#
+# JENKINS_EXIT_ON_FAIL:
+#     TRUE : exit immediately when a test fails
+#     else : run all tests, independent of success/failure (exit code reflects
+#            failures though)I
+
+
 
 failed=0
-tests=`cat jenkins.cfg | sed -e 's/#.*//g' | grep -v '^ *$'  | grep integration | cut -f 1 -d :`
 
-for t in $tests
+SUCCESS_MARKER="JENKINS TEST SUCCESS"
+
+for s in integration mpi
 do
-    echo "# -----------------------------------------------------"
-    echo "# INTEGRATION TEST $T"
-    echo "# "
-    log_tgt="./rp.test_mpi.$t.log"
-    out_tgt="./rp.test_mpi.$t.out"
-    export RADICAL_LOG_TARGETS="stdout,$log_tgt"
-    export RADICAL_UTILS_LOG_TARGETS="stdout,$log_tgt"
-    export SAGA_LOG_TARGETS="stdout,$log_tgt"
-    export RADICAL_PILOT_LOG_TARGETS="stdout,$log_tgt"
-    ./test_integration.py "$t" 2>&1 > "$out_tgt"
-    if test "$?" = 0
-    then
-        echo
-        echo "# "
-        echo "# INTEGRATION SUCCESS $t"
+    tests=`cat jenkins.cfg | sed -e 's/#.*//g' | grep -v '^ *$'  | grep "$s" | cut -f 1 -d :`
+    for t in $tests
+    do
         echo "# -----------------------------------------------------"
+        echo "# TEST: $s $t"
+        echo "# "
+
+        log_tgt="./rp.test_$s_$t.log"
+
         if test "$JENKINS_VERBOSE" = "TRUE"
         then
-            cat "$log_tgt"
-            echo "# -----------------------------------------------------"
-            cat "$out_tgt"
-            echo "# -----------------------------------------------------"
+            progress='print'
+        else
+            progress='printf "."'
         fi
-    else
-        echo
-        echo "# "
-        echo "# INTEGRATION FAILED $t"
-        echo "# -----------------------------------------------------"
-        cat "$log_tgt"
-        echo "# -----------------------------------------------------"
-        cat "$out_tgt"
-        echo "# -----------------------------------------------------"
-        failed=1
-    fi
-done
 
+        export SAGA_VERBOSE=DEBUG
+        export RADICAL_VERBOSE=DEBUG
+        export RADICAL_UTILS_VERBOSE=DEBUG
+        export RADICAL_PILOT_VERBOSE=DEBUG
 
-tests=`cat jenkins.cfg | sed -e 's/#.*//g' | grep -v '^ *$'  | grep mpi | cut -f 1 -d :`
-for t in $tests
-do
-    echo "# -----------------------------------------------------"
-    echo "# MPI TEST $t"
-    echo "# "
-    log_tgt="./rp.test_mpi.$t.log"
-    out_tgt="./rp.test_mpi.$t.out"
-    export RADICAL_LOG_TARGETS="stdout,$log_tgt"
-    export RADICAL_UTILS_LOG_TARGETS="stdout,$log_tgt"
-    export SAGA_LOG_TARGETS="stdout,$log_tgt"
-    export RADICAL_PILOT_LOG_TARGETS="stdout,$log_tgt"
-    ./test_mpi.py "$t" 2>&1 > "$out_tgt"
-    if test "$?" = 0
-    then
-        echo
-        echo "# "
-        echo "# MPI SUCCESS $t"
-        echo "# -----------------------------------------------------"
-        if test "$JENKINS_VERBOSE" = "TRUE"
+        ( set -e ; "./test_$s.py" "$t" ; echo "$SUCCESS_MARKER") 2>&1 \
+        | tee "$log_tgt" | awk "{$progress}"
+        
+        if grep "$SUCCESS_MARKER" "$log_tgt"
         then
-            cat "$log_tgt"
+            echo
+            echo "# "
+            echo "# SUCCESS $s $t"
             echo "# -----------------------------------------------------"
-            cat "$out_tgt"
+        else
+            echo
+            echo "# "
+            echo "# FAILED $s $t"
             echo "# -----------------------------------------------------"
+
+            if ! test "$JENKINS_VERBOSE" = "TRUE"
+            then
+                cat "$log_tgt"
+                echo "# -----------------------------------------------------"
+            fi
+
+            if test "$JENKINS_EXIT_ON_FAIL" = "TRUE"
+            then
+                exit 1
+            fi
+            failed=1
         fi
-    else
-        echo
-        echo "# "
-        echo "# MPI FAILED $t"
-        echo "# -----------------------------------------------------"
-        cat "$log_tgt"
-        echo "# -----------------------------------------------------"
-        cat "$out_tgt"
-        echo "# -----------------------------------------------------"
-        failed=1
-    fi
+    done
 done
 
 exit $failed
