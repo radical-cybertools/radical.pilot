2c2b583095e055a6a726adb2eb5349b461a8fe7a
Andre Merzky <andre@merzky.net>
2015-07-11 20:21:35 +0200

    clean up branch -- performance opts
    this prepares the feature/plot branch for merging.  The main changes are
    
      - addition of profile data frames
      - rpu support for plotting
      - switch agent to  be process based
      - move shell spawner temp files to /tmp

--- src/radical/pilot/agent/radical-pilot-agent-multicore.py
+++ src/radical/pilot/agent/radical-pilot-agent-multicore.py
@@ -568,7 +568,7 @@ class Scheduler(threading.Thread):
     def stop(self):
         
         rpu.prof ('stop request')
-      # rpu.flush_prof()
+        rpu.flush_prof()
         self._terminate.set()
 
 
@@ -3354,7 +3354,7 @@ class ExecWorker(COMPONENT_TYPE):
     def stop(self):
 
         rpu.prof ('stop request')
-      # rpu.flush_prof()
+        rpu.flush_prof()
         self._terminate.set()
 
 
@@ -3417,7 +3417,7 @@ class ExecWorker_POPEN (ExecWorker) :
 
         # shut down the watcher thread
         rpu.prof ('stop request')
-      # rpu.flush_prof()
+        rpu.flush_prof()
         self._terminate.set()
         self._watcher.join()
 
@@ -4334,7 +4334,7 @@ class UpdateWorker(threading.Thread):
     def stop(self):
 
         rpu.prof ('stop request')
-      # rpu.flush_prof()
+        rpu.flush_prof()
         self._terminate.set()
 
 
@@ -4480,7 +4480,7 @@ class StageinWorker(threading.Thread):
     def stop(self):
 
         rpu.prof ('stop request')
-      # rpu.flush_prof()
+        rpu.flush_prof()
         self._terminate.set()
 
 
@@ -4665,7 +4665,7 @@ class StageoutWorker(threading.Thread):
     def stop(self):
 
         rpu.prof ('stop request')
-      # rpu.flush_prof()
+        rpu.flush_prof()
         self._terminate.set()
 
 
@@ -4927,7 +4927,7 @@ class HeartbeatMonitor(threading.Thread):
     def stop(self):
 
         rpu.prof ('stop request')
-      # rpu.flush_prof()
+        rpu.flush_prof()
         self._terminate.set()
         self._agent.stop()
 
@@ -4980,13 +4980,13 @@ class HeartbeatMonitor(threading.Thread):
             if command[COMMAND_TYPE] == COMMAND_CANCEL_PILOT:
                 self.stop()
                 pilot_CANCELED(self._p, self._pilot_id, self._log, "CANCEL received. Terminating.")
-              # rpu.flush_prof()
+                rpu.flush_prof()
                 sys.exit(1)
 
             elif state == rp.CANCELING:
                 self.stop()
                 pilot_CANCELED(self._p, self._pilot_id, self._log, "CANCEL implied. Terminating.")
-              # rpu.flush_prof()
+                rpu.flush_prof()
                 sys.exit(1)
 
             elif command[COMMAND_TYPE] == COMMAND_CANCEL_COMPUTE_UNIT:
@@ -5184,7 +5184,7 @@ class Agent(object):
         """
 
         rpu.prof ('stop request')
-      # rpu.flush_prof()
+        rpu.flush_prof()
         self._terminate.set()
 
 
@@ -5309,7 +5309,7 @@ class Agent(object):
                 self.stop()
                 pilot_FAILED(self._p, self._pilot_id, self._log,
                     "ERROR in agent main loop: %s. %s" % (e, traceback.format_exc()))
-              # rpu.flush_prof()
+                rpu.flush_prof()
                 sys.exit(1)
 
 
@@ -5336,7 +5336,7 @@ class Agent(object):
                 "Terminated (_terminate set).")
 
         rpu.prof ('stop')
-      # rpu.flush_prof()
+        rpu.flush_prof()
         sys.exit(0)
 
 
@@ -5542,7 +5542,7 @@ def main():
     def sigint_handler(signum, frame):
         msg = 'Caught SIGINT. EXITING. (%s: %s)' % (signum, frame)
         pilot_FAILED(mongo_p, options.pilot_id, logger, msg)
-      # rpu.flush_prof()
+        rpu.flush_prof()
         sys.exit(2)
     signal.signal(signal.SIGINT, sigint_handler)
 
@@ -5553,7 +5553,7 @@ def main():
         msg = 'Caught SIGALRM (Walltime limit reached?). EXITING (%s: %s)' \
             % (signum, frame)
         pilot_FAILED(mongo_p, options.pilot_id, logger, msg)
-      # rpu.flush_prof()
+        rpu.flush_prof()
         sys.exit(3)
     signal.signal(signal.SIGALRM, sigalarm_handler)
 
@@ -5613,14 +5613,14 @@ def main():
 
     except SystemExit:
         logger.error("Caught keyboard interrupt. EXITING")
-      # rpu.flush_prof()
+        rpu.flush_prof()
         return(6)
 
     except Exception as e:
         error_msg = "Error running agent: %s" % str(e)
         logger.exception(error_msg)
         pilot_FAILED(mongo_p, options.pilot_id, logger, error_msg)
-      # rpu.flush_prof()
+        rpu.flush_prof()
         sys.exit(7)
 
     finally:
