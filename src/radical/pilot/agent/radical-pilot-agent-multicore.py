83d7eb0460b330fa80ce67c9f866112c0da04daf
Andre Merzky <andre@merzky.net>
2015-08-10 12:19:33 +0200

    disentangle scheduler/lrm from runjob LM

--- src/radical/pilot/agent/radical-pilot-agent-multicore.py
+++ src/radical/pilot/agent/radical-pilot-agent-multicore.py
@@ -997,16 +997,22 @@ class SchedulerTorus(AgentSchedulingComponent):
             return
 
         # TODO: return something else than corner location? Corner index?
-        corner = block[offset][self.TORUS_BLOCK_COOR]
-        sub_block_shape = sub_block_shape_table[num_nodes]
+        sub_block_shape     = sub_block_shape_table[num_nodes]
+        sub_block_shape_str = self._lrms.shape2str(sub_block_shape)
+        corner              = block[offset][self.TORUS_BLOCK_COOR]
+        corner_offset       = self._scheduler.corner2offset(self._scheduler._lrms.torus_block, corner)
+        corner_node         = self._scheduler._lrms.torus_block[corner_offset][self.TORUS_BLOCK_NAME]
 
         end = self.get_last_node(corner, sub_block_shape)
         self._log.debug('Allocating sub-block of %d node(s) with dimensions %s'
                        ' at offset %d with corner %s and end %s.',
-                        num_nodes, self._lrms.shape2str(sub_block_shape), offset,
+                        num_nodes, sub_block_shape_str, offset,
                         self._lrms.loc2str(corner), self._lrms.loc2str(end))
 
-        return corner, sub_block_shape
+        return {'cores_per_node'      : self._lrms.cores_per_node, 
+                'loadl_bg_block'      : self._lrms.loadl_bg_block,
+                'sub_block_shape_str' : sub_block_shape_str,
+                'corner_node'         : corner_node}
 
 
     # --------------------------------------------------------------------------
@@ -1602,11 +1608,22 @@ class LaunchMethodRUNJOB(LaunchMethod):
     # --------------------------------------------------------------------------
     #
     def construct_command(self, task_exec, task_args, task_numcores,
-                          launch_script_hop, (corner, sub_block_shape)):
+                          launch_script_hop, opaque_slots):
 
-        if task_numcores % self._scheduler._lrms.cores_per_node:
-            msg = "Num cores (%d) is not a multiple of %d!" % (
-                task_numcores, self._scheduler._lrms.cores_per_node)
+        if  'cores_per_node'      not in opaque_slots or\
+            'loadl_bg_block'      not in opaque_slots or\
+            'sub_block_shape_str' not in opaque_slots or\
+            'corner_node'         not in opaque_slots :
+            raise RuntimeError('insufficient information to launch via %s: %s' \
+                    % (self.name, opaque_slots))
+
+        cores_per_node      = opaque_slots['cores_per_node']
+        loadl_bg_block      = opaque_slots['loadl_bg_block']
+        sub_block_shape_str = opaque_slots['sub_block_shape_str']
+        corner_node         = opaque_slots['corner_node']
+
+        if task_numcores % cores_per_node:
+            msg = "Num cores (%d) is not a multiple of %d!" % (task_numcores, cores_per_node)
             self._log.exception(msg)
             raise ValueError(msg)
 
@@ -1616,17 +1633,14 @@ class LaunchMethodRUNJOB(LaunchMethod):
         # Set the number of tasks/ranks per node
         # TODO: Currently hardcoded, this should be configurable,
         #       but I don't see how, this would be a leaky abstraction.
-        runjob_command += ' --ranks-per-node %d' % min(self._scheduler._lrms.cores_per_node, task_numcores)
+        runjob_command += ' --ranks-per-node %d' % min(cores_per_node, task_numcores)
 
         # Run this subjob in the block communicated by LoadLeveler
-        runjob_command += ' --block %s' % self._scheduler._lrms.loadl_bg_block
-
-        corner_offset = self._scheduler.corner2offset(self._scheduler._lrms.torus_block, corner)
-        corner_node = self._scheduler._lrms.torus_block[corner_offset][self._scheduler.TORUS_BLOCK_NAME]
+        runjob_command += ' --block %s'  % loadl_bg_block
         runjob_command += ' --corner %s' % corner_node
 
         # convert the shape
-        runjob_command += ' --shape %s' % self._scheduler._lrms.shape2str(sub_block_shape)
+        runjob_command += ' --shape %s' % sub_block_shape_str
 
         # runjob needs the full path to the executable
         if os.path.basename(task_exec) == task_exec:
