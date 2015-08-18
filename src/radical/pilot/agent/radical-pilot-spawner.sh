2206513656a0e8a8eba2ea50fc7da96642dc08df
Andre Merzky <andre@merzky.net>
2015-08-18 14:03:07 +0200

    use a posix fix for a posix problem...
    xargs -0 / --null is not posix

--- src/radical/pilot/agent/radical-pilot-spawner.sh
+++ src/radical/pilot/agent/radical-pilot-spawner.sh
@@ -50,7 +50,7 @@ fi
 # -- but printf will interprete every single '%' in the string, which we don't
 # want.  We thus escape it to '%%'
 qprintf(){
-  \echo "$*" | sed -e 's/%/%%/g' | \xargs --null \printf 
+  \printf "%b\n" "$*"
 }
 
 # --------------------------------------------------------------------
