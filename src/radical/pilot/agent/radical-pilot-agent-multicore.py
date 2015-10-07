fffb2b70648314b6eae06c6344ae4c24cace6720
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2015-10-07 15:33:14 +0200

    POPEN doesnt use RP_SPAWNER_HOP.
    This is a partial revert from an illustre "snapshot" commit.
    I dont understand most of the other (interface) changes either.

--- src/radical/pilot/agent/radical-pilot-agent-multicore.py
+++ src/radical/pilot/agent/radical-pilot-agent-multicore.py
@@ -3814,15 +3814,13 @@ class AgentExecutingComponent_POPEN (AgentExecutingComponent) :
                     else:
                         task_args_string += '"%s" ' % arg  # Otherwise return between double quotes.
 
-            launch_script_hop = "/usr/bin/env RP_SPAWNER_HOP=TRUE %s" % launch_script_name
-
             # The actual command line, constructed per launch-method
             try:
                 launch_command, hop_cmd = \
                     launcher.construct_command(cu['description']['executable'],
                                                task_args_string,
                                                cu['description']['cores'],
-                                               launch_script_hop,
+                                               launch_script_name,
                                                cu['opaque_slots'])
                 if hop_cmd : cmdline = hop_cmd
                 else       : cmdline = launch_script_name
