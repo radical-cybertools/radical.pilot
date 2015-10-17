9921bbb5011f3bafa443a49bab82ce14933bed4c
Andre Merzky <andre@merzky.net>
2015-10-17 21:34:47 +0200

    install examples into shared/radical.pilot
    that saves us from needing to clone stuff during tutorial, or from downloading etc.

--- setup.py
+++ setup.py
@@ -153,6 +153,81 @@ def read(*rnames):
         return ''
 
 
+# ------------------------------------------------------------------------------
+#
+# borrowed from the MoinMoin-wiki installer
+#
+def makeDataFiles(prefix, dir):
+    """ Create distutils data_files structure from dir
+
+    distutil will copy all file rooted under dir into prefix, excluding
+    dir itself, just like 'ditto src dst' works, and unlike 'cp -r src
+    dst, which copy src into dst'.
+
+    Typical usage:
+        # install the contents of 'wiki' under sys.prefix+'share/moin'
+        data_files = makeDataFiles('share/moin', 'wiki')
+
+    For this directory structure:
+        root
+            file1
+            file2
+            dir
+                file
+                subdir
+                    file
+
+    makeDataFiles('prefix', 'root')  will create this distutil data_files structure:
+        [('prefix', ['file1', 'file2']),
+         ('prefix/dir', ['file']),
+         ('prefix/dir/subdir', ['file'])]
+
+    """
+    # Strip 'dir/' from of path before joining with prefix
+    dir = dir.rstrip('/')
+    strip = len(dir) + 1
+    found = []
+    os.path.walk(dir, visit, (prefix, strip, found))
+    return found
+
+def visit((prefix, strip, found), dirname, names):
+    """ Visit directory, create distutil tuple
+
+    Add distutil tuple for each directory using this format:
+        (destination, [dirname/file1, dirname/file2, ...])
+
+    distutil will copy later file1, file2, ... info destination.
+    """
+    files = []
+    # Iterate over a copy of names, modify names
+    for name in names[:]:
+        path = os.path.join(dirname, name)
+        # Ignore directories -  we will visit later
+        if os.path.isdir(path):
+            # Remove directories we don't want to visit later
+            if isbad(name):
+                names.remove(name)
+            continue
+        elif isgood(name):
+            files.append(path)
+    destination = os.path.join(prefix, dirname[strip:])
+    found.append((destination, files))
+
+def isbad(name):
+    """ Whether name should not be installed """
+    return (name.startswith('.') or
+            name.startswith('#') or
+            name.endswith('.pickle') or
+            name == 'CVS')
+
+def isgood(name):
+    """ Whether name should be installed """
+    if not isbad(name):
+        if name.endswith('.py') or name.endswith('.json'):
+            return True
+    return False
+
+
 # -------------------------------------------------------------------------------
 setup_args = {
     'name'               : name,
@@ -218,6 +293,9 @@ setup_args = {
 #   'upload_sphinx'      : {
 #       'upload-dir'     : 'docs/build/html',
 #   }
+    # This copies the contents of the examples/ dir under
+    # sys.prefix/share/radical.pilot.
+    'data_files': makeDataFiles('share/radical.pilot/examples/', 'examples')
 }
 
 # ------------------------------------------------------------------------------
