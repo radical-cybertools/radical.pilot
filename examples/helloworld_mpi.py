44d382f50a42748e8a20e64752d4195fc7f42809
Andre Merzky <andre@merzky.net>
2015-10-20 17:52:18 +0200

    small iteration on mpi hello...

--- examples/helloworld_mpi.py
+++ examples/helloworld_mpi.py
@@ -3,21 +3,23 @@
 # This is an example MPI4Py program that is used
 # by different examples and tests.
 
-import sys
 import time
-import traceback
 
 from mpi4py import MPI
 
-try :
-    name  = MPI.Get_processor_name().split(".")[0]
+
+# ------------------------------------------------------------------------------
+#
+if __name__ == '__main__':
+
+    host  = MPI.Get_processor_name().split('.')[0]
     comm  = MPI.COMM_WORLD
 
-    print "%d/%d/%s"  % (comm.rank+1, comm.size, name)
+    time.sleep(1)
+
+    print "%d/%d/%s"  % (comm.rank+1, comm.size, host)
 
     comm.Barrier()   # wait for everybody to synchronize here
 
-except Exception as e :
-    traceback.print_exc ()
-    sys.exit (1)
+# ------------------------------------------------------------------------------
 
