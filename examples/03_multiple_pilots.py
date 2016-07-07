b0db369bc6c86ebe4040b02aad3be9c612f7d861
Andre Merzky <andre@merzky.net>
2016-07-07 15:23:07 +0200

    fix logging problem reported by Ming and Vivek

--- examples/03_multiple_pilots.py
+++ examples/03_multiple_pilots.py
@@ -26,7 +26,7 @@ import radical.utils as ru
 if __name__ == '__main__':
 
     # we use a reporter class for nicer output
-    report = ru.LogReporter(name='radical.pilot', level=verbose)
+    report = ru.LogReporter(name='radical.pilot.examples', level=verbose)
     report.title('Getting Started (RP version %s)' % rp.version)
 
     # use the resource specified as argument, fall back to localhost
