1aaed227a3b62d33d8e87d495929b7d48bd53cad
itomaldonado <mo.maldonado@gmail.com>
2017-03-27 09:58:51 -0400

    added 01 and 03 acceptance cases

--- tests/acceptance/test_acceptance.py
+++ tests/acceptance/test_acceptance.py
@@ -4,6 +4,7 @@ __copyright__ = 'Copyright 2013-2014, http://radical.rutgers.edu'
 __license__ = 'MIT'
 
 import os
+import time
 import unittest
 import radical.pilot as rp  # noqa
 import radical.utils as ru  # noqa
@@ -63,11 +64,9 @@ class AcceptanceTests(unittest.TestCase):
         # Number of Compute Units (CUs)
         self.n = 128   # number of units to run
 
-    def test_00_getting_started(self):
-        """  """
         # Define an [n]-core local pilot that runs for [x] minutes
         # Here we use a dict to initialize the description object
-        pd_init = {
+        self.pd_init = {
             'resource': self.resource,
             'runtime': 15,  # pilot runtime (min)
             'exit_on_error': True,
@@ -76,10 +75,17 @@ class AcceptanceTests(unittest.TestCase):
             'access_schema': self.config[self.resource]['schema'],
             'cores': self.config[self.resource]['cores'],
         }
-        pdesc = rp.ComputePilotDescription(pd_init)
+
+
+
+    def test_00_getting_started(self):
+        """Test a standard pilot run"""
+
+        # Create description object from template description
+        pilot_desc = rp.ComputePilotDescription(self.pd_init)
 
         # Launch the pilot.
-        pilot = self.pmgr.submit_pilots(pdesc)
+        pilot = self.pmgr.submit_pilots(pilot_desc)
 
         self.umgr.add_pilots(pilot)
 
@@ -96,15 +102,15 @@ class AcceptanceTests(unittest.TestCase):
         # Submit the previously created ComputeUnit descriptions to the
         # PilotManager. This will trigger the selected scheduler to start
         # assigning ComputeUnits to the ComputePilots.
-        self.umgr.submit_units(cuds)
+        units = self.umgr.submit_units(cuds)
 
         # Wait for all compute units to reach a final state (DONE, CANCELED or
         # FAILED).
         self.umgr.wait_units()
 
-        # Verify that 100% of the pilots came back with 'DONE' status
+        # Verify that 100% of the units came back with 'DONE' status
         done_units = 0
-        for description in self.umgr.get_units():
+        for description in units:
             if description.state == "DONE":
                 done_units += 1
         self.assertEquals(
@@ -112,23 +118,16 @@ class AcceptanceTests(unittest.TestCase):
             "Only {0}% of CUs were DONE."
             .format(str((float(done_units) / float(self.n)) * 100.00)))
 
-    def test_02_failing_units(self):
-        """  """
-        # Define an [n]-core local pilot that runs for [x] minutes
-        # Here we use a dict to initialize the description object
-        pd_init = {
-            'resource': self.resource,
-            'runtime': 15,  # pilot runtime (min)
-            'exit_on_error': True,
-            'project': self.config[self.resource]['project'],
-            'queue': self.config[self.resource]['queue'],
-            'access_schema': self.config[self.resource]['schema'],
-            'cores': self.config[self.resource]['cores'],
-        }
-        pdesc = rp.ComputePilotDescription(pd_init)
+
+
+    def test_01_unit_details(self):
+        """Test failing units, about ~50% of the units will fail"""
+
+        # Create description object from template description
+        pilot_desc = rp.ComputePilotDescription(self.pd_init)
 
         # Launch the pilot.
-        pilot = self.pmgr.submit_pilots(pdesc)
+        pilot = self.pmgr.submit_pilots(pilot_desc)
 
         self.umgr.add_pilots(pilot)
 
@@ -150,23 +149,129 @@ class AcceptanceTests(unittest.TestCase):
         # Submit the previously created ComputeUnit descriptions to the
         # PilotManager. This will trigger the selected scheduler to start
         # assigning ComputeUnits to the ComputePilots.
-        self.umgr.submit_units(cuds)
+        units = self.umgr.submit_units(cuds)
 
         # Wait for all compute units to reach a final state (DONE, CANCELED or
         # FAILED).
         self.umgr.wait_units()
 
-        # Verify that >= 50% of the pilots came back with 'DONE' status
+        # Not asserting for 100% completion, that is not the idea here...
+
+        # Get some more details for one unit:
+        unit_dict = units[0].as_dict()
+        self.assertIsNotNone(unit_dict['working_directory'])
+        self.assertIsNotNone(unit_dict['execution_details']['pilot'])
+        self.assertIsNotNone(unit_dict['execution_details']['statehistory'])
+
+
+    def test_02_failing_units(self):
+        """Test failing units, about ~50% of the units will fail"""
+
+        # Create description object from template description
+        pilot_desc = rp.ComputePilotDescription(self.pd_init)
+
+        # Launch the pilot.
+        pilot = self.pmgr.submit_pilots(pilot_desc)
+
+        self.umgr.add_pilots(pilot)
+
+        # Create a workload of ComputeUnits.
+        # Each compute unit runs '/bin/date'.
+        # About ~50% of them will fail
+        cuds = list()
+        for i in range(1, self.n + 1):
+            # create a new CU description, and fill it.
+            # Here we don't use dict initialization.
+            cud = rp.ComputeUnitDescription()
+            if i % 2:
+                cud.executable = '/bin/date'
+            else:
+                # trigger an error now and then
+                cud.executable = '/bin/data'  # does not exist
+            cuds.append(cud)
+
+        # Submit the previously created ComputeUnit descriptions to the
+        # PilotManager. This will trigger the selected scheduler to start
+        # assigning ComputeUnits to the ComputePilots.
+        units = self.umgr.submit_units(cuds)
+
+        # Wait for all compute units to reach a final state (DONE, CANCELED or
+        # FAILED).
+        self.umgr.wait_units()
+
+        # Verify that >= 50% of the units came back with 'DONE' status
         # TODO: better checks for failures...
         done_units = 0
-        something = self.umgr.get_units()
-        for description in self.umgr.get_units():
+        for description in units:
             if description.state == "DONE":
                 done_units += 1
         self.assertGreaterEqual(
             (float(done_units) / float(self.n)), 0.50,
             "Only {0}% of CUs were DONE."
             .format(str((float(done_units) / float(self.n)) * 100.00)))
+
+
+    def test_03_multiple_pilots(self):
+        """Test multiple pilots"""
+
+        # Have to hard-code list of resources
+        # TODO: get real list of resources
+        resources = ['local.localhost']
+
+        # Create multiple pilot descriptions, one for each resource
+        pilot_descriptions = list()
+        resource_count = len(resources)
+        for resource in resources:
+            pd_init = {
+                'resource': resource,
+                'runtime': 15,  # pilot runtime (min)
+                'exit_on_error': True,
+                'project': self.config[resource]['project'],
+                'queue': self.config[resource]['queue'],
+                'access_schema': self.config[resource]['schema'],
+                'cores': self.config[resource]['cores'],
+            }
+            pilot_descriptions.append(rp.ComputePilotDescription(pd_init))
+
+        # Launch the pilot.
+        pilot = self.pmgr.submit_pilots(pilot_descriptions)
+        pilot_count = len(pilot)
+
+        self.umgr.add_pilots(pilot)
+
+        # Create a workload of ComputeUnits.
+        # Each compute unit runs '/bin/date'.
+        cuds = list()
+        for i in range(0, self.n):
+            # create a new CU description, and fill it.
+            # Here we don't use dict initialization.
+            cud = rp.ComputeUnitDescription()
+            cud.executable = '/bin/date'
+            cuds.append(cud)
+
+        # Submit the previously created ComputeUnit descriptions to the
+        # PilotManager. This will trigger the selected scheduler to start
+        # assigning ComputeUnits to the ComputePilots.
+        units = self.umgr.submit_units(cuds)
+
+        # Wait for all compute units to reach a final state (DONE, CANCELED or
+        # FAILED).
+        self.umgr.wait_units()
+
+        # Verify that 100% of the units came back with 'DONE' status
+        done_units = 0
+        for description in units:
+            if description.state == "DONE":
+                done_units += 1
+        self.assertEquals(
+            (float(done_units) / float(self.n)), 1.0,
+            "Only {0}% of CUs were DONE."
+            .format(str((float(done_units) / float(self.n)) * 100.00)))
+
+        # Finally assert that the number of requested vs submitted pilots are the same
+        self.assertEquals(resource_count, pilot_count)
+
+
 
     def tearDown(self):
         # Close pilot session
