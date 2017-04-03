d4a03bd20f9753e5c625fb63b4476586af3111b4
Andre Merzky <andre@merzky.net>
2017-04-03 15:51:20 +0200

    revert an unintended change

--- examples/01_unit_details.py
+++ examples/01_unit_details.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
         # Create a workload of ComputeUnits.
         # Each compute unit runs '/bin/date'.
 
-        n = 2   # number of units to run
+        n = 128  # number of units to run
         report.info('create %d unit description(s)\n\t' % n)
 
         cuds = list()
