051fa2c4dfcebe82a509a3de1bf04fb1d6cf5629
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2017-04-04 12:26:54 -0400

    Fix issue #1307

--- src/radical/pilot/agent/lm/spark.py
+++ src/radical/pilot/agent/lm/spark.py
@@ -170,12 +170,9 @@ class Spark(LaunchMethod):
         opaque_slots = cu['opaque_slots']
         cud          = cu['description']
         task_exec    = cud['executable']
-        task_cores   = cud['cores']
         task_args    = cud.get('arguments')
         task_env     = cud.get('environment')
-        work_dir     = cu['workdir']
-        unit_id      = cu['_id']
-
+        
         # Construct the args_string which is the arguments given as input to the
         # shell script. Needs to be a string
         self._log.debug("Constructing SPARK command")
