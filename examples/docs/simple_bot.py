b0c416f61d0506f74ed5fa4c70cef83f9f6fa082
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2017-05-08 23:38:41 -0500

    Simple bot commit

--- examples/docs/simple_bot.py
+++ examples/docs/simple_bot.py
@@ -6,55 +6,36 @@ __license__   = "MIT"
 import sys
 import os
 import radical.pilot as rp
+import radical.utils as ru
 
+verbose  = os.environ.get('RADICAL_PILOT_VERBOSE', 'REPORT')
+os.environ['RADICAL_PILOT_VERBOSE'] = verbose
 
 """ DESCRIPTION: Tutorial 1: A Simple Workload consisting of a Bag-of-Tasks
 """
 
-# READ: The RADICAL-Pilot documentation: 
+# READ: The RADICAL-Pilot documentation:
 #   http://radicalpilot.readthedocs.org/en/latest
 #
-# Try running this example with RADICAL_PILOT_VERBOSE=debug set if 
+# Try running this example with RADICAL_PILOT_VERBOSE=debug set if
 # you want to see what happens behind the scences!
 
 
-#------------------------------------------------------------------------------
-#
-def pilot_state_cb (pilot, state):
-
-    if not pilot:
-        return
-
-    print "[Callback]: ComputePilot '%s' state: %s." % (pilot.uid, state)
-
-    if state == rp.FAILED:
-        sys.exit (1)
-
-
-#------------------------------------------------------------------------------
-#
-def unit_state_cb (unit, state):
-
-    if not unit:
-        return
-
-    global CNT
-
-    print "[Callback]: unit %s on %s: %s." % (unit.uid, unit.pilot_id, state)
-
-    if state == rp.FAILED:
-        print "stderr: %s" % unit.stderr
-        sys.exit(2)
-
-
-#------------------------------------------------------------------------------
 #
 if __name__ == "__main__":
+    
+    RESOURCE_LABEL = 'local.localhost'
+    PILOT_CORES = 2
+    BAG_SIZE = 10 # The number of units
+    CU_CORES = 1 # The cores each CU will take.
+ 
+    # we use a reporter class for nicer output
+    report = ru.LogReporter(name='radical.pilot')
+    report.title('Getting Started (RP version %s)' % rp.version)
 
     # Create a new session. No need to try/except this: if session creation
     # fails, there is not much we can do anyways...
-    session = rp.Session(database_url=os.environ.get('RADICAL_PILOT_DBURL'))
-    print "session id: %s" % session.uid
+    session = rp.Session()
 
     # all other pilot code is now tried/excepted.  If an exception is caught, we
     # can rely on the session object to exist and be valid, and we can thus tear
@@ -62,52 +43,41 @@ if __name__ == "__main__":
     try:
 
         # Add a Pilot Manager. Pilot managers manage one or more ComputePilots.
-        print "Initializing Pilot Manager ..."
         pmgr = rp.PilotManager(session=session)
 
-        # Register our callback with the PilotManager. This callback will get
-        # called every time any of the pilots managed by the PilotManager
-        # change their state.
-        pmgr.register_callback(pilot_state_cb)
-
-        # ----- CHANGE THIS -- CHANGE THIS -- CHANGE THIS -- CHANGE THIS ------
-        # 
-        # Change the resource below if you want to run on a remote resource. 
-        # You also might have to set the 'project' to your allocation ID if 
-        # your remote resource does compute time accounting. 
         #
-        # A list of preconfigured resources can be found at: 
+        # Change the resource below if you want to run on a remote resource.
+        # You also might have to set the 'project' to your allocation ID if
+        # your remote resource does compute time accounting.
+        #
+        # A list of preconfigured resources can be found at:
         # http://radicalpilot.readthedocs.org/en/latest/machconf.html#preconfigured-resources
-        # 
+        #
         pdesc = rp.ComputePilotDescription ()
-        pdesc.resource = "local.localhost"  # this is a "label", not a hostname
-        pdesc.cores    =  1
-        pdesc.runtime  = 10    # minutes
+        pdesc.resource = RESOURCE_LABEL  # this is a "label", not a hostname
+        pdesc.cores    =  PILOT_CORES
+        pdesc.runtime  = 30    # minutes
         pdesc.cleanup  = True  # clean pilot sandbox and database entries
 
         # submit the pilot.
-        print "Submitting Compute Pilot to Pilot Manager ..."
+        report.header("Submitting Compute Pilot to Pilot Manager ...")
         pilot = pmgr.submit_pilots(pdesc)
 
         # create a UnitManager which schedules ComputeUnits over pilots.
-        print "Initializing Unit Manager ..."
-        umgr = rp.UnitManager (session=session,
-                               scheduler=rp.SCHED_DIRECT_SUBMISSION)
+        report.header("Initializing Unit Manager ...")
+        umgr = rp.UnitManager (session=session)
 
-        # Register our callback with the UnitManager. This callback will get
-        # called every time any of the units managed by the UnitManager
-        # change their state.
-        umgr.register_callback(unit_state_cb)
 
         # Add the created ComputePilot to the UnitManager.
-        print "Registering Compute Pilot with Unit Manager ..."
+        report.ok('>>ok\n')
+
         umgr.add_pilots(pilot)
 
-        NUMBER_JOBS  = 10 # the total number of cus to run
+        report.info('Create %d Unit Description(s)\n\t' % BAG_SIZE)
 
         # create CU descriptions
         cudesc_list = []
-        for i in range(NUMBER_JOBS):
+        for i in range(BAG_SIZE):
 
             # -------- BEGIN USER DEFINED CU DESCRIPTION --------- #
             cudesc = rp.ComputeUnitDescription()
@@ -118,21 +88,21 @@ if __name__ == "__main__":
             # -------- END USER DEFINED CU DESCRIPTION --------- #
 
             cudesc_list.append(cudesc)
-
+            report.progress()
+        report.ok('>>>ok\n')
         # Submit the previously created ComputeUnit descriptions to the
         # PilotManager. This will trigger the selected scheduler to start
         # assigning ComputeUnits to the ComputePilots.
-        print "Submit Compute Units to Unit Manager ..."
+        report.header("Submit Compute Units to Unit Manager ...")
         cu_set = umgr.submit_units (cudesc_list)
 
-        print "Waiting for CUs to complete ..."
+        report.header("Waiting for CUs to complete ...")
         umgr.wait_units()
 
-        print "All CUs completed:"
         for unit in cu_set:
             print "* CU %s, state %s, exit code: %s, stdout: %s" \
                 % (unit.uid, unit.state, unit.exit_code, unit.stdout.strip())
-    
+
 
     except Exception as e:
         # Something unexpected happened in the pilot code above
@@ -149,7 +119,7 @@ if __name__ == "__main__":
     finally:
         # always clean up the session, no matter if we caught an exception or
         # not.
-        print "closing session"
+        report.header("closing session")
         session.close ()
 
         # the above is equivalent to
@@ -158,7 +128,6 @@ if __name__ == "__main__":
         #
         # it will thus both clean out the session's database record, and kill
         # all remaining pilots.
-
+    report.header()
 
 #-------------------------------------------------------------------------------
-
