269546912c4b16c024d16d922ea504f9300ac0ea
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2017-05-09 00:05:31 -0500

    Couples Tasks Changed

--- examples/docs/coupled_tasks.py
+++ examples/docs/coupled_tasks.py
@@ -7,61 +7,35 @@ import sys
 import os
 import radical.pilot as rp
 
+verbose  = os.environ.get('RADICAL_PILOT_VERBOSE', 'REPORT')
+os.environ['RADICAL_PILOT_VERBOSE'] = verbose
 
 """ DESCRIPTION: Tutorial 3: Coupled Tasks
 For every task A1 and B1 a C1 is started.
 """
 
-# READ: The RADICAL-Pilot documentation: 
+# READ: The RADICAL-Pilot documentation:
 #   http://radicalpilot.readthedocs.org/en/latest
 #
-# Try running this example with RADICAL_PILOT_VERBOSE=debug set if 
+# Try running this example with RADICAL_PILOT_VERBOSE=debug set if
 # you want to see what happens behind the scences!
-
-
-#------------------------------------------------------------------------------
-#
-def pilot_state_cb (pilot, state):
-
-    if not pilot:
-        return
-
-    print "[Callback]: ComputePilot '%s' state: %s." % (pilot.uid, state)
-
-    if state == rp.FAILED:
-        sys.exit (1)
-
-
-#------------------------------------------------------------------------------
-#
-def unit_state_cb (unit, state):
-
-    if not unit:
-        return
-
-    global CNT
-
-    print "[Callback]: unit %s on %s: %s." % (unit.uid, unit.pilot_id, state)
-
-    if state == rp.FAILED:
-        print "stderr: %s" % unit.stderr
-        sys.exit(2)
 
 
 #------------------------------------------------------------------------------
 #
 if __name__ == "__main__":
 
-    # we can optionally pass session name to RP
-    if len(sys.argv) > 1:
-        session_name = sys.argv[1]
-    else:
-        session_name = None
+    RESOURCE_LABEL =
+    PILOT_CORES = 
+    NUMBER_COUPLES = 
+    CU_A_CORES = 
+    CU_B_CORES = 
+    CU_C_CORES = 
+
 
     # Create a new session. No need to try/except this: if session creation
     # fails, there is not much we can do anyways...
-    session = rp.Session(name=session_name,database_url=os.environ.get('RADICAL_PILOT_DBURL'))
-    print "session id: %s" % session.uid
+    session = rp.Session()
 
     # all other pilot code is now tried/excepted.  If an exception is caught, we
     # can rely on the session object to exist and be valid, and we can thus tear
@@ -69,25 +43,11 @@ if __name__ == "__main__":
     # clause...
     try:
 
-        # ----- CHANGE THIS -- CHANGE THIS -- CHANGE THIS -- CHANGE THIS ------
-        # 
-        # Change the user name below if you are using a remote resource 
-        # and your username on that resource is different from the username 
-        # on your local machine. 
-        #
-        c = rp.Context('userpass')
-        #c.user_id = "tutorial_X"
-        #c.user_pass = "PutYourPasswordHere"
-        session.add_context(c)
 
         # Add a Pilot Manager. Pilot managers manage one or more ComputePilots.
         print "Initializing Pilot Manager ..."
         pmgr = rp.PilotManager(session=session)
 
-        # Register our callback with the PilotManager. This callback will get
-        # called every time any of the pilots managed by the PilotManager
-        # change their state.
-        pmgr.register_callback(pilot_state_cb)
 
         # ----- CHANGE THIS -- CHANGE THIS -- CHANGE THIS -- CHANGE THIS ------
         # 
@@ -102,9 +62,9 @@ if __name__ == "__main__":
         # http://radicalpilot.readthedocs.org/en/latest/machconf.html#preconfigured-resources
         # 
         pdesc = rp.ComputePilotDescription ()
-        pdesc.resource = "local.localhost"  # NOTE: This is a "label", not a hostname
-        pdesc.runtime  = 10 # minutes
-        pdesc.cores    = 1
+        pdesc.resource = RESOURCE_LABEL  # NOTE: This is a "label", not a hostname
+        pdesc.runtime  = 30 # minutes
+        pdesc.cores    = PILOT_CORES
         pdesc.cleanup  = True
 
         # submit the pilot.
@@ -114,30 +74,22 @@ if __name__ == "__main__":
         # Combine the ComputePilot, the ComputeUnits and a scheduler via
         # a UnitManager object.
         print "Initializing Unit Manager ..."
-        umgr = rp.UnitManager (session=session,
-                               scheduler=rp.SCHED_DIRECT_SUBMISSION)
-
-        # Register our callback with the UnitManager. This callback will get
-        # called every time any of the units managed by the UnitManager
-        # change their state.
-        umgr.register_callback(unit_state_cb)
+        umgr = rp.UnitManager (session=session)
 
         # Add the created ComputePilot to the UnitManager.
         print "Registering Compute Pilot with Unit Manager ..."
         umgr.add_pilots(pilot)
 
-        NUMBER_JOBS  = 2 # the total number of CUs to chain
-
         # submit A cus to pilot job
         cudesc_list_A = []
-        for idx in range(NUMBER_JOBS):
+        for idx in range(NUMBER_COUPLES):
 
             # -------- BEGIN USER DEFINED CU 1 DESCRIPTION --------- #
             cudesc = rp.ComputeUnitDescription()
             cudesc.environment = {"CU_LIST": "A", "CU_NO": "%02d" % idx}
             cudesc.executable  = "/bin/echo"
             cudesc.arguments   = ['"$CU_LIST CU with id $CU_NO"']
-            cudesc.cores       = 1
+            cudesc.cores       = CU_A_CORES
             # -------- END USER DEFINED CU 1 DESCRIPTION --------- #
 
             cudesc_list_A.append(cudesc)
@@ -150,14 +102,14 @@ if __name__ == "__main__":
 
         # submit B cus to pilot job
         cudesc_list_B = []
-        for idx in range(NUMBER_JOBS):
+        for idx in range(NUMBER_COUPLES):
 
             # -------- BEGIN USER DEFINED CU 2 DESCRIPTION --------- #
             cudesc = rp.ComputeUnitDescription()
             cudesc.environment = {"CU_LIST": "B", "CU_NO": "%02d" % idx}
             cudesc.executable  = "/bin/echo"
             cudesc.arguments   = ['"$CU_LIST CU with id $CU_NO"']
-            cudesc.cores       = 1
+            cudesc.cores       = CU_B_CORES
             # -------- END USER DEFINED CU 2 DESCRIPTION --------- #
 
             cudesc_list_B.append(cudesc)
@@ -178,14 +130,14 @@ if __name__ == "__main__":
         # submit 'C' tasks to pilot job. each 'C' task takes the output of
         # an 'A' and a 'B' task and puts them together.
         cudesc_list_C = []
-        for idx in range(NUMBER_JOBS):
+        for idx in range(NUMBER_COUPLES):
 
             # -------- BEGIN USER DEFINED CU 3 DESCRIPTION --------- #
             cudesc = rp.ComputeUnitDescription()
             cudesc.environment = {"CU_SET": "C", "CU_NO": "%02d" % idx}
             cudesc.executable  = "/bin/echo"
             cudesc.arguments   = ['"$CU_SET CU with id $CU_NO"']
-            cudesc.cores       = 1
+            cudesc.cores       = CU_C_CORES
             # -------- END USER DEFINED CU 3 DESCRIPTION --------- #
 
             cudesc_list_C.append(cudesc)
