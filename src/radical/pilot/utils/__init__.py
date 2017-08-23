987f36bcc7be999f2b337ad4ce90e228ec6a7043
Andre Merzky <andre@merzky.net>
2017-08-23 10:22:10 +0200

    catch up with __file__ changes

--- src/radical/pilot/utils/__init__.py
+++ src/radical/pilot/utils/__init__.py
@@ -11,7 +11,7 @@ _pwd   = os.path.dirname (__file__)
 _root  = "%s/.." % _pwd
 
 version_short, version_detail, version_base, version_branch, \
-        sdist_name, sdist_path = _ru.get_version([_root])
+        sdist_name, sdist_path = _ru.get_version([_root, _pwd])
 version = version_short
 logger  = _ru.get_logger('radical.pilot')
 
