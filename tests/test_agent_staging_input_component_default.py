4f052e0b29ab432ac79f1a8ca8786176f6f1a5f4
chengwill97 <chengwill97@gmail.com>
2018-01-30 14:56:56 -0500

    Added single move test

--- tests/test_agent_staging_input_component_default.py
+++ tests/test_agent_staging_input_component_default.py
@@ -85,6 +85,9 @@ class TestStagingInputComponent(unittest.TestCase):
         # Unit output directory
         self.unit_directory = os.path.join(workdir, self.unit['uid'])
 
+        # Pilot staging directory
+        self.pilot_directory = os.path.join(workdir, 'staging_area')
+
         # Create unit folder
         os.makedirs(self.unit_directory)
 
@@ -121,6 +124,33 @@ class TestStagingInputComponent(unittest.TestCase):
 
         # Verify the actionables were done...        
         self.assertTrue(os.path.isfile(os.path.join(self.unit_directory, 'single-file')))
+
+
+    @mock.patch.object(Default, '__init__', return_value=None)
+    @mock.patch.object(Default, 'advance')
+    @mock.patch.object(ru.Profiler, 'prof')
+    @mock.patch('radical.utils.raise_on')
+    def test_move_single_file(self, mocked_init, mocked_method, mocked_profiler, mocked_raise_on):
+        component = Default(cfg=self.cfg, session=None)
+        component._prof = mocked_profiler
+        component._log = ru.get_logger('dummy')
+        actionables = list()
+        actionables.append({
+            'uid'   : ru.generate_id('sd'),
+            'source': 'pilot:///single-file',
+            'action': rp.MOVE,
+            'target': 'unit:///single-file',
+            'flags':    [rp.CREATE_PARENTS, rp.SKIP_FAILED],
+            'priority': 0
+        })
+        
+        # Call the component's '_handle_unit' function
+        # Should perform all of the actionables in order
+        component._handle_unit(self.unit, actionables)
+
+        # Verify the actionables were done...        
+        self.assertTrue(os.path.isfile(os.path.join(self.unit_directory, 'single-file')))
+        self.assertFalse(os.path.exists(os.path.join(self.pilot_directory, 'single-file')))
 
 
     @mock.patch.object(Default, '__init__', return_value=None)
