1f64ce8a40c5f40e5815b134c25646a9a1aac692
Andre Merzky <andre@merzky.net>
2018-03-17 01:33:48 +0100

    typo

--- bin/radical-pilot-limits.py
+++ bin/radical-pilot-limits.py
@@ -17,7 +17,33 @@ def _work():
     time.sleep(10)
 
 
-print
+base = '/tmp/rp_limit_%d.%%d' % os.getpid()
+while True:
+    try:
+        f = open(base % len(files), 'w')
+        files.append(f)
+    except:
+        break
+
+print 'files  : %5d' % len(files)
+for f in files:
+    os.unlink(f.name)
+    f.close()
+
+host = 'localhost'
+port = 22
+while True:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        sockets.append(s)
+    except Exception as e:
+        break
+
+print 'sockets: %5d' % len(sockets)
+for s in sockets:
+    s.close()
+
 while True:
     try:
         t = mt.Thread(target=_work)
@@ -31,7 +57,6 @@ for t in threads:
     t.join()
 
 
-print
 while True:
     try:
         p = mp.Process(target=_work)
@@ -46,33 +71,3 @@ for p in procs:
     p.join()
 
 
-print
-base = '/tmp/rp_limit_%d.%%d' % os.getpid()
-while True:
-    try:
-        f = open(base % len(files), 'w')
-        files.append(f)
-    except:
-        break
-
-print 'files  : %5d' % len(files)
-for f in files:
-    os.unlink(f.name)
-    f.close()
-
-print
-host = 'localhost'
-port = 22
-while True:
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect([host, port])
-        sockets.append(s)
-    except:
-        break
-
-print 'sockets: %5d' % len(sockets)
-for s in sockets:
-    s.close()
-
-print
