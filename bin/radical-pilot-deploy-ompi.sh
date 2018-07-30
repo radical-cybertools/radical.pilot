99f2e20d59e645787ce72211ae007bfcdba5bd0b
Andre Merzky <andre@merzky.net>
2018-07-30 19:21:55 +0200

    Fix/issue 1671 (#1680)
    * fix outdated links in ompi deploy script
    
    * fix orte core placement on threaded units

--- bin/radical-pilot-deploy-ompi.sh
+++ bin/radical-pilot-deploy-ompi.sh
@@ -17,8 +17,9 @@
 # procedure!
 
 export OMPI_DIR=$HOME/radical/ompi/                  # target location for install
-export OMPI_COMMIT=539f71d                           # OpenMPI commit to install
+export OMPI_DIR=/lustre/atlas2/csc230/world-shared/openmpi/
 export OMPI_COMMIT=a3ac67be0d
+export OMPI_COMMIT=539f71d                           # OpenMPI commit to install
 export OMPI_LABEL=$(date '+%Y_%m_%d'_${OMPI_COMMIT}) # module flag for installed version
 export MAKEFLAGS=-j16                                # speed up build on multicore machines
 
@@ -46,10 +47,10 @@ mkdir -p $OMPI_DOWNLOAD
 mkdir -p $OMPI_SOURCE
 
 cd $OMPI_DOWNLOAD
-wget http://ftp.nluug.nl/gnu/autoconf/autoconf-2.69.tar.gz
-wget http://ftp.nluug.nl/gnu/automake/automake-1.13.4.tar.gz
-wget http://nl.mirror.babylon.network/gnu/libtool/libtool-2.4.2.tar.gz
-wget https://ftp.gnu.org/gnu/m4/m4-1.4.16.tar.gz
+wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
+wget http://ftp.gnu.org/gnu/automake/automake-1.13.4.tar.gz
+wget http://ftp.gnu.org/gnu/libtool/libtool-2.4.2.tar.gz
+wget http://ftp.gnu.org/gnu/m4/m4-1.4.16.tar.gz
 
 cd $OMPI_SOURCE
 tar -xvzf $OMPI_DOWNLOAD/m4-1.4.16.tar.gz
@@ -75,6 +76,7 @@ make install
 cd $OMPI_SOURCE
 tar -xvzf $OMPI_DOWNLOAD/libtool-2.4.2.tar.gz
 cd libtool-2.4.2
+./bootstrap
 ./configure --prefix=$OMPI_TOOLS_PREFIX
 make
 make install
