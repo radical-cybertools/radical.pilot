8577611678740118dccd56a05ab10fde8acad43e
Andre Merzky <andre@merzky.net>
2018-11-04 16:51:17 -0500

    shape example update

--- examples/misc/wl_shape.sh
+++ examples/misc/wl_shape.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-p=$RP_PROCESSES
-t=$RP_THREADS
+export p=$RP_PROCESSES
+export t=$RP_THREADS
 
-echo -n "$RP_UNIT_ID  $p  $t  $(( (p-3)*(p-3) + (t-5)*(t-5) ))" >> ../app_stats.log
+echo "$RP_UNIT_ID  $p  $t  $(( (p-3)*(p-3) + (t-5)*(t-5) ))" >> ../app_stats.log
 echo "$p processes / $t threads"
 
