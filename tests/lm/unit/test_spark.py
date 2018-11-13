6f79b9b5a8b432b37e8a6000836fb46156174c2c
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2018-11-13 09:28:01 -0500

    Spark Launch Method test based on #1758

---
+++ tests/lm/unit/test_spark.py
@@ -0,0 +1,46 @@
+import radical.utils as ru
+import radical.pilot as rp
+from radical.pilot.agent.lm.spark import Spark
+import pytest
+import glob
+import os
+import json
+
+try:
+    import mock
+except ImportError:
+    from unittest import mock
+
+# Setup to be done for every test
+#------------------------------------------------------------------------------
+def setUp():
+
+    test_cases = json.load(open('tests/lm/unit/test_cases_spark.json'))
+
+    return test_cases
+#------------------------------------------------------------------------------
+
+def tearDown():
+    pass
+#------------------------------------------------------------------------------
+
+# Test Summit Scheduler construct_command method
+#------------------------------------------------------------------------------
+@mock.patch.object(Spark, '__init__', return_value=None)
+@mock.patch.object(Spark, '_configure', return_value=None)
+@mock.patch('radical.utils.raise_on')
+def test_construct_command(mocked_init, mocked_configure,
+                           mocked_raise_on):
+
+    test_cases = setUp()
+
+    component = Spark(cfg=None, session=None)
+    component._log  = ru.get_logger('dummy')
+    component.launch_command = ''
+    for i in range(len(test_cases['trigger'])):
+        cu = test_cases['trigger'][i]
+        command,_ = component.construct_command(cu,None)
+        print command
+        assert command == test_cases['result'][i]
+
+    tearDown()
