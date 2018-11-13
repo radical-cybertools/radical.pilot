c4683f908aeb8db7b9e768cb6c2c9cad0e1de2c7
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2018-11-13 09:58:32 -0500

    Passing Pylint and flake8. Continuing #1758

--- tests/lm/unit/test_spark.py
+++ tests/lm/unit/test_spark.py
@@ -1,9 +1,5 @@
 import radical.utils as ru
-import radical.pilot as rp
 from radical.pilot.agent.lm.spark import Spark
-import pytest
-import glob
-import os
 import json
 
 try:
@@ -11,21 +7,25 @@ try:
 except ImportError:
     from unittest import mock
 
+#pylint: disable=protected-access, unused-argument
+
 # Setup to be done for every test
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 def setUp():
 
     test_cases = json.load(open('tests/lm/unit/test_cases_spark.json'))
 
     return test_cases
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
 
 def tearDown():
     pass
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
 
 # Test Summit Scheduler construct_command method
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 @mock.patch.object(Spark, '__init__', return_value=None)
 @mock.patch.object(Spark, '_configure', return_value=None)
 @mock.patch('radical.utils.raise_on')
@@ -34,7 +34,7 @@ def test_construct_command(mocked_init, mocked_configure,
 
     test_cases = setUp()
 
-    component = Spark(cfg=None, session=None)
+    component = Spark(cfg=None, logger=ru.get_logger('dummy'))
     component._log  = ru.get_logger('dummy')
     component.launch_command = ''
     for i in range(len(test_cases['trigger'])):
