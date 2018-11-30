021aad4c0ed2335d5823de9cd65cb630a6386b5e
Vivekanandan (Vivek) Balasubramanian <vivek.balasubramanian@rutgers.edu>
2018-11-30 11:07:10 -0500

    edits for proposal to separate input from result

--- tests/lm/unit/test_common.py
+++ tests/lm/unit/test_common.py
@@ -3,37 +3,38 @@
 # pylint: disable=protected-access, unused-argument
 
 import os
-import glob
-
 import radical.utils as ru
 
 
 # ------------------------------------------------------------------------------
-# 
+#
 def setUp(test_type, test_name):
 
     pwd = os.path.dirname(__file__)
     ret = list()
-    for fin in glob.glob('%s/../../test_cases/unit.*.json' % pwd):
 
-        tc     = ru.read_json(fin)
-        unit   = tc['unit'   ]
-        setup  = tc['setup'  ].get(test_type, {})
-        result = tc['results'].get(test_type, {}).get(test_name)
-        test   = ru.dict_merge(unit, setup, ru.PRESERVE)
+    tests = ru.read_json('%s/results_%s.json' % (pwd, test_name))
+    uids = tests.keys()
 
-        if result:
-            ret.append([test, result])
+    for uid in uids:
+        fin = '%s/../../test_cases/%s.json' % (pwd, uid)
+        tc = ru.read_json(fin)
+        unit = tc['unit']
+        setup = tc['setup'].get(test_type, {})
+        test = ru.dict_merge(unit, setup, ru.PRESERVE)
+        result = tests[uid]
+
+        ret.append([test, result])
 
     return ret
 
-
 # ------------------------------------------------------------------------------
 #
+
+
 def tearDown():
 
     pass
 
 
 # ------------------------------------------------------------------------------
-
