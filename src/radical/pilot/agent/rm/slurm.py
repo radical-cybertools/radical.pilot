01f056acd5cb4026d02df0128a05aa4d764e6d35
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2018-12-06 12:06:53 -0500

    Commit for issue #1775

--- src/radical/pilot/agent/rm/slurm.py
+++ src/radical/pilot/agent/rm/slurm.py
@@ -75,7 +75,12 @@ class Slurm(LRMS):
         # in case of a single partial node allocation.
         self.cores_per_node = self._cfg.get('cores_per_node', 0)
         self.gpus_per_node  = self._cfg.get('gpus_per_node',  0)  # FIXME GPU
-        self.lfs_per_node   = {'path' : self._cfg.get('lfs_path_per_node', None),
+
+        lfs_path = self._cfg.get('lfs_path_per_node', None)
+        if '$' in path:
+            lfs_path = os.path.expandvars(path)
+
+        self.lfs_per_node   = {'path' : lfs_path,
                                'size' : self._cfg.get('lfs_size_per_node', 0)
                               }
 
