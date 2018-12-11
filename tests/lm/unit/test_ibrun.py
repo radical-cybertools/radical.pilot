796855ab5c2ddf63bbde70ccd4b4aafca6571f78
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2018-12-11 12:18:24 -0500

    IBrun test

---
+++ tests/lm/unit/test_ibrun.py
@@ -0,0 +1,49 @@
+
+# pylint: disable=protected-access, unused-argument
+
+
+from   test_common                   import setUp
+from   radical.pilot.agent.lm.ibrun import IBRun
+
+import radical.utils as ru
+import pytest
+
+
+try:
+    import mock
+except ImportError:
+    from unittest import mock
+
+
+# ------------------------------------------------------------------------------
+#
+@mock.patch.object(IBRun, '__init__',   return_value=None)
+@mock.patch.object(IBRun, '_configure', return_value=None)
+@mock.patch('radical.utils.raise_on')
+def test_construct_command(mocked_init, 
+                           mocked_configure,
+                           mocked_raise_on):
+
+    test_cases = setUp('lm', 'ibrun')
+    component  = IBRun(name=None, cfg=None, session=None)
+
+    component._log           = ru.get_logger('dummy')
+    component.name           = 'IBRun'
+    component.mpi_flavor     = None
+    component.launch_command = 'ibrun'
+    component.ccmrun_command = ''
+    component.dplace_command = ''
+
+    for unit, result in test_cases:
+        if result == "Error":
+            with pytest.raises(RuntimeError):
+                command, hop = component.construct_command(unit, None)
+        else:
+            command, hop = component.construct_command(unit, None)
+            assert([command, hop] == result)
+
+
+# ------------------------------------------------------------------------------
+
+
+
