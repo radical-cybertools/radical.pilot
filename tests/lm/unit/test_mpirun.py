3621805d4600b90be59e2a0e892870f4294786ee
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2018-12-12 12:11:41 -0500

    Increasing MPIRUN coverage

--- tests/lm/unit/test_mpirun.py
+++ tests/lm/unit/test_mpirun.py
@@ -17,6 +17,101 @@ except ImportError:
 # ------------------------------------------------------------------------------
 #
 @mock.patch.object(MPIRun, '__init__',   return_value=None)
+@mock.patch.object(MPIRun, '_get_mpi_info', return_value=[5,'ORTE'])
+@mock.patch('radical.utils.raise_on')
+@mock.patch('radical.utils.which', return_value='/usr/bin/mpirun')
+def test_configure(mocked_init, mocked_get_mpi_info, mocked_raise_on,
+                   mocked_which):
+
+    component = MPIRun(name=None, cfg=None, session=None)
+    component.name = 'MPIRun'
+    component._configure()
+    assert('mpirun' == component.launch_command)
+    assert(5 == component.mpi_version)
+    assert('ORTE' == component.mpi_flavor)
+
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+#
+@mock.patch.object(MPIRun, '__init__',   return_value=None)
+@mock.patch.object(MPIRun, '_get_mpi_info', return_value=[5,'ORTE'])
+@mock.patch('radical.utils.raise_on')
+@mock.patch('radical.utils.which', return_value='/usr/bin/mpirun')
+def test_configure_rsh(mocked_init, mocked_get_mpi_info, mocked_raise_on,
+                   mocked_which):
+
+    component = MPIRun(name=None, cfg=None, session=None)
+    component.name = 'MPIRun_rsh'
+    component._configure()
+    assert('mpirun' == component.launch_command)
+    assert(5 == component.mpi_version)
+    assert('ORTE' == component.mpi_flavor)
+
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+#
+@mock.patch.object(MPIRun, '__init__',   return_value=None)
+@mock.patch.object(MPIRun, '_get_mpi_info', return_value=[5,'ORTE'])
+@mock.patch('radical.utils.raise_on')
+@mock.patch('radical.utils.which', return_value='/usr/bin/mpirun')
+def test_configure_mpt(mocked_init, mocked_get_mpi_info, mocked_raise_on,
+                   mocked_which):
+
+    component = MPIRun(name=None, cfg=None, session=None)
+    component.name = 'MPIRun_mpt'
+    component._configure()
+    assert('mpirun' == component.launch_command)
+    assert(5 == component.mpi_version)
+    assert('ORTE' == component.mpi_flavor)
+
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+#
+@mock.patch.object(MPIRun, '__init__',   return_value=None)
+@mock.patch.object(MPIRun, '_get_mpi_info', return_value=[5,'ORTE'])
+@mock.patch('radical.utils.raise_on')
+@mock.patch('radical.utils.which', return_value='/usr/bin/mpirun')
+def test_configure_ccmrun(mocked_init, mocked_get_mpi_info, mocked_raise_on,
+                   mocked_which):
+
+    component = MPIRun(name=None, cfg=None, session=None)
+    component.name = 'MPIRun_ccmrun'
+    component._configure()
+    assert('mpirun' == component.launch_command)
+    assert(5 == component.mpi_version)
+    assert('ORTE' == component.mpi_flavor)
+
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+#
+@mock.patch.object(MPIRun, '__init__',   return_value=None)
+@mock.patch.object(MPIRun, '_get_mpi_info', return_value=[5,'ORTE'])
+@mock.patch('radical.utils.raise_on')
+@mock.patch('radical.utils.which', return_value='/usr/bin/mpirun')
+def test_configure_dplace(mocked_init, mocked_get_mpi_info, mocked_raise_on,
+                   mocked_which):
+
+    component = MPIRun(name=None, cfg=None, session=None)
+    component.name = 'MPIRun_dplace'
+    component._configure()
+    assert('mpirun' == component.launch_command)
+    assert(5 == component.mpi_version)
+    assert('ORTE' == component.mpi_flavor)
+
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+#
+@mock.patch.object(MPIRun, '__init__',   return_value=None)
 @mock.patch.object(MPIRun, '_configure', return_value=None)
 @mock.patch('radical.utils.raise_on')
 def test_construct_command(mocked_init, 
