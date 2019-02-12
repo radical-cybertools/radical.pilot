28c47ab43f742b83f3bf7f6b4dc5033c6a4c1ddb
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2019-02-12 16:36:21 -0500

    Increasing coverage!

--- tests/rm/unit/test_slurm.py
+++ tests/rm/unit/test_slurm.py
@@ -1,7 +1,7 @@
 
 # pylint: disable=protected-access, unused-argument
 import os
-
+import pytest
 import radical.utils as ru
 from radical.pilot.agent.rm.slurm import Slurm
 
@@ -50,4 +50,72 @@ def test_configure(mocked_init, mocked_raise_on, mocked_expand_hostlist):
     component.lm_info = {'cores_per_node': None}
     component._configure()
     assert component.node_list == [['nodes1','nodes1'],['nodes2','nodes2']]
+    assert component.cores_per_node == 24
+    assert component.gpus_per_node == 1
+    assert component.lfs_per_node == {'path': 'test/', 'size': 100}
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+#
+@mock.patch.object(Slurm, '__init__',   return_value=None)
+@mock.patch('radical.utils.raise_on')
+@mock.patch('hostlist.expand_hostlist', return_value=['nodes1', 'nodes2'])
+def test_configure_error(mocked_init, mocked_raise_on, mocked_expand_hostlist):
+
+    # Test 1 no config file
+    del os.environ['SLURM_NODELIST']
+    os.environ['SLURM_NPROCS'] = '48'
+    os.environ['SLURM_NNODES'] = '2'
+    os.environ['SLURM_CPUS_ON_NODE'] = '24'
+
+    component = Slurm(cfg=None, session=None)
+    component._log = ru.get_logger('dummy')
+    component._cfg = {}
+    component.lm_info = {}
+
+    with pytest.raises(RuntimeError):
+        component._configure()
+
+    # Test 2 config file
+    os.environ['SLURM_NODELIST'] = 'nodes-[1-2]'
+    del os.environ['SLURM_NPROCS']
+    os.environ['SLURM_NNODES'] = '2'
+    os.environ['SLURM_CPUS_ON_NODE'] = '24'
+
+    component = Slurm(cfg=None, session=None)
+    component._log = ru.get_logger('dummy')
+    component._cfg = {}
+    component.lm_info = {}
+
+    with pytest.raises(RuntimeError):
+        component._configure()
+
+    # Test 2 config file
+    os.environ['SLURM_NODELIST'] = 'nodes-[1-2]'
+    os.environ['SLURM_NPROCS'] = '48'
+    del os.environ['SLURM_NNODES']
+    os.environ['SLURM_CPUS_ON_NODE'] = '24'
+
+    component = Slurm(cfg=None, session=None)
+    component._log = ru.get_logger('dummy')
+    component._cfg = {}
+    component.lm_info = {}
+
+    with pytest.raises(RuntimeError):
+        component._configure()
+
+    # Test 2 config file
+    os.environ['SLURM_NODELIST'] = 'nodes-[1-2]'
+    os.environ['SLURM_NPROCS'] = '48'
+    os.environ['SLURM_NNODES'] = '2'
+    del os.environ['SLURM_CPUS_ON_NODE']
+
+    component = Slurm(cfg=None, session=None)
+    component._log = ru.get_logger('dummy')
+    component._cfg = {}
+    component.lm_info = {}
+
+    with pytest.raises(RuntimeError):
+        component._configure()
 # ------------------------------------------------------------------------------
