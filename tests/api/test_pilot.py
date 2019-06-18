e8bae3e611ffdb8a2edbd1512ec7448625ea45a3
Andre Merzky <andre@merzky.net>
2019-06-18 21:29:28 +0200

    ensure session close on some tests

--- tests/api/test_pilot.py
+++ tests/api/test_pilot.py
@@ -28,35 +28,37 @@ class TestPilot(unittest.TestCase):
         """
         session = rp.Session()
 
-        pm = rp.PilotManager(session=session)
+        try:
+            pm = rp.PilotManager(session=session)
 
-        cpd = rp.ComputePilotDescription()
-        cpd.resource = "local.localhost"
-        cpd.cores = 1
-        cpd.runtime = 1
-        cpd.sandbox = "/tmp/rp.sandbox.unittests"
-        cpd.cleanup = True
+            cpd = rp.ComputePilotDescription()
+            cpd.resource = "local.localhost"
+            cpd.cores = 1
+            cpd.runtime = 1
+            cpd.sandbox = "/tmp/rp.sandbox.unittests"
+            cpd.cleanup = True
 
-        pilot = pm.submit_pilots(pilot_descriptions=cpd)
+            pilot = pm.submit_pilots(pilot_descriptions=cpd)
 
-        assert pilot is not None
-        assert pilot.start_time is None
-        assert pilot.stop_time is None
+            assert pilot is not None
+            assert pilot.start_time is None
+            assert pilot.stop_time is None
 
-        pilot.wait(state=[rp.PMGR_ACTIVE, rp.FAILED], timeout=300)
-        assert pilot.submission_time is not None
-        assert pilot.state == rp.PMGR_ACTIVE
-        assert pilot.start_time is not None
-        assert pilot.log is not None
-        assert pilot.sandbox == "file://localhost%s/pilot-%s/" % (cpd.sandbox, pilot.uid)
+            pilot.wait(state=[rp.PMGR_ACTIVE, rp.FAILED], timeout=300)
+            assert pilot.submission_time is not None
+            assert pilot.state == rp.PMGR_ACTIVE
+            assert pilot.start_time is not None
+            assert pilot.log is not None
+            assert pilot.sandbox == "file://localhost%s/pilot-%s/" % (cpd.sandbox, pilot.uid)
 
-        # the pilot should finish after it has reached run_time
+            # the pilot should finish after it has reached run_time
 
-        pilot.wait(timeout=300)
-        assert pilot.state == rp.DONE
-        assert pilot.stop_time is not None
+            pilot.wait(timeout=300)
+            assert pilot.state == rp.DONE
+            assert pilot.stop_time is not None
 
-        session.close()
+        finally:
+            session.close()
 
     # -------------------------------------------------------------------------
     #
@@ -65,31 +67,33 @@ class TestPilot(unittest.TestCase):
         """
         session = rp.Session()
 
-        pm = rp.PilotManager(session=session)
+        try:
+            pm = rp.PilotManager(session=session)
 
-        cpd = rp.ComputePilotDescription()
-        cpd.resource = "local.localhost"
-        cpd.cores = 1
-        cpd.runtime = 1
-        cpd.sandbox = "/non-/existing/directory..."
-        cpd.cleanup = True
+            cpd = rp.ComputePilotDescription()
+            cpd.resource = "local.localhost"
+            cpd.cores = 1
+            cpd.runtime = 1
+            cpd.sandbox = "/non-/existing/directory..."
+            cpd.cleanup = True
 
-        pilot = pm.submit_pilots(pilot_descriptions=cpd)
-        pilot.wait(timeout=300)
-        assert pilot.state == rp.FAILED, "State is '%s' instead of 'Failed'." % pilot.state
+            pilot = pm.submit_pilots(pilot_descriptions=cpd)
+            pilot.wait(timeout=300)
+            assert pilot.state == rp.FAILED, "State is '%s' instead of 'Failed'." % pilot.state
 
-        cpd = rp.ComputePilotDescription()
-        cpd.resource = "local.localhost"
-        cpd.cores = 100000000000  # This should fail - at least in 2014 ;-)
-        cpd.runtime = 1
-        cpd.sandbox = "/tmp/rp.sandbox.unittests"
-        cpd.cleanup = True
+            cpd = rp.ComputePilotDescription()
+            cpd.resource = "local.localhost"
+            cpd.cores = 100000000000  # This should fail - at least in 2014 ;-)
+            cpd.runtime = 1
+            cpd.sandbox = "/tmp/rp.sandbox.unittests"
+            cpd.cleanup = True
 
-        pilot = pm.submit_pilots(pilot_descriptions=cpd)
-        pilot.wait(timeout=300)
-        assert pilot.state == rp.FAILED, ("state should be %s and not %s" % (rp.FAILED, pilot.state))
+            pilot = pm.submit_pilots(pilot_descriptions=cpd)
+            pilot.wait(timeout=300)
+            assert pilot.state == rp.FAILED, ("state should be %s and not %s" % (rp.FAILED, pilot.state))
 
-        session.close()
+        finally:
+            session.close()
 
     # -------------------------------------------------------------------------
     #
@@ -98,31 +102,37 @@ class TestPilot(unittest.TestCase):
         """
         session = rp.Session()
 
-        pm = rp.PilotManager(session=session)
+        try:
+            pm = rp.PilotManager(session=session)
 
-        cpd = rp.ComputePilotDescription()
-        cpd.resource = "local.localhost"
-        cpd.cores = 1
-        cpd.runtime = 1
-        cpd.sandbox = "/tmp/rp.sandbox.unittests"
-        cpd.cleanup = True
+            cpd = rp.ComputePilotDescription()
+            cpd.resource = "local.localhost"
+            cpd.cores = 1
+            cpd.runtime = 1
+            cpd.sandbox = "/tmp/rp.sandbox.unittests"
+            cpd.cleanup = True
 
-        pilot = pm.submit_pilots(pilot_descriptions=cpd)
+            pilot = pm.submit_pilots(pilot_descriptions=cpd)
 
-        assert pilot is not None
-        assert pilot.start_time is None
-        assert pilot.stop_time is None
+            assert pilot is not None
+            assert pilot.start_time is None
+            assert pilot.stop_time is None
 
-        pilot.wait(state=[rp.PMGR_ACTIVE, rp.FAILED], timeout=300)
-        assert pilot.submission_time is not None
-        assert pilot.state == rp.PMGR_ACTIVE
-        assert pilot.start_time is not None
+            pilot.wait(state=[rp.PMGR_ACTIVE, rp.FAILED], timeout=300)
+            assert pilot.submission_time is not None
+            assert pilot.state == rp.PMGR_ACTIVE
+            assert pilot.start_time is not None
 
-        # the pilot should finish after it has reached run_time
-        pilot.cancel()
+            # the pilot should finish after it has reached run_time
+            pilot.cancel()
 
-        pilot.wait(timeout=300)
-        assert pilot.state == rp.CANCELED
-        assert pilot.stop_time is not None
+            pilot.wait(timeout=300)
+            assert pilot.state == rp.CANCELED
+            assert pilot.stop_time is not None
 
-        session.close()
+        finally:
+            session.close()
+
+
+# ------------------------------------------------------------------------------
+
