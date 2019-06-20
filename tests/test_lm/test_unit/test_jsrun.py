2ae4eccf36587e76850274b430bde55765d4fa7d
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2019-06-20 11:06:23 -0400

    The linter has to pass for the tests as well

--- tests/test_lm/test_unit/test_jsrun.py
+++ tests/test_lm/test_unit/test_jsrun.py
@@ -1,8 +1,7 @@
+# pylint: disable=protected-access, unused-argument
 
 import os
-import json
 import glob
-from pprint import pprint
 import radical.utils as ru
 from test_common                  import setUp
 from radical.pilot.agent.lm.jsrun import JSRUN
@@ -12,6 +11,7 @@ try:
     import mock
 except ImportError:
     from unittest import mock
+
 
 # ------------------------------------------------------------------------------
 #
@@ -29,7 +29,7 @@ def tearDown():
 def test_create_resource_set_file(mocked_init, mocked_method, mocked_raise_on):
 
     test_cases = setUp('lm', 'jsrun')
-    component    = JSRUN(cfg=None, session=None)
+    component    = JSRUN(name=None, cfg=None, session=None)
 
     for unit, _, resource_file in test_cases:
 
@@ -55,15 +55,11 @@ def test_construct_command(mocked_init, mocked_configure,
                            mocked_create_resource_set_file, mocked_raise_on):
 
     test_cases = setUp('lm', 'jsrun')
-    
-    component = JSRUN(cfg=None, session=None)
+
+    component = JSRUN(name=None, cfg=None, session=None)
     component._log  = ru.Logger('dummy')
     component.launch_command = 'jsrun'
     for unit, result,_ in test_cases:
         command, hop = component.construct_command(unit, None)
         assert([command, hop] == result)
-
-
-
 # ------------------------------------------------------------------------------
-
