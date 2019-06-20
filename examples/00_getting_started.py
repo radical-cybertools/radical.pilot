2122f66443d08c4873b96791d8d4bac36a8a45ed
Andre Merzky <andre@merzky.net>
2019-06-20 18:54:37 -0400

    fix prte

--- examples/00_getting_started.py
+++ examples/00_getting_started.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
                    'project'       : config[resource]['project'],
                    'queue'         : config[resource]['queue'],
                    'access_schema' : config[resource]['schema'],
-                   'cores'         : 1024
+                   'cores'         : 42*4*4
                   }
         pdesc = rp.ComputePilotDescription(pd_init)
 
@@ -78,6 +78,9 @@ if __name__ == '__main__':
         # Each compute unit runs '/bin/date'.
 
         n = 128  # number of units to run
+        n = 1024
+        n = 1024 * 10
+        n = 42 * 4 * 4 * 10
         report.info('create %d unit description(s)\n\t' % n)
 
         cuds = list()
