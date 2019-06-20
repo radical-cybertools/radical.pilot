0620334c133d128673e4dc3e0daa7aa34ceddc44
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2019-06-20 16:11:04 -0400

    Fixing states test based on Andre's comments

--- tests/test_states.py
+++ tests/test_states.py
@@ -59,7 +59,33 @@ def test_unit_state_value():
 #
 def test_unit_state_collapse():
     from radical.pilot.states import _unit_state_collapse, _unit_state_values
+    states = ['AGENT_STAGING_OUTPUT',
+              'AGENT_EXECUTING_PENDING',
+              'UMGR_SCHEDULING',
+              'UMGR_STAGING_OUTPUT',
+              'UMGR_STAGING_OUTPUT_PENDING',
+              'AGENT_SCHEDULING',
+              'AGENT_STAGING_OUTPUT_PENDING',
+              'AGENT_SCHEDULING_PENDING']
+    assert(_unit_state_collapse(states) == 'UMGR_STAGING_OUTPUT')
 
-    for key in _unit_state_values.iterkeys():
-        assert(_unit_state_collapse([key]) == key)
+    states =  ['DONE',
+               'UMGR_STAGING_INPUT_PENDING',
+               'AGENT_SCHEDULING_PENDING',
+               'UMGR_SCHEDULING_PENDING',
+               'AGENT_STAGING_OUTPUT_PENDING',
+               'AGENT_EXECUTING',
+               'AGENT_STAGING_INPUT',
+               'FAILED']
+    assert(_unit_state_collapse(states) == 'DONE')
+
+    states = ['AGENT_STAGING_OUTPUT_PENDING',
+              'CANCELED',
+              'UMGR_STAGING_OUTPUT_PENDING',
+              'UMGR_STAGING_INPUT',
+              'NEW',
+              'AGENT_SCHEDULING',
+              'AGENT_STAGING_INPUT',
+              'UMGR_SCHEDULING']
+    assert(_unit_state_collapse(states) == 'CANCELED')
 # ------------------------------------------------------------------------------
