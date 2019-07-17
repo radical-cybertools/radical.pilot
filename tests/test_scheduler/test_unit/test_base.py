c73445df34ab7b971fa9210a352b35bcfad52e10
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2019-07-17 11:39:16 -0400

    Small tests for base and torus classes.

--- tests/test_scheduler/test_unit/test_base.py
+++ tests/test_scheduler/test_unit/test_base.py
@@ -1,10 +1,47 @@
 # pylint: disable=protected-access, unused-argument
 # pylint: disable=no-value-for-parameter
-import pytest
-import radical.utils as ru
+
+import os
+import radical.pilot
 from radical.pilot.agent.scheduler.base import AgentSchedulingComponent
 
 try:
     import mock
 except ImportError:
     from unittest import mock
+
+# ------------------------------------------------------------------------------
+#
+@mock.patch.object(radical.pilot.utils.Component, '__init__', return_value=None)
+@mock.patch('radical.utils.generate_id', return_value='test')
+def test_init(mocked_init, mocked_generate_id):
+    component = AgentSchedulingComponent(cfg={'owner': 'test'}, session=None)
+
+    assert component.nodes == None
+    assert component._lrms == None
+    assert component._uid == 'test'
+    assert not component._uniform_wl
+
+    os.environ['RP_UNIFORM_WORKLOAD'] = 'true'
+    component = AgentSchedulingComponent(cfg={'owner': 'test'}, session=None)
+
+    assert component.nodes == None
+    assert component._lrms == None
+    assert component._uid == 'test'
+    assert component._uniform_wl
+
+    os.environ['RP_UNIFORM_WORKLOAD'] = 'YES'
+    component = AgentSchedulingComponent(cfg={'owner': 'test'}, session=None)
+
+    assert component.nodes == None
+    assert component._lrms == None
+    assert component._uid == 'test'
+    assert component._uniform_wl
+
+    os.environ['RP_UNIFORM_WORKLOAD'] = '1'
+    component = AgentSchedulingComponent(cfg={'owner': 'test'}, session=None)
+
+    assert component.nodes == None
+    assert component._lrms == None
+    assert component._uid == 'test'
+    assert component._uniform_wl
--- tests/test_scheduler/test_unit/test_torus.py
+++ tests/test_scheduler/test_unit/test_torus.py
@@ -8,3 +8,21 @@ try:
     import mock
 except ImportError:
     from unittest import mock
+
+# ------------------------------------------------------------------------------
+#
+@mock.patch.object(Torus, '__init__', return_value=None)
+def test_configure(mocked_init):
+
+    component = Torus()
+    component._lrms_cores_per_node = 15
+    component._configure()
+
+    assert component._cores_per_node == 15
+    assert component.nodes == 'bogus'
+
+    component = Torus()
+    component._lrms_info = {'name': 'Test'}
+    component._lrms_cores_per_node = 0
+    with pytest.raises(RuntimeError):
+        component._configure()
