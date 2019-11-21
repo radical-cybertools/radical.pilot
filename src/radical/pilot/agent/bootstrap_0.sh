37f62162c9ceae9350e06a9ba8d1a5a1655c1120
Andre Merzky <andre@merzky.net>
2019-11-21 05:40:00 -0700

    completed cheyenne support

--- src/radical/pilot/agent/bootstrap_0.sh
+++ src/radical/pilot/agent/bootstrap_0.sh
@@ -1027,7 +1027,7 @@ virtenv_create()
     for dep in $VIRTENV_RADICAL_DEPS
     do
         run_cmd "install $dep" \
-                "$PIP install $dep" \
+                "$PIP --no-cache-dir install --no-build-isolation $dep" \
              || echo "Couldn't install $dep! Lets see how far we get ..."
     done
 
@@ -1237,7 +1237,7 @@ rp_install()
     pip_flags="$pip_flags --src '$PILOT_SANDBOX/rp_install/src'"
     pip_flags="$pip_flags --build '$PILOT_SANDBOX/rp_install/build'"
     pip_flags="$pip_flags --install-option='--prefix=$RP_INSTALL'"
-    pip_flags="$pip_flags --no-deps"
+    pip_flags="$pip_flags --no-deps --no-cache-dir --no-build-isolation"
 
     for src in $rp_install_sources
     do
