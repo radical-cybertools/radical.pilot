d2075f071414c1b0dce7a4d2e4cc2c8d122531f2
Mikhail Titov <mikhail.a.titov@gmail.com>
2020-04-26 23:47:56 -0400

    updated LM IBRun (added env variable TACC_TASKS_PER_NODE to consider the actual number of tasks)

--- src/radical/pilot/agent/launch_method/ibrun.py
+++ src/radical/pilot/agent/launch_method/ibrun.py
@@ -1,8 +1,6 @@
 
-
 __copyright__ = "Copyright 2016, http://radical.rutgers.edu"
 __license__   = "MIT"
-
 
 import radical.utils as ru
 
@@ -41,26 +39,41 @@ class IBRun(LaunchMethod):
 
         slots        = cu['slots']
         cud          = cu['description']
+
         task_exec    = cud['executable']
-        task_cores   = cud['cpu_processes']  # FIXME: handle cpu_threads
         task_args    = cud.get('arguments') or []
         task_argstr  = self._create_arg_string(task_args)
+        task_env     = cud.get('environment') or dict()
 
-        cpn     = self._cfg.cores_per_node
-        index   = 0
-        offsets = list()
+        n_tasks      = cud['cpu_processes']  # FIXME: handle cpu_threads
 
-        import pprint
+        # Usage of env variable TACC_TASKS_PER_NODE is purely for MPI tasks,
+        #  and threads are not considered (info provided by TACC support)
+        n_node_tasks = int(task_env.get('TACC_TASKS_PER_NODE') or
+                           self._cfg.get('cores_per_node', 1))
+
+        if not self._node_list:
+            raise RuntimeError('attribute "_node_list" (from RM) is not set')
+        if not slots.get('nodes'):
+            raise RuntimeError('unit.slots.nodes is not set')
+
+        ibrun_offset = 0
+        offsets      = list()
+        node_id      = 0
+
         for node in self._node_list:
             for slot_node in slots['nodes']:
                 if slot_node['uid'] == node[0]:
                     for core_map in slot_node['core_map']:
-                        for core_idx in core_map:
-                            offsets.append(index + core_idx)
-    
-            index += cpn
+                        if core_map:
+                            # core_map contains core ids for each thread,
+                            # but threads are ignored for offsets
+                            offsets.append(node_id +
+                                           (core_map[0] // len(core_map)))
+            node_id += n_node_tasks
 
-        ibrun_offset = min(offsets)
+        if offsets:
+            ibrun_offset = min(offsets)
 
         if task_argstr:
             task_command = "%s %s" % (task_exec, task_argstr)
@@ -68,11 +81,10 @@ class IBRun(LaunchMethod):
             task_command = task_exec
 
         ibrun_command = "%s -n %s -o %d %s" % \
-                        (self.launch_command, task_cores,
+                        (self.launch_command, n_tasks,
                          ibrun_offset, task_command)
 
         return ibrun_command, None
 
 
 # ------------------------------------------------------------------------------
-
