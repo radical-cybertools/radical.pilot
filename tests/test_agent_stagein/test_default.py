a9f5485d9b48032829020909ade95201a157dfa2
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2020-05-05 15:57:25 -0400

    Mocking advance and handle units

--- tests/test_agent_stagein/test_default.py
+++ tests/test_agent_stagein/test_default.py
@@ -1,11 +1,8 @@
 # pylint: disable=protected-access, unused-argument
-import os
 import glob
 import radical.utils as ru
 from radical.pilot.agent.staging_input.default import Default
 from unittest import TestCase, mock
-
-os.environ['RP_UNIT_TESTING'] = 'TRUE'
 
 
 class TestDefault(TestCase):
@@ -30,20 +27,39 @@ class TestDefault(TestCase):
 
         pass
 
+
     # --------------------------------------------------------------------------
     #
     @mock.patch.object(Default, '__init__',   return_value=None)
-    @mock.patch.object(Default, '_handle_unit', return_value=None)
-    @mock.patch.object(Default, 'advance', return_value=None)
     @mock.patch('radical.utils.raise_on')
-    def test_work(self, mocked_init, mocked_raise_on, mocked_handle_unit,
-                  mocked_advance):
+    def test_work(self, mocked_init, mocked_raise_on):
+
+        global_things = []
+        global_state = []
+        # ------------------------------------------------------------------------------
+        #
+        def _advance_side_effect(things, state, publish, push):
+            nonlocal global_things
+            nonlocal global_state
+            global_things.append(things)
+            global_state.append(state)
+
+
+        # ------------------------------------------------------------------------------
+        #
+        def _handle_unit_side_effect(unit, actionables):
+            _advance_side_effect(unit, actionables, False, False)
+
 
         tests = self.setUp()
         component = Default(cfg=None, session=None)
+        component._handle_unit = mock.MagicMock(side_effect=_handle_unit_side_effect)
+        component.advance = mock.MagicMock(side_effect=_advance_side_effect)
         component._log = ru.Logger('dummy')
 
         for test in tests:
-            staging_units, no_staging_units = component._work([test[0]])
-            self.assertEqual(staging_units, test[1][0])
-            self.assertEqual(no_staging_units, test[1][1])
+            global_things = []
+            global_state = []
+            component._work([test[0]])
+            self.assertEqual(global_things, test[1][0])
+            self.assertEqual(global_state, test[1][1])
