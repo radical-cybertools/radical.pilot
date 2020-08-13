74e4fe370d8a5279d1e299a100e373433991d9ee
Andre Merzky <andre@merzky.net>
2020-08-13 14:09:16 +0200

    move bs_1 bre_exec up front

--- src/radical/pilot/agent/bootstrap_0.sh
+++ src/radical/pilot/agent/bootstrap_0.sh
@@ -1760,6 +1760,10 @@ cd $PILOT_SANDBOX
 export PATH="$PB1_PATH"
 export LD_LIBRARY_PATH="$PB1_LDLB"
 
+# pass environment variables down so that module load becomes effective at
+# the other side too (e.g. sub-agents).
+$PREBOOTSTRAP2_EXPANDED
+
 # activate virtenv
 if test "$PYTHON_DIST" = "anaconda" && test -e "`which conda`"
 then
@@ -1784,10 +1788,6 @@ unset RADICAL_PILOT_DBURL
 
 # avoid ntphost lookups on compute nodes
 export RADICAL_PILOT_NTPHOST=$RADICAL_PILOT_NTPHOST
-
-# pass environment variables down so that module load becomes effective at
-# the other side too (e.g. sub-agents).
-$PREBOOTSTRAP2_EXPANDED
 
 # start agent, forward arguments
 # NOTE: exec only makes sense in the last line of the script
