50ca2e028ecc2ea0cb13f56cf54e33e415ac720d
Mikhail Titov <mikhail.a.titov@gmail.com>
2020-12-09 18:34:34 -0500

    updated Cobalt RM (to be run on ThetaGPU) and added corresponding config data for ThetaGPU

--- src/radical/pilot/agent/resource_manager/cobalt.py
+++ src/radical/pilot/agent/resource_manager/cobalt.py
@@ -20,25 +20,23 @@ class Cobalt(ResourceManager):
 
         ResourceManager.__init__(self, cfg, session)
 
-
     # --------------------------------------------------------------------------
     #
     def _configure(self):
 
-        # we only support Cobalt on Theta right now, and since we know that
-        # Theta is a Cray, we know that aprun is available.  Alas, aprun
-        # provides the only way (we could find so far) to determing the list of
-        # nodes we have available (`COBALT_NODELIST` seems broken).  So we run
-        # `aprun` with the rank of nodes we *think* we have, and with `-N 1` to
-        # place one rank per node, and run `hostname` - that gives is the list
-        # of hostnames.  The number of nodes we receive from `$COBALT_PARTSIZE`.
+        try:
+            with open(os.environ['COBALT_NODEFILE'], 'r') as f:
+                node_list = [node.strip() for node in f.readlines() if node]
+        except KeyError:
+            n_nodes = int(os.environ['COBALT_PARTSIZE'])
+            node_list = ['node_%04d' % i for i in range(n_nodes)]
 
-        n_nodes          = int(os.environ['COBALT_PARTSIZE'])
-      # out, _, _        = ru.sh_callout('aprun -q -n %d -N 1 hostname' % n_nodes)
-      # node_list        = out.split()
-      # print('=== out 1 : [%s]' % out)a
-        node_list        = ['node_%04d' %  i for i in range(n_nodes)]
-        assert(len(node_list) == n_nodes), node_list
+            # Another option is to run `aprun` with the rank of nodes
+            # we *think* we have, and with `-N 1` to place one rank per node,
+            # and run `hostname` - that gives the list of hostnames.
+            # The number of nodes we receive from `$COBALT_PARTSIZE`.
+          # out, _, _ = ru.sh_callout('aprun -q -n %d -N 1 hostname' % n_nodes)
+          # node_list = out.split()
 
       # # we also want to learn the core count per node
       # cmd              = 'cat /proc/cpuinfo | grep processor | wc -l'
@@ -48,23 +46,13 @@ class Cobalt(ResourceManager):
       # assert(len(core_counts) == 1), core_counts
       # cores_per_node   = core_counts[0]
 
-        cores_per_node   = self._cfg.get('cores_per_node', 1)
-        gpus_per_node    = self._cfg.get('gpus_per_node', 0)
-        lfs_per_node     = {'path': ru.expand_env(
-                                       self._cfg.get('lfs_path_per_node')),
-                            'size':    self._cfg.get('lfs_size_per_node', 0)
-                           }
-        mem_per_node     = self._cfg.get('mem_per_node', 0)
-
-        self._log.info("Found unique core counts: %s", cores_per_node)
-
         # node names are unique, so can serve as node uids
-        self.node_list        = [[node, node] for node in node_list]
-        self.cores_per_node   = cores_per_node
-        self.gpus_per_node    = gpus_per_node
-        self.lfs_per_node     = lfs_per_node
-        self.mem_per_node     = mem_per_node
-
+        self.node_list      = [[node, node] for node in node_list]
+        self.cores_per_node = self._cfg.get('cores_per_node', 1)
+        self.gpus_per_node  = self._cfg.get('gpus_per_node', 0)
+        self.mem_per_node   = self._cfg.get('mem_per_node', 0)
+        self.lfs_per_node   = {'path': ru.expand_env(
+                                       self._cfg.get('lfs_path_per_node')),
+                               'size': self._cfg.get('lfs_size_per_node', 0)}
 
 # ------------------------------------------------------------------------------
-
