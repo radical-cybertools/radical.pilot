b8898dcedd289939ce64ad0306c3d16cdd2a73ac
Ioannis Paraskevakos <g.paraskev@rutgers.edu>
2020-12-15 12:32:58 -0500

    Included batch script for comet and README file

---
+++ tests/test_resources_integration/batch_scripts/comet.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH -J rp_integration_test  # Job name
+#SBATCH --begin=now+1week
+#SBATCH -o rp_integration_test.%j.out   # Name of stdout output file(%j expands to jobId)
+#SBATCH -e rp_integration_test.%j.err   # Name of stderr output file(%j expands to jobId)
+#SBATCH -p compute
+#SBATCH -N 1                # Total number of nodes requested (16 cores/node)
+#SBATCH -n 1                # Total number of mpi tasks requested
+#SBATCH -t 00:30:00         # Run time (hh:mm:ss) - 1.5 hours
+# The next line is required if the user has more than one project
+#SBATCH -A  # Allocation name to charge job against
+
+TEST="radical.pilot/tests/test_resources/test_rm/test_slurm.py"
+
+cdw
+cd integration_tests
+rm -rf radical.pilot testing
+git clone --branch devel https://github.com/radical-cybertools/radical.pilot.git
+
+module load anaconda/3.7.0
+
+conda create -p testing python=3.7 pytest PyGithub -y
+
+source activate testing
+pip install ./radical.pilot --upgrade
+pytest -vvv $TEST > output.log 2>&1
+
+if test "$?" = 1
+then
+    python radical.pilot/tests/utils/integration_test_issue.py.py 'SDSC Comet' output.log
+fi
+
+sbatch comet.sh
--- tests/utils/integration_test_issue.py
+++ tests/utils/integration_test_issue.py
@@ -7,11 +7,11 @@ token = os.getenv('GIT_TOKEN', '...')
 g = Github(token)
 repo = g.get_repo("radical-cybertools/radical.pilot")
 
-with open(sys.argv[1]) as f:
+with open(sys.argv[2]) as f:
     lines = f.readlines()
 
 i = repo.create_issue(
-    title="Integration Tests failed on SDSC Comet: %s" % date.today(),
+    title="Integration Tests failed on %s : %s" % (sys.argv[1], date.today()),
     body='```python\n' + ''.join(x for x in lines) + '\n```',
     labels=[repo.get_label(name="topic:testing"),
             repo.get_label(name="type:bug"),
