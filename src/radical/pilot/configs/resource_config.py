abb3031187fba893238275045ebecbf15a2680d0
Andre Merzky <andre@merzky.net>
2020-12-16 14:04:48 +0100

    snapshot

--- src/radical/pilot/configs/resource_config.py
+++ src/radical/pilot/configs/resource_config.py
@@ -4,59 +4,106 @@ import radical.utils as ru
 
 PilotConfig :
     {
-        ResourceConfig :
-        {
-            'description'    : str,
-            'lrms'           : str,
-
-            'cores_per_node' : int,
-            'gpus_per_node'  : int,
-            'mem_per_node'   : int,
-            'lfs_per_node'   : int,
-            'lfs_path'       : str,
-        },
-        AccessConfig:
-        {
-            'default'        : str,
-            'options'        :
+        'named_envs' : {
+            str : EnvironmentConfig:
             {
-                str: AccessConfigOption :
-                {
-                    'job_manager'    : str,
-                    'job_manager_hop': str,
-                    'filesystem'     : str,
-                }
+                'kind'  : str,
+                'script': str,
             }
-        }
-        SystemConfig:
+        },
+        'resource' : ResourceConfig:
         {
+            'cfg_name'      : str,
+            'description'   : str,
+            'lrms'          : str,
+            'cores_per_node': int,
+            'gpus_per_node' : int,
+            'mem_per_node'  : int,
+            'lfs_per_node'  : int,
+            'lfs_path'      : str,
+            'access'        : AccessConfig:
+            {
+                'cfg_name'       : str,
+                'job_manager'    : str,
+                'job_manager_hop': str,
+                'filesystem'     : str,
+
+            }
+        },
+        'system' : SystemConfig:
+        {
+            'cfg_name'       :  str,
             'valid_roots'    : [str],
             'sandbox_base'   :  str,
             'python_dist'    :  str,
             'virtenv'        :  str,
             'virtenv_mode'   :  str,
             'rp_version'     :  str,
-            'pre_bootstrap_0': [str],
-            'pre_bootstrap_1': [str],
-            'pre_bootstrap_2': [str],
-            'export_to_task'   : [str],
-            'pre_exec_task'    : [str],
-            'options'        :
-            {
-                str: SystemConfigOption :
-                {
-                   'pre_bootstrap_0': [str],
-                   'pre_bootstrap_1': [str],
-                   'pre_bootstrap_2': [str],
-                   'export_to_task'   : [str],
-                   'pre_exec_task'    : [str],
-                }
-            }
         }
-        AgentConfig:
+        'partitions' :
         [
             PartitionConfig:
+            {
+                'cfg_name'       :  str,
+                'named_env'      :  str,
+                'bridges':
+                [
+                    BridgeConfig:
+                    {
+                        'cfg_name' : str,
+                        'kind'     : str,
+                        'log_lvl'  : str,
+                        'stall_hwm': int,
+                        'bulk_size': int,
+                    }
+                ]
+                'agents':
+                [
+                    AgentConfig:
+                    {
+                        'cfg_name'  : str,
+                        'mode'      : str,
+                        'target'    : str,
+                        'components':
+                        [
+                            ComponentConfig:
+                            {
+                                'count': int,
+                            }
+                        ]
+                    }
+                ]
+                'launch_methods':
+                [
+                    LaunchMethodConfig:
+                    {
+                        'cfg_name'       :  str,
+                        'named_env'      :  str,
+                        'tags'           : [str],
+                        'export_to_task' : [str],
+                        'pre_exec_launch': [str],
+                        'pre_exec_task'  : [str],
+                        'pre_exec_rank'  : {int: [str]},
+                    }
+                ]
+            }
         ]
+    }
+
+
+# ------------------------------------------------------------------------------
+#
+class EnvironmentConfig(ru.Config):
+
+    _schema = {
+            'kind'  : str,
+            'script': str,
+    }
+
+    _defaults = {
+            'kind'  : 'virtualenv',  # conda, conda-pack, module, script
+            'script': '',
+    }
 
 
 # ------------------------------------------------------------------------------
@@ -88,7 +135,7 @@ class ResourceConfig(ru.Config):
 
 # ------------------------------------------------------------------------------
 #
-class AccessConfigOption(ru.Config):
+class AccessConfig(ru.Config):
 
     _schema = {
             'job_manager'    : str,
@@ -100,21 +147,6 @@ class AccessConfigOption(ru.Config):
             'job_manager'    : 'fork://localhost/',
             'job_manager_hop': None,
             'filesystem'     : 'file://localhost/',
-    }
-
-
-# ------------------------------------------------------------------------------
-#
-class AccessConfig(ru.Config):
-
-    _schema = {
-            'default'        : str,
-            'options'        : {str: AccessConfigOption}
-    }
-
-    _defaults = {
-            'default'        : 'fork',
-            'options'        : {'fork': AccessConfigOption()}
     }
 
 
@@ -154,11 +186,11 @@ class SystemConfig(ru.Config):
 
 # ------------------------------------------------------------------------------
 #
-class BridgeOption(ru.Config):
+class BridgeConfig(ru.Config):
 
     _schema = {
             'kind'     : str,
-            'log_lvl'  : str
+            'log_lvl'  : str,
             'stall_hwm': int,
             'bulk_size': int,
     }
@@ -172,33 +204,16 @@ class BridgeOption(ru.Config):
 
 # ------------------------------------------------------------------------------
 #
-class ComponentOption(ru.Config):
+class ComponentConfig(ru.Config):
 
     _schema = {
+            'name'     : str,
             'count'    : int,
     }
 
     defaults = {
+            'name'     : None,
             'count'    : 1,
-    }
-
-
-# ------------------------------------------------------------------------------
-#
-class SubAgentConfig(ru.Config):
-
-    _schema = {
-            'components': {str: ComponentOption},
-    }
-
-    _default = {
-            'components': {
-                'update'               : ComponentOption({'count' : 1}),
-                'agent_staging_input'  : ComponentOption({'count' : 1}),
-                'agent_scheduling'     : ComponentOption({'count' : 1}),
-                'agent_executing'      : ComponentOption({'count' : 1}),
-                'agent_staging_output' : ComponentOption({'count' : 1}),
-            },
     }
 
 
@@ -238,8 +253,6 @@ class AgentConfig(ru.Config):
             'pre_bootstrap_0': [str],
             'pre_bootstrap_1': [str],
             'pre_bootstrap_2': [str],
-            'export_to_task'   : [str],
-            'pre_exec_task'    : [str],
             'launch_methods' : {str: LaunchMethodConfig}
     }
 
@@ -254,7 +267,7 @@ class AgentConfig(ru.Config):
 
 # ------------------------------------------------------------------------------
 #
-class PartitionConfigOption(ru.Config):
+class PartitionConfig(ru.Config):
 
     _schema = {
             'bulk_collection_size' : int,
@@ -270,17 +283,14 @@ class PartitionConfigOption(ru.Config):
             'heartbeat_interval'   : float,
             'heartbeat_timeout'    : float,
 
-            'bridges'              : {str: BridgeOption},
-            'sub_agents'           : [SubAgentConfig],
+            'bridges'              : {str: BridgeConfig},
+            'agents'               : [AgentConfig],
     }
 
     _defaults = {
             'bulk_collection_size' : 1024,
             'bulk_collection_time' : 1.0,
             'db_poll_sleeptime'    : 2.0,
-
-            'target'               : 'local',
-            'mode'                 : 'shared',
 
             'bulk_time'            : 1.0,
             'bulk_size'            : 1024,
@@ -289,38 +299,23 @@ class PartitionConfigOption(ru.Config):
             'heartbeat_timeout'    : 60.0,
 
             'bridges'              : {
-                'agent_scheduling'    : BridgeOption({'kind': 'queue'}),
-                'agent_executing'     : BridgeOption({'kind': 'queue'}),
-                'agent_staging_input' : BridgeOption({'kind': 'queue'})
-                'agent_staging_output': BridgeOption({'kind': 'queue'}),
+                'agent_scheduling'    : BridgeConfig({'kind': 'queue'}),
+                'agent_executing'     : BridgeConfig({'kind': 'queue'}),
+                'agent_staging_input' : BridgeConfig({'kind': 'queue'})
+                'agent_staging_output': BridgeConfig({'kind': 'queue'}),
 
-                'funcs_req'           : BridgeOption({'kind': 'queue'}),
-                'funcs_res'           : BridgeOption({'kind': 'queue'}),
-                'agent_unschedule'    : BridgeOption({'kind': 'pubsub'}),
-                'agent_schedule'      : BridgeOption({'kind': 'pubsub'}),
+                'funcs_req'           : BridgeConfig({'kind': 'queue'}),
+                'funcs_res'           : BridgeConfig({'kind': 'queue'}),
+                'agent_unschedule'    : BridgeConfig({'kind': 'pubsub'}),
+                'agent_schedule'      : BridgeConfig({'kind': 'pubsub'}),
 
-                'control'             : BridgeOption({'kind': 'pubsub'}),
-                'state'               : BridgeOption({'kind': 'pubsub'}),
-              # 'log_pubsub'          : BridgeOption({'kind': 'pubsub'}),
+                'control'             : BridgeConfig({'kind': 'pubsub'}),
+                'state'               : BridgeConfig({'kind': 'pubsub'}),
+              # 'log_pubsub'          : BridgeConfig({'kind': 'pubsub'}),
             },
 
-            'sub_agents'           : [SubAgentConfig()],
+            'agents'               : [AgentConfig()],
 
-    }
-
-
-# ------------------------------------------------------------------------------
-#
-class PartitionConfig(ru.Config):
-
-    _schema = {
-            'default'        : str,
-            'options'        : {str: PartitionConfigOption},
-    }
-
-    _defaults = {
-            'default'        : 'single',
-            'options'        : {'single': PartitionConfigOption()},
     }
 
 
@@ -329,13 +324,28 @@ class PartitionConfig(ru.Config):
 class AgentConfig(ru.Config):
 
     _schema = {
-            'partitions'     : [PartitionConfig],
+            'target'    : str,
+            'mode'      : str,
+            'components': [ComponentConfig],
     }
 
-
-    _defaults = {
-            'partitions'     : [PartitionConfig()],
+    _default = {
+            'target'    : 'local',
+            'mode'      : 'shared',
+            'components': [
+                ComponentConfig({'name' : 'update',
+                                 'count': 1}),
+                ComponentConfig({'name' : 'agent_staging_input',
+                                 'count': 1}),
+                ComponentConfig({'name' : 'agent_scheduling',
+                                 'count': 1}),
+                ComponentConfig({'name' : 'agent_executing',
+                                 'count': 1}),
+                ComponentConfig({'name' : 'agent_staging_output',
+                                 'count': 1}),
+            ],
     }
+
 
 # ------------------------------------------------------------------------------
 #
