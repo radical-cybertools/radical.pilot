c221d3747aeb1ad0ea7ac247fc908b1bda7d4c00
Andre Merzky <andre@merzky.net>
2020-12-18 11:41:39 +0100

    more testing

--- tests/test_raptor/test_raptor.py
+++ tests/test_raptor/test_raptor.py
@@ -28,16 +28,43 @@ class TestWorker(TestCase):
 
         worker = rp.task_overlay.Worker(cfg, session=mock.Mock())
 
-        task = {'cores': 1,
-                'gpus' : 1}
+        task_1 = {'cores': 1, 'gpus' : 1}
+        task_2 = {'cores': 2, 'gpus' : 1}
+        task_3 = {'cores': 3, 'gpus' : 1}
 
         self.assertEqual(worker._resources['cores'], [0, 0, 0, 0, 0, 0, 0, 0])
 
-        worker._alloc_task(task)
+        worker._alloc_task(task_1)
         self.assertEqual(worker._resources['cores'], [1, 0, 0, 0, 0, 0, 0, 0])
+        self.assertEqual(worker._resources['gpus' ], [1, 0])
+        self.assertEqual(task_1['resources'], {'cores': [0], 'gpus': [0]})
 
-        worker._dealloc_task(task)
+        worker._alloc_task(task_2)
+        self.assertEqual(worker._resources['cores'], [1, 1, 1, 0, 0, 0, 0, 0])
+        self.assertEqual(worker._resources['gpus' ], [1, 1])
+        self.assertEqual(task_2['resources'], {'cores': [1, 2], 'gpus': [1]})
+
+        worker._alloc_task(task_3)
+        self.assertEqual(worker._resources['cores'], [1, 1, 1, 0, 0, 0, 0, 0])
+        self.assertEqual(worker._resources['gpus' ], [1, 1])
+        self.assertEqual(task_3.get('resources'), None)
+
+        worker._dealloc_task(task_1)
+        self.assertEqual(worker._resources['cores'], [0, 1, 1, 0, 0, 0, 0, 0])
+        self.assertEqual(worker._resources['gpus' ], [0, 1])
+
+        worker._alloc_task(task_3)
+        self.assertEqual(worker._resources['cores'], [1, 1, 1, 1, 1, 0, 0, 0])
+        self.assertEqual(worker._resources['gpus' ], [1, 1])
+        self.assertEqual(task_3['resources'], {'cores': [0, 3, 4], 'gpus': [0]})
+
+        worker._dealloc_task(task_2)
+        self.assertEqual(worker._resources['cores'], [1, 0, 0, 1, 1, 0, 0, 0])
+        self.assertEqual(worker._resources['gpus' ], [1, 0])
+
+        worker._dealloc_task(task_3)
         self.assertEqual(worker._resources['cores'], [0, 0, 0, 0, 0, 0, 0, 0])
+        self.assertEqual(worker._resources['gpus' ], [0, 0])
 
 
 # ------------------------------------------------------------------------------
