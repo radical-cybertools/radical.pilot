b1be97c41fd9d3bc98f9cba3bede5db7edda03af
Andre Merzky <andre@merzky.net>
2020-12-18 16:20:22 +0100

    lint test

--- src/radical/pilot/worker/__init__.py
+++ src/radical/pilot/worker/__init__.py
@@ -3,7 +3,7 @@ __copyright__ = "Copyright 2016, http://radical.rutgers.edu"
 __license__   = "MIT"
 
 # pylint: disable=unused-wildcard-import,unused-import
-# noqa
+# noqa:   F401
 
 from .update    import Update
 from .stager    import Stager
