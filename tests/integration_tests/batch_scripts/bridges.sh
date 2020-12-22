ae55446f7f9efdad3de79305d9c5714647516d6c
Ioannis Paraskevakos <g.paraskev@rutgers.edu>
2020-12-22 11:05:05 -0500

    refining batch scripts

--- tests/integration_tests/batch_scripts/bridges.sh
+++ tests/integration_tests/batch_scripts/bridges.sh
@@ -2,12 +2,13 @@
 #SBATCH -J rp_integration_test  # Job name
 #SBATCH -o rp_integration_test.%j.out   # Name of stdout output file(%j expands to jobId)
 #SBATCH -e rp_integration_test.%j.err   # Name of stderr output file(%j expands to jobId)
-#SBATCH -p compute
+#SBATCH -p RM
 #SBATCH -N 1                # Total number of nodes requested (16 cores/node)
-#SBATCH -n 1                # Total number of mpi tasks requested
+#SBATCH -n 28                # Total number of mpi tasks requested
 #SBATCH -t 00:30:00         # Run time (hh:mm:ss) - 1.5 hours
 # The next line is required if the user has more than one project
 #SBATCH -A  # Allocation name to charge job against
+#SBATCH -C EGRESS
 
 # ------------------------------------------------------------------------------
 # Test files
@@ -22,7 +23,7 @@ git clone --branch devel https://github.com/radical-cybertools/radical.pilot.git
 
 # ------------------------------------------------------------------------------
 # Python distribution specific. Change if needed.
-module load anaconda/3.7.0
+module load python3conda/3.8.5
 conda create -p testing python=3.7 pytest PyGithub -y
 source activate $PWD/testing
 tmpLOC=`which python`
