2e569f9bbb8ce02bde98a3d56c9da3566fa9d553
Andre Merzky <andre@merzky.net>
2021-01-09 12:09:27 +0100

    silence linter on __init__.py

--- src/radical/pilot/worker/__init__.py
+++ src/radical/pilot/worker/__init__.py
@@ -2,9 +2,6 @@
 __copyright__ = "Copyright 2016, http://radical.rutgers.edu"
 __license__   = "MIT"
 
-# pylint: disable=unused-wildcard-import,unused-import
-# noqa:   F401
-
 from .update    import Update
 from .stager    import Stager
 
