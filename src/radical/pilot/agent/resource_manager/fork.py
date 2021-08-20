980cd11fd7aaa0e7b5ef8def87b3ad4f0cc2e65b
Mikhail Titov <mikhail.a.titov@gmail.com>
2021-08-20 00:35:15 -0400

    updated RM Fork class and corresponding test accordingly

--- src/radical/pilot/agent/resource_manager/fork.py
+++ src/radical/pilot/agent/resource_manager/fork.py
@@ -1,13 +1,10 @@
 
-__copyright__ = "Copyright 2016, http://radical.rutgers.edu"
-__license__   = "MIT"
+__copyright__ = 'Copyright 2016-2021, The RADICAL-Cybertools Team'
+__license__   = 'MIT'
 
-import math
 import multiprocessing
 
-import radical.utils as ru
-
-from .base import ResourceManager, RMInfo
+from .base import ResourceManager
 
 
 # ------------------------------------------------------------------------------
@@ -20,33 +17,30 @@ class Fork(ResourceManager):
 
         ResourceManager.__init__(self, cfg, log, prof)
 
-
     # --------------------------------------------------------------------------
     #
-    def _init_from_scratch(self, info):
-
-        self._log.info("Using fork on localhost.")
+    def _update_info(self, info):
 
         # check if the requested cores are available
         detected_cores = multiprocessing.cpu_count()
 
         if detected_cores != info.requested_cores:
             if self._cfg.resource_cfg.fake_resources:
-                self._log.info("using %d instead of available %d cores.",
-                               info.requested_cores, detected_cores)
+                self._log.info(
+                    'using %d requested cores instead of %d available cores',
+                    info.requested_cores, detected_cores)
             else:
                 if info.requested_cores > detected_cores:
-                    raise RuntimeError('insufficient cores found (%d < %d'
-                            % (detected_cores, info.requested_cores))
+                    raise RuntimeError('insufficient cores found (%d < %d)' %
+                                       (detected_cores, info.requested_cores))
 
         # FIXME: number of GPUs should also be checked - how?
         # FIXME: we should use hwlock if available
 
-        info.node_list = [["localhost", 'localhost_%d' % i]
+        info.node_list = [['localhost', 'localhost_%d' % i]
                           for i in range(info.requested_nodes)]
 
         return info
-
 
 # ------------------------------------------------------------------------------
 
