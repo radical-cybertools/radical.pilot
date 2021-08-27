152ad4d494af82a0013943c73e93b87ecae8740f
Mikhail Titov <mikhail.a.titov@gmail.com>
2021-08-26 22:11:20 -0400

    updated RM Cobalt and corresponding tests

--- tests/unit_tests/test_rm/test_cobalt.py
+++ tests/unit_tests/test_rm/test_cobalt.py
@@ -1,44 +1,60 @@
+#!/usr/bin/env python3
+
 # pylint: disable=protected-access, unused-argument, no-value-for-parameter
+
+__copyright__ = 'Copyright 2021, The RADICAL-Cybertools Team'
+__license__   = 'MIT'
 
 import os
 
-import radical.utils as ru
-
 from unittest import mock, TestCase
 
+from radical.pilot.agent.resource_manager        import RMInfo
 from radical.pilot.agent.resource_manager.cobalt import Cobalt
 
 
 # ------------------------------------------------------------------------------
 #
-class TestCobalt(TestCase):
+class CobaltTestCase(TestCase):
 
     # --------------------------------------------------------------------------
     #
     @mock.patch.object(Cobalt, '__init__', return_value=None)
     @mock.patch('radical.utils.Logger')
-    def test_configure(self, mocked_logger, mocked_init):
+    def test_update_info(self, mocked_logger, mocked_init):
 
         os.environ['COBALT_PARTNAME'] = '1'
 
-        component = Cobalt(cfg=None, session=None)
-        component._log = mocked_logger
-        component._cfg = ru.Config(from_dict={'cores_per_node': 16})
-        component._configure()
+        rm_cobalt = Cobalt(cfg=None, log=None, prof=None)
+        rm_cobalt._log = mocked_logger
 
-        self.assertEqual(component.node_list, [['nid00001', '1']])
-        self.assertEqual(component.cores_per_node, 16)
-        self.assertEqual(component.gpus_per_node, 0)
-        self.assertEqual(component.lfs_per_node, {'path': None, 'size': 0})
+        rm_info = rm_cobalt._update_info(RMInfo())
+
+        self.assertEqual(rm_info.node_list, [['nid00001', '1']])
+
+    # --------------------------------------------------------------------------
+    #
+    @mock.patch.object(Cobalt, '__init__', return_value=None)
+    @mock.patch('radical.utils.Logger')
+    def test_update_info_error(self, mocked_logger, mocked_init):
+
+        rm_cobalt = Cobalt(cfg=None, log=None, prof=None)
+        rm_cobalt._log = mocked_logger
+
+        for cobalt_env_var in ['COBALT_NODEFILE', 'COBALT_PARTNAME']:
+            if cobalt_env_var in os.environ:
+                del os.environ[cobalt_env_var]
+        with self.assertRaises(RuntimeError):
+            # both $COBALT_NODEFILE and $COBALT_PARTNAME were not set
+            rm_cobalt._update_info(None)
 
 # ------------------------------------------------------------------------------
 
 
 if __name__ == '__main__':
 
-    tc = TestCobalt()
-    tc.test_configure()
+    tc = CobaltTestCase()
+    tc.test_update_info()
 
 
 # ------------------------------------------------------------------------------
-# pylint: enable=protected-access, unused-argument, no-value-for-parameter
