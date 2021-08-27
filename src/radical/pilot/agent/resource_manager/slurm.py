8d166aa6cedea9436be016b2f055281e8de252fb
Mikhail Titov <mikhail.a.titov@gmail.com>
2021-08-26 21:29:24 -0400

    updated RM Slurm and corresponding tests

--- src/radical/pilot/agent/resource_manager/slurm.py
+++ src/radical/pilot/agent/resource_manager/slurm.py
@@ -1,7 +1,6 @@
 
-__copyright__ = "Copyright 2016, http://radical.rutgers.edu"
-__license__   = "MIT"
-
+__copyright__ = 'Copyright 2016-2021, The RADICAL-Cybertools Team'
+__license__   = 'MIT'
 
 import os
 
@@ -20,86 +19,56 @@ class Slurm(ResourceManager):
 
         ResourceManager.__init__(self, cfg, log, prof)
 
-
     # --------------------------------------------------------------------------
     #
-    def _init_from_scratch(self):
+    def _update_info(self, info):
 
         slurm_nodelist = os.environ.get('SLURM_NODELIST')
         if slurm_nodelist is None:
-            msg = "$SLURM_NODELIST not set!"
-            self._log.error(msg)
-            raise RuntimeError(msg)
+            raise RuntimeError('$SLURM_NODELIST not set')
 
         # Parse SLURM nodefile environment variable
         slurm_nodes = ru.get_hostlist(slurm_nodelist)
-        self._log.info("Found SLURM_NODELIST %s. Expanded to: %s", slurm_nodelist, slurm_nodes)
+        self._log.info('found SLURM_NODELIST %s. Expanded to: %s',
+                       slurm_nodelist, slurm_nodes)
 
         # $SLURM_NPROCS = Total number of cores allocated for the current job
         slurm_nprocs_str = os.environ.get('SLURM_NPROCS')
         if slurm_nprocs_str is None:
-            msg = "$SLURM_NPROCS not set!"
-            self._log.error(msg)
-            raise RuntimeError(msg)
-        else:
-            slurm_nprocs = int(slurm_nprocs_str)
+            raise RuntimeError('$SLURM_NPROCS not set')
+        slurm_nprocs = int(slurm_nprocs_str)
 
-        # $SLURM_NNODES = Total number of (partial) nodes in the job's resource allocation
+        # $SLURM_NNODES = Total number of (partial) nodes in
+        # the job's resource allocation
         slurm_nnodes_str = os.environ.get('SLURM_NNODES')
         if slurm_nnodes_str is None:
-            msg = "$SLURM_NNODES not set!"
-            self._log.error(msg)
-            raise RuntimeError(msg)
-        else:
-            slurm_nnodes = int(slurm_nnodes_str)
+            raise RuntimeError('$SLURM_NNODES not set')
+        slurm_nnodes = int(slurm_nnodes_str)
 
         # $SLURM_CPUS_ON_NODE = Number of cores per node (physically)
         slurm_cpus_on_node_str = os.environ.get('SLURM_CPUS_ON_NODE')
         if slurm_cpus_on_node_str is None:
-            msg = "$SLURM_CPUS_ON_NODE not set!"
-            self._log.error(msg)
-            raise RuntimeError(msg)
-        else:
-            slurm_cpus_on_node = int(slurm_cpus_on_node_str)
+            raise RuntimeError('$SLURM_CPUS_ON_NODE not set')
+        slurm_cpus_on_node = int(slurm_cpus_on_node_str)
 
         # Verify that $SLURM_NPROCS <= $SLURM_NNODES * $SLURM_CPUS_ON_NODE
-        if not slurm_nprocs <= slurm_nnodes * slurm_cpus_on_node:
-            self._log.warning("$SLURM_NPROCS(%d) <= $SLURM_NNODES(%d) * $SLURM_CPUS_ON_NODE(%d)",
-                            slurm_nprocs, slurm_nnodes, slurm_cpus_on_node)
+        if slurm_nprocs > slurm_nnodes * slurm_cpus_on_node:
+            self._log.warning('$SLURM_NPROCS(%d) > $SLURM_NNODES(%d) * ' +
+                              '$SLURM_CPUS_ON_NODE(%d)',
+                              slurm_nprocs, slurm_nnodes, slurm_cpus_on_node)
 
         # Verify that $SLURM_NNODES == len($SLURM_NODELIST)
         if slurm_nnodes != len(slurm_nodes):
-            self._log.error("$SLURM_NNODES(%d) != len($SLURM_NODELIST)(%d)",
-                           slurm_nnodes, len(slurm_nodes))
+            self._log.error('$SLURM_NNODES(%d) != len($SLURM_NODELIST)(%d)',
+                            slurm_nnodes, len(slurm_nodes))
 
-        # Report the physical number of cores or the total number of cores
-        # in case of a single partial node allocation.
-        self.cores_per_node = self._cfg.get('cores_per_node', 0)
-        self.gpus_per_node  = self._cfg.get('gpus_per_node',  0)  # FIXME GPU
-        self.mem_per_node   = self._cfg.get('mem_per_node',   0)
+        if not info.cores_per_node:
+            info.cores_per_node = min(slurm_cpus_on_node, slurm_nprocs)
 
-        self._log.debug('lfs path %s', ru.expand_env(
-                                           self._cfg.get('lfs_path_per_node')))
-        self.lfs_per_node   = {'path': ru.expand_env(
-                                       self._cfg.get('lfs_path_per_node')),
-                               'size': self._cfg.get('lfs_size_per_node', 0)}
+        info.node_list = [[name, str(idx + 1)]
+                          for idx, name in enumerate(sorted(slurm_nodes))]
 
-        if not self.cores_per_node:
-            self.cores_per_node = min(slurm_cpus_on_node, slurm_nprocs)
-
-
-        # node names are unique, so can serve as node uids
-        self.node_list = [[node, node] for node in slurm_nodes]
-
-        self.lm_info['cores_per_node'] = self.cores_per_node
-
-      # # once we are done, we remove all `SLURM_*` and `SBATCH_*` env variables
-      # # as to not confuse srun executions
-      # for k in list(os.environ.keys()):
-      #     if k.startswith('SLURM_') or k.startswith('SBATCH_'):
-      #         print 'unset %s' % k
-      #         del(os.environ[k])
-
+        return info
 
 # ------------------------------------------------------------------------------
 
