66d4322f6f6696d9e61e4ace836388e79ba9911c
Mikhail Titov <mikhail.a.titov@gmail.com>
2021-08-27 01:13:38 -0400

    updated RM PBSPro and corresponding tests

--- src/radical/pilot/agent/resource_manager/pbspro.py
+++ src/radical/pilot/agent/resource_manager/pbspro.py
@@ -1,7 +1,6 @@
 
-__copyright__ = "Copyright 2016, http://radical.rutgers.edu"
-__license__   = "MIT"
-
+__copyright__ = 'Copyright 2016-2021, The RADICAL-Cybertools Team'
+__license__   = 'MIT'
 
 import os
 import subprocess
@@ -21,20 +20,15 @@ class PBSPro(ResourceManager):
 
         ResourceManager.__init__(self, cfg, log, prof)
 
-
     # --------------------------------------------------------------------------
     #
-    def _init_from_scratch(self):
+    def _update_info(self, info):
         # TODO: $NCPUS?!?! = 1 on archer
 
         pbspro_nodefile = os.environ.get('PBS_NODEFILE')
-
         if pbspro_nodefile is None:
-            msg = "$PBS_NODEFILE not set!"
-            self._log.error(msg)
-            raise RuntimeError(msg)
-
-        self._log.info("Found PBSPro $PBS_NODEFILE %s." % pbspro_nodefile)
+            raise RuntimeError('$PBS_NODEFILE not set')
+        self._log.info('PBS_NODEFILE: %s', pbspro_nodefile)
 
         # Dont need to parse the content of nodefile for PBSPRO, only the length
         # is interesting, as there are only duplicate entries in it.
@@ -42,12 +36,9 @@ class PBSPro(ResourceManager):
         pbspro_nodes_length = len(pbspro_nodes)
 
         # Number of Processors per Node
-        val = os.environ.get('NUM_PPN')
+        val = os.environ.get('NUM_PPN') or os.environ.get('SAGA_PPN')
         if not val:
-            val = os.environ.get('SAGA_PPN')
-
-        if not val:
-            raise RuntimeError("$NUM_PPN / $SAGA_PPN not set!")
+            raise RuntimeError('$NUM_PPN and $SAGA_PPN not set!')
 
         pbspro_num_ppn = int(val)
 
@@ -57,7 +48,7 @@ class PBSPro(ResourceManager):
             pbspro_node_count = int(val)
         else:
             pbspro_node_count = len(set(pbspro_nodes))
-            self._log.warn("$NODE_COUNT not set - use %d" % pbspro_node_count)
+            self._log.warn('$NODE_COUNT not set - use %d', pbspro_node_count)
 
         # Number of Parallel Environments
         val = os.environ.get('NUM_PES')
@@ -65,7 +56,8 @@ class PBSPro(ResourceManager):
             pbspro_num_pes = int(val)
         else:
             pbspro_num_pes = len(pbspro_nodes)
-            self._log.warn("$NUM_PES not set - use %d" % pbspro_num_pes)
+            self._log.warn('$NUM_PES not set - use %d', pbspro_num_pes)
+
         try:
             pbspro_vnodes = self._parse_pbspro_vnodes()
         except:
@@ -73,34 +65,31 @@ class PBSPro(ResourceManager):
             raise
 
         # Verify that $NUM_PES == $NODE_COUNT * $NUM_PPN == len($PBS_NODEFILE)
-        if not (pbspro_node_count * pbspro_num_ppn == pbspro_num_pes == pbspro_nodes_length):
-            self._log.warning("NUM_PES != NODE_COUNT * NUM_PPN != len($PBS_NODEFILE)")
+        if not (pbspro_node_count * pbspro_num_ppn
+                == pbspro_num_pes
+                == pbspro_nodes_length):
+            self._log.warning(
+                'NUM_PES != NODE_COUNT * NUM_PPN != len($PBS_NODEFILE)')
+
+        info.cores_per_node = pbspro_num_ppn
 
         # node names are unique, so can serve as node uids
-        self.node_list      = [[node, node] for node in pbspro_vnodes]
-        self.cores_per_node = pbspro_num_ppn
-        self.gpus_per_node  = self._cfg.get('gpus_per_node', 0)  # FIXME GPU
+        info.node_list = [[name, str(idx + 1)]
+                          for idx, name in enumerate(sorted(pbspro_vnodes))]
 
-        self.lfs_per_node   = {'path': ru.expand_env(
-                                       self._cfg.get('lfs_path_per_node')),
-                               'size': self._cfg.get('lfs_size_per_node', 0)}
-
+        return info
 
     # --------------------------------------------------------------------------
     #
     def _parse_pbspro_vnodes(self):
 
         # PBS Job ID
-        val = os.environ.get('PBS_JOBID')
-        if val:
-            pbspro_jobid = val
-        else:
-            msg = "$PBS_JOBID not set!"
-            self._log.error(msg)
-            raise RuntimeError(msg)
+        pbspro_jobid = os.environ.get('PBS_JOBID')
+        if not pbspro_jobid:
+            raise RuntimeError('$PBS_JOBID not set')
 
         # Get the output of qstat -f for this job
-        output = subprocess.check_output(["qstat", "-f", pbspro_jobid])
+        output = subprocess.check_output(['qstat', '-f', pbspro_jobid])
 
         # Get the (multiline) 'exec_vnode' entry
         vnodes_str = ''
@@ -111,14 +100,14 @@ class PBSPro(ResourceManager):
                 vnodes_str += line.strip()
             elif vnodes_str:
                 # Find continuing lines
-                if " = " not in line:
+                if ' = ' not in line:
                     vnodes_str += line.strip()
                 else:
                     break
 
         # Get the RHS of the entry
-        rhs = vnodes_str.split('=',1)[1].strip()
-        self._log.debug("input: %s", rhs)
+        rhs = vnodes_str.split('=', 1)[1].strip()
+        self._log.debug('input: %s', rhs)
 
         nodes_list = []
         # Break up the individual node partitions into vnode slices
@@ -140,18 +129,19 @@ class PBSPro(ResourceManager):
             for _slice in slices:
                 vnode, cpus = _slice.split(':')
                 cpus = int(cpus.split('=')[1])
-                self._log.debug("vnode: %s cpus: %s", vnode, cpus)
+                self._log.debug('vnode: %s cpus: %s', vnode, cpus)
                 vnodes_list.append(vnode)
                 cpus_list.append(cpus)
 
-        self._log.debug("vnodes: %s", vnodes_list)
-        self._log.debug("cpus: %s", cpus_list)
+        self._log.debug('vnodes: %s', vnodes_list)
+        self._log.debug('cpus: %s', cpus_list)
 
         cpus_list = list(set(cpus_list))
         min_cpus = int(min(cpus_list))
 
         if len(cpus_list) > 1:
-            self._log.debug("Detected vnodes of different sizes: %s, the minimal is: %d.", cpus_list, min_cpus)
+            self._log.debug('Detected vnodes of different sizes: %s, ' +
+                            'the minimal is: %d.', cpus_list, min_cpus)
 
         node_list = []
         for vnode in vnodes_list:
@@ -159,11 +149,10 @@ class PBSPro(ResourceManager):
 
         # only unique node names
         node_list = list(set(node_list))
-        self._log.debug("Node list: %s", node_list)
+        self._log.debug('Node list: %s', node_list)
 
         # Return the list of node names
-        return node_list
-
+        return sorted(node_list)
 
 # ------------------------------------------------------------------------------
 
