770502321b9d6b8cf224ce0be6ed505d2df38856
Andre Merzky <andre@merzky.net>
2022-03-16 20:39:25 +0100

    code cleanup

--- src/radical/pilot/pytask.py
+++ src/radical/pilot/pytask.py
@@ -5,33 +5,43 @@ __license__   = 'MIT'
 import inspect
 import functools
 
+from types import Callable
+
 from .utils import serialize_obj, serialize_bson
 from .utils import deserialize_obj, deserialize_bson
 
 
+# ------------------------------------------------------------------------------
+#
 class PythonTask(object):
 
+    # --------------------------------------------------------------------------
+    #
     def __new__(cls, func, *args, **kwargs):
-        """
+        '''
         We handle wrapped functions here with no args or kwargs.
+
         Example:
-        import PythonTask
-        wrapped_func   = partial(func_A, func_AB)      
-        cud.EXECUTABLE = PythonTask(wrapped_func)
-        """
+            import PythonTask
+            wrapped_func   = partial(func_A, func_AB)
+            cud.EXECUTABLE = PythonTask(wrapped_func)
+        '''
+
         if not inspect.isfunction(func):
             raise ValueError('task function not callable')
 
-        ser_func = serialize_obj(func)
-        TASK = {'func'  :ser_func,
-                'args'  :args,
-                'kwargs':kwargs}
+        task = {'func'  : serialize_obj(func),
+                'args'  : args,
+                'kwargs': kwargs}
         try:
-            SER_TASK = serialize_bson(TASK)
-            return SER_TASK
+            return serialize_bson(task)
+
         except Exception as e:
             raise ValueError from e
 
+
+    # --------------------------------------------------------------------------
+    #
     @staticmethod
     def get_func_attr(bson_obj):
 
@@ -40,32 +50,43 @@ class PythonTask(object):
 
         pytask = deserialize_bson(bson_obj)
         func   = deserialize_obj(pytask['func'])
+
         return func, pytask['args'], pytask['kwargs']
 
-    def pythontask(f):
-        """
+
+    # --------------------------------------------------------------------------
+    #
+    def pythontask(f: Callable):
+        '''
         We handle all other functions here.
+
         Example:
-        from PythonTask import pythonfunc as pythonfunc
-        @pythontask
-        def func_C(x):
-            return (x)
-        cud.EXECUTABLE = func_C(2)
-        """
+            from PythonTask import pythonfunc as pythonfunc
+            @pythontask
+            def func_C(x):
+                return (x)
+            cud.EXECUTABLE = func_C(2)
+        '''
 
         if not inspect.isfunction(f):
             raise ValueError('task function not callable')
 
+        # ----------------------------------------------------------------------
         @functools.wraps(f)
-        def decor(*args, **kwargs): 
-            ser_func = serialize_obj(f)
+        def decor(*args, **kwargs):
 
-            TASK = {'func'  :ser_func,
-                    'args'  :args,
-                    'kwargs':kwargs}
+            task = {'func'  : serialize_obj(f),
+                    'args'  : args,
+                    'kwargs': kwargs}
             try:
-                SER_TASK = serialize_bson(TASK)
-                return SER_TASK
+                return serialize_bson(task)
+
             except Exception as e:
                 raise ValueError ('failed to wrap function') from e
+
         return decor
+        # ----------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+
