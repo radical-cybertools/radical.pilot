a35bad2ff00d247fb5d4433994348aa3b2ac040c
Andre Merzky <andre@merzky.net>
2022-04-13 13:02:46 +0200

    linting

--- docs/architecture/api_draft/__init__.py
+++ docs/architecture/api_draft/__init__.py
@@ -9,18 +9,16 @@ from callback                      import Callback
 from attributes                    import Attributes
 from description                   import Description
 
-from task_description      import TaskDescription
+from task_description              import TaskDescription
 from data_task_description         import DataTaskDescription
 
 from task                          import Task
-from task                  import Task
 from data_task                     import DataTask
 
-from pilot_description     import PilotDescription
+from pilot_description             import PilotDescription
 from data_pilot_description        import DataPilotDescription
 
 from pilot                         import Pilot
-from pilot                 import Pilot
 from data_pilot                    import DataPilot
 
 from pilot_manager                 import PilotManager
