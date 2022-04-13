06eec98187f353fa2c5bc50fb1fc60f0693d896a
Andre Merzky <andre@merzky.net>
2022-04-13 14:03:24 +0200

    Update src/radical/pilot/raptor/worker.py
    Co-authored-by: M. Eric Irrgang <ericirrgang@gmail.com>

--- src/radical/pilot/raptor/worker.py
+++ src/radical/pilot/raptor/worker.py
@@ -11,7 +11,7 @@ from .. import constants as rpc
 # ------------------------------------------------------------------------------
 #
 class Worker(object):
-
+"""Implement the Raptor protocol for dispatching multiple Tasks on persistent resources."""
     # --------------------------------------------------------------------------
     #
     def __init__(self, cfg=None, register=True, session=None):
