a4eb279161cb677706bede2beee8f84f61e19f13
kartikmodi <kartik+git@modi.dev>
2022-05-10 00:17:09 -0400

    excluding a nb for build time reduction update #2553

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,7 +95,7 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['3/3.9-setup-task-environment.ipynb']
+exclude_patterns = ['3/3.9-setup-task-environment.ipynb','3/3.11-using-pre-and-post-exec-commands.ipynb']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
