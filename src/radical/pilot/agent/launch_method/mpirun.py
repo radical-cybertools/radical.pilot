44bc1f612849564ecaaf5451af7a6d8dd9875873
Mikhail Titov <mikhail.a.titov@gmail.com>
2022-08-04 19:20:11 -0400

    fixed linting

--- src/radical/pilot/agent/launch_method/mpirun.py
+++ src/radical/pilot/agent/launch_method/mpirun.py
@@ -170,7 +170,7 @@ class MPIRun(LaunchMethod):
                 # FIXME: inform this proc about the GPU to be used
 
             if '_dplace' in self.name and save_list:
-                assert(save_list == core_list), 'inhomog. core sets (dplace)'
+                assert (save_list == core_list), 'inhomog. core sets (dplace)'
             else:
                 save_list = core_list
 
