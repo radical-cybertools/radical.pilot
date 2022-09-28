3a58360712328ff77b9a147f4683995869d9eace
Mikhail Titov <mikhail.a.titov@gmail.com>
2022-09-28 18:49:12 -0400

    introduced 2 JSRun LMs: `JSRUN` and `JSRUN_ERF`

--- src/radical/pilot/agent/launch_method/base.py
+++ src/radical/pilot/agent/launch_method/base.py
@@ -1,7 +1,7 @@
 
 # pylint: disable=protected-access
 
-__copyright__ = 'Copyright 2016-2021, The RADICAL-Cybertools Team'
+__copyright__ = 'Copyright 2016-2022, The RADICAL-Cybertools Team'
 __license__   = 'MIT'
 
 import os
@@ -22,6 +22,7 @@ LM_NAME_MPIRUN_CCMRUN = 'MPIRUN_CCMRUN'
 LM_NAME_MPIRUN_DPLACE = 'MPIRUN_DPLACE'
 LM_NAME_MPIRUN_RSH    = 'MPIRUN_RSH'
 LM_NAME_JSRUN         = 'JSRUN'
+LM_NAME_JSRUN_ERF     = 'JSRUN_ERF'
 LM_NAME_PRTE          = 'PRTE'
 LM_NAME_FLUX          = 'FLUX'
 LM_NAME_RSH           = 'RSH'
@@ -110,6 +111,7 @@ class LaunchMethod(object):
         from .mpiexec        import MPIExec
         from .mpirun         import MPIRun
         from .jsrun          import JSRUN
+        from .jsrun_erf      import JSRUN_ERF
         from .prte           import PRTE
         from .flux           import Flux
         from .rsh            import RSH
@@ -130,6 +132,7 @@ class LaunchMethod(object):
                 LM_NAME_MPIRUN_MPT    : MPIRun,
                 LM_NAME_MPIRUN_DPLACE : MPIRun,
                 LM_NAME_JSRUN         : JSRUN,
+                LM_NAME_JSRUN_ERF     : JSRUN_ERF,
                 LM_NAME_PRTE          : PRTE,
                 LM_NAME_FLUX          : Flux,
                 LM_NAME_RSH           : RSH,
--- src/radical/pilot/agent/launch_method/jsrun.py
+++ src/radical/pilot/agent/launch_method/jsrun.py
@@ -1,6 +1,6 @@
 
-__copyright__ = "Copyright 2016, http://radical.rutgers.edu"
-__license__   = "MIT"
+__copyright__ = 'Copyright 2016-2022, The RADICAL-Cybertools Team'
+__license__   = 'MIT'
 
 
 import radical.utils as ru
@@ -69,110 +69,35 @@ class JSRUN(LaunchMethod):
 
     # --------------------------------------------------------------------------
     #
-    def _create_resource_set_file(self, slots, uid, sandbox):
-        """
-        This method takes as input a Task slots and creates the necessary
-        resource set file. This resource set file is then used by jsrun to
-        place and execute tasks on nodes.
-
-        An example of a resource file is:
-
-        * Task 1: 2 MPI procs, 2 threads per process and 2 gpus per process*
-
-            rank 0 : {host: 1; cpu:  {0, 1}; gpu: {0,1}}
-            rank 1 : {host: 1; cpu: {22,23}; gpu: {3,4}}
-
-        * Task 2: 2 MPI procs, 1 thread per process and 1 gpus per process*
-
-            rank 0 : {host: 2; cpu:  7; gpu: 2}
-            rank 1 : {host: 2; cpu: 30; gpu: 5}
-
-        * Task 3: 1 proc, 1 thread per process*
-
-            1 : {host: 2; cpu:  7}
-
-        Parameters
-        ----------
-        slots : List of dictionaries.
-
-            The slots that the task will be placed. A slot has the following
-            format:
-
-            {"ranks"         : [{"node_name" : "a",
-                                 "node_id"   : 1,
-                                 "core_map"  : [[0]],
-                                 "gpu_map"   : [],
-                                 "lfs"       : 0,
-                                 "mem"       : 0
-                                }]
-            }
-
-        uid     : task ID (string)
-        sandbox : task sandbox (string)
-        mpi     : MPI or not (bool, default: False)
-
-        """
-
-        # https://github.com/olcf-tutorials/ERF-CPU-Indexing
-        # `cpu_index_using: physical` causes the following issue
-        # "error in ptssup_mkcltsock_afunix()"
-        rs_str  = 'cpu_index_using: logical\n'
-        rank_id = 0
-        for rank in slots['ranks']:
-
-            gpu_maps = list(rank['gpu_map'])
-            for map_set in rank['core_map']:
-                cores = ','.join(str(core) for core in map_set)
-                rs_str += 'rank: %d: {' % rank_id
-                rs_str += ' host: %s;'  % str(rank['node_id'])
-                rs_str += ' cpu: {%s}'  % cores
-                if gpu_maps:
-                    gpus = [str(gpu_map[0]) for gpu_map in gpu_maps]
-                    rs_str += '; gpu: {%s}' % ','.join(gpus)
-                rs_str  += '}\n'
-                rank_id += 1
-
-        rs_name = '%s/%s.rs' % (sandbox, uid)
-        with ru.ru_open(rs_name, 'w') as fout:
-            fout.write(rs_str)
-
-        return rs_name
-
-
-    # --------------------------------------------------------------------------
-    #
     def get_launch_cmds(self, task, exec_path):
 
-        uid   = task['uid']
-        slots = task['slots']
-        td    = task['description']
-        sbox  = task['task_sandbox_path']
-
-        assert slots['ranks'], 'task.slots.ranks is not set'
+        uid = task['uid']
+        td  = task['description']
 
         self._log.debug('prep %s', uid)
 
-        # from https://www.olcf.ornl.gov/ \
-        #             wp-content/uploads/2018/11/multi-gpu-workshop.pdf
-        #
-        # CUDA with    MPI, use jsrun --smpiargs="-gpu"
-        # CUDA without MPI, use jsrun --smpiargs="off"
-        #
-        # We only set this for CUDA tasks
-        if 'cuda' in td.get('gpu_type', '').lower():
-            if td['ranks'] > 1:
-                # MPI is enabled
-                smpiargs = '--smpiargs="-gpu"'
-            else:
-                smpiargs = '--smpiargs="off"'
-        else:
-            smpiargs = ''
+        cmd_options = '–brs -n%(ranks)d -a1 '   \
+                      '-c%(cores_per_rank)d' % td
 
-        rs_fname = self._create_resource_set_file(slots=slots, uid=uid,
-                                                  sandbox=sbox)
+        if td['gpus_per_rank']:
 
-        cmd = '%s --erf_input %s %s %s' % (self._command, rs_fname,
-                                           smpiargs, exec_path)
+            cmd_options += ' -g%(gpus_per_rank)d' % td
+
+            # from https://www.olcf.ornl.gov/ \
+            #             wp-content/uploads/2018/11/multi-gpu-workshop.pdf
+            #
+            # CUDA with    MPI, use jsrun --smpiargs="-gpu"
+            # CUDA without MPI, use jsrun --smpiargs="off"
+            #
+            # This is set for CUDA tasks only
+            if 'cuda' in td.get('gpu_type', '').lower():
+                if td['ranks'] > 1:
+                    # MPI is enabled
+                    cmd_options += ' --smpiargs="-gpu"'
+                else:
+                    cmd_options += ' --smpiargs="off"'
+
+        cmd = '%s %s %s' % (self._command, cmd_options, exec_path)
         return cmd.rstrip()
 
 
