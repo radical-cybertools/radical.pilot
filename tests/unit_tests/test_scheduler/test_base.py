0db37eb8688b40e4801111277740851d05b7092c
Andre Merzky <andre@merzky.net>
2023-04-25 03:36:18 +0200

    fix log mocking

--- tests/unit_tests/test_scheduler/test_base.py
+++ tests/unit_tests/test_scheduler/test_base.py
@@ -101,7 +101,7 @@ class TestBaseScheduling(TestCase):
         rpa_sb._debug = True
         sched = AgentSchedulingComponent(cfg=None, session=None)
         sched._log = ru.Logger('foo', targets=None, level='DEBUG_5')
-        sched._log._debug_level = 3
+        sched._log._debug_level = 6
 
         for c in self._test_cases['slot_status']:
             sched.nodes = c['nodes']
@@ -127,7 +127,7 @@ class TestBaseScheduling(TestCase):
         component._prof       = mock.Mock()
         component._prof.prof  = mock.Mock(return_value=True)
 
-        component._log._debug_level = 3
+        component._log._debug_level = 6
 
         # FIXME: the try_allocation part in the test config has no results?
         for c in self._test_cases['try_allocation']:
--- tests/unit_tests/test_scheduler/test_continuous.py
+++ tests/unit_tests/test_scheduler/test_continuous.py
@@ -75,7 +75,7 @@ class TestContinuous(TestCase):
         component._log  = ru.Logger('foo', targets=None, level='OFF')
         component._prof = mock.Mock()
 
-        component._log._debug_level = 3
+        component._log._debug_level = 6
 
         # test `_schedule_incoming` and `_schedule_waitpool`
 
