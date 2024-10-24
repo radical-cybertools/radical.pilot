c22a22fd42d8ba66c21d0c505f1f09f39ff51eb6
Andre Merzky <andre@merzky.net>
2024-10-24 12:08:05 +0200

    up

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -126,7 +126,8 @@ napoleon_numpy_docstring = False
 
 # -- rtds settings -------------------------------------------------------------
 # The "prefix" used in the `upload-artifact` step of the action
-rtds_action_artifact_prefix = "notebooks-for-"
+rtds_action_github_repo = "radical-cybertools/radical.pilot"
+rtds_action_artifact_prefix = "rtd-"
 rtds_action_github_token = os.environ.get("GITHUB_TOKEN")
 rtds_action_error_if_missing = False
 
