55b13cc4d591dd91a871c8db4966cd8f7f3ca5a4
Andre Merzky <andre@merzky.net>
2024-11-01 00:57:36 +0100

    fix example

--- examples/misc/service_tasks.py
+++ examples/misc/service_tasks.py
@@ -26,26 +26,24 @@ def app():
         for idx in range(n_nodes):
             pdesc['services'].append(
                 rp.TaskDescription({
-                    'uid'           : 'my_service_%02d' % idx,
+                    'uid'           : 'test_service_%02d' % idx,
                     'executable'     : __file__,
-                    'arguments'      : ['service'],
+                    'arguments'      : ['service', 1024 + 1 + idx],
                     'stdout'         : '/tmp/watcher_%d.out' % idx,
                     'ranks'          : 1,
                     'cores_per_rank' : 1,
-                  # 'tags': {'colocate': str(idx),
-                  #          'exclusive': True},
-                    'named_env': 'rp',
-                  # 'pre_exec': ['hostname','conda activate ve.rct']
+                    'named_env'      : 'rp',
                 })
             )
         pilot = pmgr.submit_pilots(pdesc)
         tmgr.add_pilots(pilot)
 
         td = rp.TaskDescription({'executable': '/bin/date',
-                                 'services'  : ['my_service_00',
-                                                'my_service_01']})
+                                 'services'  : ['test_service_00',
+                                                'test_service_01']})
         task = tmgr.submit_tasks(td)
         tmgr.wait_tasks(task.uid)
+        print(' * %s: %s' % (task.uid, task.stdout))
 
       # pilot.register_service('ext_service', 'tcp://localhost:12345')
       #
@@ -53,39 +51,39 @@ def app():
       #                          'services'  : ['ext_service']})
       # task = tmgr.submit_tasks(td)
       # tmgr.wait_tasks(task.uid)
-      #
-      # # ---------------------------------------------------------------------
-      # # start SERVICE instance and wait for startup info
-      # sd = rp.TaskDescription(
-      #      {'uid'          : 'my_service',
-      #       'mode'         : rp.TASK_SERVICE,
-      #       'executable'   : __file__,
-      #       'arguments'    : ['service'],
-      #       'info_pattern' : 'stdout:.*port: ([0-9]*).*',
-      #       'timeout'      : 10,  # startup timeout
-      #       'named_env'    : 'rp',
-      #       })
-      # service = tmgr.submit_tasks(sd)
-      #
-      # # collect the endpoint port
-      # info = service.wait_info()
-      # print('found %s: %s' % (service.uid, info))
-      #
-      #
-      # # ---------------------------------------------------------------------
-      # # run the clients
-      # cds = list()
-      # for i in range(4):
-      #     cd = rp.TaskDescription({'services'   : [service.uid],
-      #                              'executable' : __file__,
-      #                              'arguments'  : ['client']})
-      #     cds.append(cd)
-      #
-      # clients = tmgr.submit_tasks(cds)
-      # tmgr.wait_tasks(uids=[client.uid for client in clients])
-      #
-      # for client in clients:
-      #     print('%s: %s' % (client.uid, client.stdout))
+
+        # ---------------------------------------------------------------------
+        # start SERVICE instance and wait for startup info
+        sd = rp.TaskDescription(
+             {'uid'          : 'my_service',
+              'mode'         : rp.TASK_SERVICE,
+              'executable'   : __file__,
+              'arguments'    : ['service', 1024],
+              'info_pattern' : 'stdout:.*port: ([0-9]*).*',
+              'timeout'      : 10,  # startup timeout
+              'named_env'    : 'rp',
+              })
+        service = tmgr.submit_tasks(sd)
+
+        # collect the endpoint port
+        info = service.wait_info()
+        print('found %s: %s' % (service.uid, info))
+
+
+        # ---------------------------------------------------------------------
+        # run the clients
+        cds = list()
+        for i in range(4):
+            cd = rp.TaskDescription({'services'   : [service.uid],
+                                     'executable' : __file__,
+                                     'arguments'  : ['client']})
+            cds.append(cd)
+
+        clients = tmgr.submit_tasks(cds)
+        tmgr.wait_tasks(uids=[client.uid for client in clients])
+
+        for client in clients:
+            print(' * %s: %s' % (client.uid, client.stdout))
 
     finally:
         session.close(download=True)
@@ -93,14 +91,16 @@ def app():
 
 # ------------------------------------------------------------------------------
 #
-def service():
+def service(port):
+
+    port = int(port)
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.settimeout(1.0)
-    sock.bind(('localhost', 12345))
+    sock.bind(('localhost', port))
 
-    sys.stdout.write('port: 12345\n')
+    sys.stdout.write('port: %d\n' % port)
     sys.stdout.flush()
 
     sock.listen(1)
@@ -111,7 +111,7 @@ def service():
 
         try:
             client, _ = sock.accept()
-            client.send(client.recv(1024))
+            client.send(client.recv(port))
             client.close()
             print('%.2f: service served' % time.time())
 
@@ -130,7 +130,7 @@ def client():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(('localhost', port))
     sock.send(b'foo')
-    print(str(sock.recv(1024)))
+    print(str(sock.recv(port)))
     sock.close()
 
 
@@ -139,9 +139,10 @@ def client():
 if __name__ == '__main__':
 
     mode = sys.argv[1] if len(sys.argv) > 1 else 'app'
+    port = sys.argv[2] if len(sys.argv) > 2 else 1024
 
     if   mode == 'app'    : app()
-    elif mode == 'service': service()
+    elif mode == 'service': service(port)
     elif mode == 'client' : client()
     else: raise ValueError('unknown mode %s' % mode)
 
