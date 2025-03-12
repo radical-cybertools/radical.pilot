481bcd9f44b1f2c2bfc59df345f11672576d0b14
Mikhail Titov <mikhail.a.titov@gmail.com>
2025-03-12 16:00:42 -0400

    keep name consistent for all components (including Executor and Scheduler) - `is_canceled`

--- src/radical/pilot/agent/scheduler/base.py
+++ src/radical/pilot/agent/scheduler/base.py
@@ -1017,7 +1017,7 @@ class AgentSchedulingComponent(rpu.AgentComponent):
                 # now that we added the task to the waitpool, check if a cancel
                 # request has meanwhile arrived - if so remove it, otherwise it
                 # will get removed during the next iteration of the main loop
-                if self.check_canceled(task) is True:
+                if self.is_canceled(task) is True:
                     del self._waitpool[priority][uid]
 
         # we performed some activity (worked on tasks)
